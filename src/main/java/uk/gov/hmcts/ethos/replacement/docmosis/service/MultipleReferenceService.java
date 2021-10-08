bfb36a38e35c1167962ae50909ea3ec5f93e008e
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2021-10-08 10:07:20 +0100

    Fix multiple reference creation

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/MultipleReferenceService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/MultipleReferenceService.java
@@ -3,20 +3,21 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import static uk.gov.hmcts.ecm.common.model.helper.Constants.*;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefBristolRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefLeedsRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefLondonCentralRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefLondonEastRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefLondonSouthRepository;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.MultipleReference;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefEnglandWalesRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefMidlandsEastRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefMidlandsWestRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefNewcastleRepository;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefRepository;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefScotlandRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefWalesRepository;
-import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefWatfordRepository;
+
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_DEV_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_OFFICE_NUMBER;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_USERS_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_DEV_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_OFFICE_NUMBER;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_USERS_BULK_CASE_TYPE_ID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,39 +26,27 @@ public class MultipleReferenceService {
 
     private final MultipleRefEnglandWalesRepository multipleRefEnglandWalesRepository;
     private final MultipleRefScotlandRepository multipleRefScotlandRepository;
-    private final MultipleRefLeedsRepository multipleRefLeedsRepository;
-    private final MultipleRefMidlandsWestRepository multipleRefMidlandsWestRepository;
-    private final MultipleRefMidlandsEastRepository multipleRefMidlandsEastRepository;
-    private final MultipleRefBristolRepository multipleRefBristolRepository;
-    private final MultipleRefWalesRepository multipleRefWalesRepository;
-    private final MultipleRefNewcastleRepository multipleRefNewcastleRepository;
-    private final MultipleRefWatfordRepository multipleRefWatfordRepository;
-    private final MultipleRefLondonCentralRepository multipleRefLondonCentralRepository;
-    private final MultipleRefLondonSouthRepository multipleRefLondonSouthRepository;
-    private final MultipleRefLondonEastRepository multipleRefLondonEastRepository;
 
     public synchronized String createReference(String caseTypeId, int numberCases) {
-//        switch (caseTypeId) {
-//            case ENGLANDWALES_DEV_BULK_CASE_TYPE_ID:
-//            case ENGLANDWALES_USERS_BULK_CASE_TYPE_ID:
-//            case ENGLANDWALES_BULK_CASE_TYPE_ID:
-//                return generateOfficeReference(multipleRefEnglandWalesRepository, numberCases,
-//                        ENGLANDWALES_OFFICE_NUMBER, ENGLANDWALES_CASE_TYPE_ID);
-//            case SCOTLAND_DEV_BULK_CASE_TYPE_ID:
-//            case SCOTLAND_USERS_BULK_CASE_TYPE_ID:
-//            case SCOTLAND_BULK_CASE_TYPE_ID:
-//                return generateOfficeReference(multipleRefScotlandRepository, numberCases,
-//                        GLASGOW_OFFICE_NUMBER, SCOTLAND_CASE_TYPE_ID);
-//            default:
-//                return generateOfficeReference(multipleRefLeedsRepository, numberCases,
-//                        ENGLANDWALES_OFFICE_NUMBER, ENGLANDWALES_CASE_TYPE_ID);
-//        }
-        throw new UnsupportedOperationException();
+        switch (caseTypeId) {
+            case ENGLANDWALES_DEV_BULK_CASE_TYPE_ID:
+            case ENGLANDWALES_USERS_BULK_CASE_TYPE_ID:
+            case ENGLANDWALES_BULK_CASE_TYPE_ID:
+                return generateOfficeReference(multipleRefEnglandWalesRepository, numberCases,
+                        ENGLANDWALES_OFFICE_NUMBER, ENGLANDWALES_CASE_TYPE_ID);
+            case SCOTLAND_DEV_BULK_CASE_TYPE_ID:
+            case SCOTLAND_USERS_BULK_CASE_TYPE_ID:
+            case SCOTLAND_BULK_CASE_TYPE_ID:
+                return generateOfficeReference(multipleRefScotlandRepository, numberCases,
+                        SCOTLAND_OFFICE_NUMBER, SCOTLAND_CASE_TYPE_ID);
+            default:
+                throw new IllegalArgumentException(
+                        String.format("Unable to create case reference: unexpected caseTypeId %s", caseTypeId));
+        }
     }
 
-    private String generateOfficeReference(MultipleRefRepository referenceRepository, int numberCases,
-                                           String officeNumber, String officeName) {
+    private String generateOfficeReference(MultipleRefRepository<? extends MultipleReference> referenceRepository,
+                                           int numberCases, String officeNumber, String officeName) {
         return officeNumber + referenceRepository.ethosMultipleCaseRefGen(numberCases, officeName);
     }
-
 }
--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/MultipleReferenceServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/MultipleReferenceServiceTest.java
@@ -2,26 +2,78 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefEnglandWalesRepository;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.MultipleRefScotlandRepository;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_DEV_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_OFFICE_NUMBER;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_USERS_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_BULK_CASE_TYPE_ID;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_DEV_BULK_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_OFFICE_NUMBER;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_USERS_BULK_CASE_TYPE_ID;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MultipleReferenceServiceTest {
+
+    @InjectMocks
+    private MultipleReferenceService multipleReferenceService;
+
+    @Mock
+    private MultipleRefEnglandWalesRepository multipleRefEnglandWalesRepository;
 
     @Mock
     private MultipleRefScotlandRepository multipleRefScotlandRepository;
 
     @Test
-    public void createScotlandReference() {
-        when(multipleRefScotlandRepository.ethosMultipleCaseRefGen(1, SCOTLAND_CASE_TYPE_ID)).thenReturn("00015");
-        //String scotlandRef = GLASGOW_OFFICE_NUMBER + "00015";
-        //assertEquals(multipleReferenceService.createReference(SCOTLAND_DEV_BULK_CASE_TYPE_ID,1), scotlandRef);
-        fail("Unit test needs to be rewritten for new repositories");
+    public void createEnglandWalesReference() {
+        when(multipleRefEnglandWalesRepository.ethosMultipleCaseRefGen(1, ENGLANDWALES_CASE_TYPE_ID))
+                .thenReturn("00015");
+        var expectedReference = ENGLANDWALES_OFFICE_NUMBER + "00015";
+
+        assertEquals(expectedReference,
+                multipleReferenceService.createReference(ENGLANDWALES_DEV_BULK_CASE_TYPE_ID, 1));
+        assertEquals(expectedReference,
+                multipleReferenceService.createReference(ENGLANDWALES_USERS_BULK_CASE_TYPE_ID, 1));
+        assertEquals(expectedReference, multipleReferenceService.createReference(ENGLANDWALES_BULK_CASE_TYPE_ID, 1));
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void createEnglandWalesReferenceCaseTypeNotFound() {
+        when(multipleRefEnglandWalesRepository.ethosMultipleCaseRefGen(1, ENGLANDWALES_CASE_TYPE_ID))
+                .thenReturn("00015");
+
+        multipleReferenceService.createReference("invalid-case-type-id", 1);
+
+        fail("Should throw IllegalArgumentException");
+    }
+
+    @Test
+    public void createScotlandReference() {
+        when(multipleRefScotlandRepository.ethosMultipleCaseRefGen(1, SCOTLAND_CASE_TYPE_ID)).thenReturn("00015");
+        var expectedReference = SCOTLAND_OFFICE_NUMBER + "00015";
+
+        assertEquals(expectedReference, multipleReferenceService.createReference(SCOTLAND_DEV_BULK_CASE_TYPE_ID, 1));
+        assertEquals(expectedReference, multipleReferenceService.createReference(SCOTLAND_USERS_BULK_CASE_TYPE_ID, 1));
+        assertEquals(expectedReference, multipleReferenceService.createReference(SCOTLAND_BULK_CASE_TYPE_ID, 1));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void createScotlandReferenceCaseTypeNotFound() {
+        when(multipleRefScotlandRepository.ethosMultipleCaseRefGen(1, SCOTLAND_CASE_TYPE_ID))
+                .thenReturn("00015");
+
+        multipleReferenceService.createReference("invalid-case-type-id", 1);
+
+        fail("Should throw IllegalArgumentException");
+    }
 }

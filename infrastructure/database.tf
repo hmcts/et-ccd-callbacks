1b439c6e4c8e6c8d0bb91dab1093bff8a795f214
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2021-11-16 15:15:21 +0000

    Infrastructure changes

---
+++ infrastructure/database.tf
@@ -0,0 +1,12 @@
+module "db" {
+  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product            = "${var.product}-${var.component}-postgres-db"
+  component          = var.component
+  location           = var.location_db
+  env                = var.env
+  database_name      = "et_ccd_callbacks"
+  postgresql_user    = "et_ccd_callbacks"
+  postgresql_version = "11"
+  common_tags        = var.common_tags
+  subscription       = var.subscription
+}

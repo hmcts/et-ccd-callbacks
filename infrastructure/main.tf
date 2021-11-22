b9c43d71635cfe363d36139c724f66652bb6273f
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2021-11-22 10:08:03 +0000

    Add resource group

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,6 +12,12 @@ locals {
   }
 }
 
+resource "azurerm_resource_group" "rg" {
+  name     = local.resource_group_name
+  location = var.location
+  tags     = local.common_tags
+}
+
 data "azurerm_user_assigned_identity" "et-identity" {
   name                = "${var.product}-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"

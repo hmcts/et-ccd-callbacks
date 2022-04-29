735003fce40fe1e193b31bab7406e996e34eb5bb
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-04-29 09:05:06 +0100

    Refactor test to use JUnit 5

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/ReportHelperTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/ReportHelperTest.java
@@ -1,7 +1,7 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.helpers;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.et.common.model.ccd.SubmitEvent;
 import uk.gov.hmcts.et.common.model.listing.ListingData;
 import uk.gov.hmcts.et.common.model.listing.ListingDetails;
@@ -10,18 +10,18 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_LISTING_CASE_TYPE_ID;
 import static uk.gov.hmcts.ethos.replacement.docmosis.utils.ResourceLoader.generateListingDetails;
 import static uk.gov.hmcts.ethos.replacement.docmosis.utils.ResourceLoader.generateSubmitEventList;
 
-public class ReportHelperTest {
+class ReportHelperTest {
     private ListingDetails listingDetails;
     private ListingDetails listingDetailsEngWales;
     private List<SubmitEvent> submitEvents;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    void setUp() throws Exception {
         listingDetails = generateListingDetails("listingDetailsTest5.json");
         listingDetailsEngWales = generateListingDetails("listingDetailsTest5.json");
         listingDetailsEngWales.setCaseTypeId(ENGLANDWALES_LISTING_CASE_TYPE_ID);
@@ -29,7 +29,7 @@ public class ReportHelperTest {
     }
 
     @Test
-    public void getLocalReportDetailsForAllOfTheOffices() {
+    void getLocalReportDetailsForAllOfTheOffices() {
         ListingData listingData = ReportHelper.processLiveCaseloadRequest(listingDetails, submitEvents);
         var expected = List.of("Edinburgh", "Glasgow", "Dundee", "Aberdeen", "London Central");
         var offices = listingData.getLocalReportsDetail()
@@ -41,7 +41,7 @@ public class ReportHelperTest {
     }
 
     @Test
-    public void getLocalReportDetailsForEngWales() {
+    void getLocalReportDetailsForEngWales() {
         ListingData listingData = ReportHelper.processLiveCaseloadRequest(listingDetailsEngWales, submitEvents);
         var expected = List.of("EngWales");
         var offices = listingData.getLocalReportsDetail()

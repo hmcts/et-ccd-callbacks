9bf84a17a6d661f8ce3e28df1c46bac849f83029
robin-ieong <101253854+robin-ieong@users.noreply.github.com>
2022-05-16 15:08:53 +0100

    Ret 1778 (#259)
    * RET-1778: Add judge
    
    * Add the rest of the files
    
    * Refactored judge controller and service to be in own package
    
    * Swapped assertion order
    
    * Refactored to throw exception with error message
    
    * Check if same judge code exists globally
    
    * Updated to check both code and office
    
    * Refactored JudgeServiceTest
    
    * Resolved conflicts

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/admin/AdminData.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/admin/AdminData.java
@@ -16,6 +16,18 @@ public class AdminData {
     private ImportFile staffImportFile;
     @JsonProperty("venueImport")
     private VenueImport venueImport;
+
+    // For adding judge
+    @JsonProperty("tribunalOffice")
+    private String tribunalOffice;
+    @JsonProperty("judgeCode")
+    private String judgeCode;
+    @JsonProperty("judgeName")
+    private String judgeName;
+    @JsonProperty("employmentStatus")
+    private String employmentStatus;
+
     @JsonProperty("adminCourtWorker")
     private AdminCourtWorker adminCourtWorker;
+
 }

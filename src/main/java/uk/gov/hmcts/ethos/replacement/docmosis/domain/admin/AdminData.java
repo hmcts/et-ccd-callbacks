0746a08ceedb63d8d0b4e95866d37f587fa1270c
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-05-19 14:53:00 +0100

    RET-1786 (#266)

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/admin/AdminData.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/admin/AdminData.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.ethos.replacement.docmosis.domain.admin;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import uk.gov.hmcts.et.common.model.bulk.types.DynamicFixedListType;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.admin.types.AdminCourtWorker;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.admin.types.ImportFile;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.admin.types.VenueImport;
@@ -35,5 +36,17 @@ public class AdminData {
 
     @JsonProperty("adminCourtWorker")
     private AdminCourtWorker adminCourtWorker;
+
+    //updateCourtWorker
+    @JsonProperty("updateCourtWorkerOffice")
+    private String updateCourtWorkerOffice;
+    @JsonProperty("updateCourtWorkerType")
+    private String updateCourtWorkerType;
+    @JsonProperty("updateCourtWorkerCode")
+    private String updateCourtWorkerCode;
+    @JsonProperty("updateCourtWorkerName")
+    private String updateCourtWorkerName;
+    @JsonProperty("updateCourtWorkerSelectList")
+    private DynamicFixedListType updateCourtWorkerSelectList;
 
 }

1fed2b770ac7b80bc65dd119a58f436b42ec4587
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-05-30 09:47:55 +0100

    Add Update Judge

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/admin/staff/JudgeController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/admin/staff/JudgeController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,9 +20,11 @@ import uk.gov.hmcts.ethos.replacement.docmosis.service.admin.staff.JudgeService;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.admin.staff.SaveJudgeException;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Slf4j
 @RestController
 @RequestMapping("/admin/staff")
 @RequiredArgsConstructor
@@ -41,6 +44,8 @@ public class JudgeController {
             @RequestHeader("Authorization") String userToken,
             @RequestBody CCDRequest ccdRequest) {
 
+        log.info("/addJudge");
+
         if (!verifyTokenService.verifyTokenSignature(userToken)) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN.value()).build();
         }
@@ -54,4 +59,74 @@ public class JudgeController {
 
         return CCDCallbackResponse.getCallbackRespEntityNoErrors(adminData);
     }
+
+    @PostMapping(value = "/updateJudgeMidEventSelectOffice", consumes = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Populates the dynamicList for judge when office and type selected")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<CCDCallbackResponse> updateJudgeMidEventSelectOffice(
+            @RequestHeader("Authorization") String userToken,
+            @RequestBody CCDRequest ccdRequest) {
+
+        log.info("/updateJudgeMidEventSelectOffice");
+
+        if (!verifyTokenService.verifyTokenSignature(userToken)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN.value()).build();
+        }
+
+        var adminData = ccdRequest.getCaseDetails().getAdminData();
+        List<String> errors = judgeService.updateJudgeMidEventSelectOffice(adminData);
+
+        return CCDCallbackResponse.getCallbackRespEntityErrors(errors, adminData);
+    }
+
+    @PostMapping(value = "/updateJudgeMidEventSelectJudge", consumes = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Populates the judge code and name when dynamicList selected")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<CCDCallbackResponse> updateJudgeMidEventSelectJudge(
+            @RequestHeader("Authorization") String userToken,
+            @RequestBody CCDRequest ccdRequest) {
+
+        log.info("/updateJudgeMidEventSelectJudge");
+
+        if (!verifyTokenService.verifyTokenSignature(userToken)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN.value()).build();
+        }
+
+        var adminData = ccdRequest.getCaseDetails().getAdminData();
+        List<String> errors = judgeService.updateJudgeMidEventSelectJudge(adminData);
+
+        return CCDCallbackResponse.getCallbackRespEntityErrors(errors, adminData);
+    }
+
+    @PostMapping(value = "/updateJudge", consumes = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Update a judge")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully"),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<CCDCallbackResponse> updateJudge(
+            @RequestHeader("Authorization") String userToken,
+            @RequestBody CCDRequest ccdRequest) {
+
+        log.info("/updateJudge");
+
+        if (!verifyTokenService.verifyTokenSignature(userToken)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN.value()).build();
+        }
+
+        var adminData = ccdRequest.getCaseDetails().getAdminData();
+        List<String> errors = judgeService.updateJudge(adminData);
+
+        return CCDCallbackResponse.getCallbackRespEntityErrors(errors, adminData);
+    }
+
 }
--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/admin/staff/JudgeService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/admin/staff/JudgeService.java
@@ -3,10 +3,15 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service.admin.staff;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ecm.common.model.helper.TribunalOffice;
+import uk.gov.hmcts.et.common.model.bulk.types.DynamicFixedListType;
+import uk.gov.hmcts.et.common.model.bulk.types.DynamicValueType;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.admin.AdminData;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.referencedata.Judge;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.referencedata.JudgeEmploymentStatus;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.JudgeRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -16,9 +21,10 @@ public class JudgeService {
 
     public static final String ADD_JUDGE_CODE_AND_OFFICE_CONFLICT_ERROR =
             "A judge with the same Code (%s) and Tribunal Office (%s) already exists.";
-
     public static final String ADD_JUDGE_NAME_AND_OFFICE_CONFLICT_ERROR =
             "A judge with the same Name (%s) and Tribunal Office (%s) already exists.";
+    public static final String NO_FOUND_ERROR_MESSAGE = "No judge found in the %s office";
+    public static final String SAVE_ERROR_MESSAGE = "Update failed";
 
     public JudgeService(JudgeRepository judgeRepository) {
         this.judgeRepository = judgeRepository;
@@ -42,5 +48,68 @@ public class JudgeService {
         } else {
             judgeRepository.save(judge);
         }
+    }
+
+    public List<String> updateJudgeMidEventSelectOffice(AdminData adminData) {
+        List<String> errors = new ArrayList<>();
+        var tribunalOffice = adminData.getTribunalOffice();
+
+        List<Judge> judgeList =
+                judgeRepository.findByTribunalOfficeOrderById(TribunalOffice.valueOfOfficeName(tribunalOffice));
+        if (judgeList.isEmpty()) {
+            errors.add(String.format(NO_FOUND_ERROR_MESSAGE, tribunalOffice));
+            return errors;
+        }
+
+        List<DynamicValueType> dynamicJudge = new ArrayList<>();
+        for (var judge : judgeList) {
+            dynamicJudge.add(DynamicValueType.create(judge.getId().toString(), judge.getName()));
+        }
+
+        var judgeDynamicList = new DynamicFixedListType();
+        judgeDynamicList.setListItems(dynamicJudge);
+
+        adminData.setJudgeSelectList(judgeDynamicList);
+        return errors;
+    }
+
+    public List<String> updateJudgeMidEventSelectJudge(AdminData adminData) {
+        List<String> errors = new ArrayList<>();
+        var selectedId = Integer.parseInt(adminData.getJudgeSelectList().getSelectedCode());
+
+        var findJudge = judgeRepository.findById(selectedId);
+        if (!findJudge.isEmpty()) {
+            var selectedJudge = findJudge.get(0);
+            adminData.setJudgeCode(selectedJudge.getCode());
+            adminData.setJudgeName(selectedJudge.getName());
+            adminData.setEmploymentStatus(selectedJudge.getEmploymentStatus().toString());
+        } else {
+            errors.add(SAVE_ERROR_MESSAGE);
+        }
+
+        return errors;
+    }
+
+    public List<String> updateJudge(AdminData adminData) {
+        List<String> errors = new ArrayList<>();
+        var selectedId = Integer.parseInt(adminData.getJudgeSelectList().getSelectedCode());
+
+        var findJudge = judgeRepository.findById(selectedId);
+        if (!findJudge.isEmpty()) {
+            var thisJudge = findJudge.get(0);
+            thisJudge.setName(adminData.getJudgeName());
+            thisJudge.setEmploymentStatus(JudgeEmploymentStatus.valueOf(adminData.getEmploymentStatus()));
+            if (judgeRepository.existsByTribunalOfficeAndNameAndIdIsNot(
+                    thisJudge.getTribunalOffice(), thisJudge.getName(), selectedId)) {
+                errors.add(String.format(ADD_JUDGE_NAME_AND_OFFICE_CONFLICT_ERROR, thisJudge.getName(),
+                        thisJudge.getTribunalOffice()));
+            } else {
+                judgeRepository.save(thisJudge);
+            }
+        } else {
+            errors.add(SAVE_ERROR_MESSAGE);
+        }
+
+        return errors;
     }
 }

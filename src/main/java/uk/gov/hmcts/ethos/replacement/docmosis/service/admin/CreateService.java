04f30d3f95704de2a0530d115fd7e0a4332b1496
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-06-10 17:51:50 +0100

    Update CreateService

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/admin/CreateService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/admin/CreateService.java
@@ -1,7 +1,5 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.service.admin;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ecm.common.client.CcdClient;
@@ -13,8 +11,6 @@ import java.util.List;
 import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.MAX_ES_SIZE;
 
-@Slf4j
-@RequiredArgsConstructor
 @Service
 public class CreateService {
 
@@ -22,6 +18,10 @@ public class CreateService {
     public static final String CREATE_EXIST_ERROR_MESSAGE = "ECM Admin already exists.";
 
     private final CcdClient ccdClient;
+
+    public CreateService(CcdClient ccdClient) {
+        this.ccdClient = ccdClient;
+    }
 
     public List<String> initCreateAdmin(String userToken) {
         List<String> errors = new ArrayList<>();
@@ -37,7 +37,6 @@ public class CreateService {
             String query = boolQueryCreate();
             listSubmitEvents = ccdClient.executeElasticSearch(userToken, ECM_ADMIN_CASE_TYPE_ID, query);
         } catch (Exception ex) {
-            log.error("Error retrieving case for Create ECM Admin");
             throw new RuntimeException("Error retrieving case for Create ECM Admin", ex);
         }
         return !listSubmitEvents.isEmpty();

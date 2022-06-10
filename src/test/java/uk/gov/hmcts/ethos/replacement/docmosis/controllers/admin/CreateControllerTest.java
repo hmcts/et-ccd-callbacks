ca2ca95eb759edaf5607ca2a4f37f743e918fc17
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-06-10 19:31:38 +0100

    RET-1860 (#290)

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/admin/CreateControllerTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/admin/CreateControllerTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.controllers.admin;
 
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +13,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.admin.AdminData;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.VerifyTokenService;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.admin.CreateService;
 import uk.gov.hmcts.ethos.replacement.docmosis.utils.AdminDataBuilder;
 import uk.gov.hmcts.ethos.replacement.docmosis.utils.JsonMapper;
 
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -28,8 +36,12 @@ import static uk.gov.hmcts.ethos.replacement.docmosis.controllers.admin.CreateCo
 @WebMvcTest({CreateController.class, JsonMapper.class})
 class CreateControllerTest {
 
+    private String token;
+
     @MockBean
     private VerifyTokenService verifyTokenService;
+    @MockBean
+    private CreateService createService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -37,10 +49,15 @@ class CreateControllerTest {
     @Autowired
     private JsonMapper jsonMapper;
 
+    @BeforeEach
+    void setUp() {
+        token = "some-token";
+        when(createService.initCreateAdmin(token)).thenReturn(new ArrayList<>());
+    }
+
     @Test
     void testHandleAboutToSubmitEventSuccess() throws Exception {
         var ccdRequest = AdminDataBuilder.builder().buildAsCCDRequest();
-        var token = "some-token";
         when(verifyTokenService.verifyTokenSignature(token)).thenReturn(true);
 
         ResultActions resultActions = mockMvc.perform(post("/admin/create/aboutToSubmitEvent")
@@ -49,7 +66,7 @@ class CreateControllerTest {
                 .content(jsonMapper.toJson(ccdRequest)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data", notNullValue()))
-                .andExpect(jsonPath("$.errors", nullValue()))
+                .andExpect(jsonPath("$.errors", hasSize(0)))
                 .andExpect(jsonPath("$.warnings", nullValue()));
 
         var result = resultActions.andReturn();
@@ -57,13 +74,13 @@ class CreateControllerTest {
         var json = new JSONObject(contentAsString).getString("data");
         var adminData = jsonMapper.fromJson(json, AdminData.class);
 
+        verify(createService, times(1)).initCreateAdmin(token);
         assertEquals(ADMIN_CASE_NAME, adminData.getName());
     }
 
     @Test
     void testHandleAboutToSubmitEventForbidden() throws Exception {
         var ccdRequest = AdminDataBuilder.builder().buildAsCCDRequest();
-        var token = "some-token";
         when(verifyTokenService.verifyTokenSignature(token)).thenReturn(false);
 
         mockMvc.perform(post("/admin/create/aboutToSubmitEvent")
@@ -71,11 +88,11 @@ class CreateControllerTest {
                         .header("Authorization", token)
                         .content(jsonMapper.toJson(ccdRequest)))
                 .andExpect(status().isForbidden());
+        verify(createService, never()).initCreateAdmin(token);
     }
 
     @Test
     void testHandleAboutToSubmitEventBadRequest() throws Exception {
-        var token = "some-token";
         when(verifyTokenService.verifyTokenSignature(token)).thenReturn(true);
 
         mockMvc.perform(post("/admin/create/aboutToSubmitEvent")
@@ -83,5 +100,6 @@ class CreateControllerTest {
                         .header("Authorization", token)
                         .content("error"))
                 .andExpect(status().isBadRequest());
+        verify(createService, never()).initCreateAdmin(token);
     }
 }

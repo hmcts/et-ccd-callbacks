243256c2dc588df1acf33f28569d98a0773421bb
robin-ieong <robin.ieong@version1.com>
2022-06-15 15:54:31 +0100

    Update claimant name variable

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseActionsForCaseWorkerController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseActionsForCaseWorkerController.java
@@ -1189,31 +1189,6 @@ public class CaseActionsForCaseWorkerController {
                 caseData.getClaimantType().getClaimantAddressUK(),
                 caseData.getRespondentCollection()));
 
-        return getCallbackRespEntityNoErrors(ccdRequest.getCaseDetails().getCaseData());
-    }
-
-    @PostMapping(value = "/emailLinkToAcas", consumes = APPLICATION_JSON_VALUE)
-    @Operation(summary = "Generate mailto link for sending emails to Acas.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Accessed successfully",
-            content = {
-                @Content(mediaType = "application/json", schema = @Schema(implementation = CCDCallbackResponse.class))
-            }),
-        @ApiResponse(responseCode = "400", description = "Bad Request"),
-        @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    })
-    public ResponseEntity<CCDCallbackResponse> generateEmailLinkToAcas(
-        @RequestBody CCDRequest ccdRequest,
-        @RequestHeader(value = "Authorization") String userToken) {
-
-        if (!verifyTokenService.verifyTokenSignature(userToken)) {
-            log.error(INVALID_TOKEN, userToken);
-            return ResponseEntity.status(FORBIDDEN.value()).build();
-        }
-
-        CaseData caseData = ccdRequest.getCaseDetails().getCaseData();
-
-        String caseNumber = caseData.getEthosCaseReference();
 
         StringBuilder respondentList = new StringBuilder();
         List<RespondentSumTypeItem> respondentCollection = caseData.getRespondentCollection();
@@ -1221,21 +1196,23 @@ public class CaseActionsForCaseWorkerController {
             if (!respondentList.toString().isEmpty()) {
                 respondentList.append("%2C%20");
             }
-
             respondentList.append(respondent.getValue().getRespondentName().replaceAll("\\s+", "%20"));
         }
 
+        String caseNumber = caseData.getEthosCaseReference();
+        String claimantName = caseData.getClaimantIndType().getClaimantFirstNames() + " "
+                + caseData.getClaimantIndType().getClaimantLastName();
         String mailToLink = "mailto:ET3@acas.org.uk?subject="
-            + caseNumber
-            + "&body=Parties%20in%20claim%3A%20"
-            + caseData.getClaimant().replaceAll("\\s+", "%20")
-            + "%20vs%20"
-            + respondentList
-            + "%0D%0ACase%20reference%20number%3A%20"
-            + caseNumber
-            + "%0D%0A%0D%0ADear%20Acas%2C%0D%0A%0D%0A"
-            + "The%20tribunal%20has%20completed%20ET1%20serving%20to%20the%20respondent.%0D%0A%0D%0A"
-            + "The%20documents%20we%20sent%20are%20attached%20to%20this%20email.%0D%0A%0D%0A";
+                + caseNumber
+                + "&body=Parties%20in%20claim%3A%20"
+                + claimantName.replaceAll("\\s+", "%20")
+                + "%20vs%20"
+                + respondentList
+                + "%0D%0ACase%20reference%20number%3A%20"
+                + caseNumber
+                + "%0D%0A%0D%0ADear%20Acas%2C%0D%0A%0D%0A"
+                + "The%20tribunal%20has%20completed%20ET1%20serving%20to%20the%20respondent.%0D%0A%0D%0A"
+                + "The%20documents%20we%20sent%20are%20attached%20to%20this%20email.%0D%0A%0D%0A";
 
         caseData.setEmailLinkToAcas(mailToLink);
 

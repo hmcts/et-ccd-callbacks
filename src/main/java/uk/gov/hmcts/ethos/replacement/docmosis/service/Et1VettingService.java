5f6acd37ec9dab221ea26be31633d2790d82db47
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-06-23 16:06:35 +0100

    RET-1838 (#304)

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/Et1VettingService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/Et1VettingService.java
@@ -3,49 +3,77 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
 import uk.gov.hmcts.et.common.model.ccd.items.DocumentTypeItem;
+import uk.gov.hmcts.ethos.replacement.docmosis.utils.IntWrapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class Et1VettingService {
 
-    static final String ET1_DOC_TYPE = "ET1";
-    static final String ACAS_DOC_TYPE = "ACAS Certificate";
-    static final String DOC_LINK_DEFAULT = "/cases/case-details/%s#Documents";
-    static final String BEFORE_LINK_LABEL = "Open these documents to help you complete this form: "
-            + "<br/><a target=\"_blank\" href=\"%s\">ET1 form (opens in new tab)</a>"
-            + "<br/><a target=\"_blank\" href=\"%s\">Acas certificate (opens in new tab)</a>"
+    private static final String ET1_DOC_TYPE = "ET1";
+    private static final String ACAS_DOC_TYPE = "ACAS Certificate";
+    private static final String BEFORE_LABEL_TEMPLATE = "Open these documents to help you complete this form: %s%s"
             + "<br/>Check the Documents tab for additional ET1 documents the claimant may have uploaded.";
+    private static final String BEFORE_LABEL_ET1 =
+            "<br/><a target=\"_blank\" href=\"%s\">ET1 form (opens in new tab)</a>";
+    private static final String BEFORE_LABEL_ACAS =
+            "<br/><a target=\"_blank\" href=\"%s\">Acas certificate %s (opens in new tab)</a>";
+    private static final String BEFORE_LABEL_ACAS_OPEN_TAB =
+            "<br/><a target=\"_blank\" href=\"/cases/case-details/%s#Documents\">"
+                    + "Open the Documents tab to view/open Acas certificates (opens in new tab)</a>";
 
     /**
      * Update et1VettingBeforeYouStart.
      * @param caseDetails Get caseId and Update caseData
      */
-    public void initialBeforeYouStart(CaseDetails caseDetails) {
-
-        var et1BinaryUrl = createDocLinkDefault(caseDetails.getCaseId());
-        var acasBinaryUrl = createDocLinkDefault(caseDetails.getCaseId());
-
-        var documentCollection = caseDetails.getCaseData().getDocumentCollection();
-        if (documentCollection != null && !documentCollection.isEmpty()) {
-            for (DocumentTypeItem d : documentCollection) {
-                if (ET1_DOC_TYPE.equals(d.getValue().getTypeOfDocument())) {
-                    et1BinaryUrl = createDocLinkBinary(d);
-                }
-                if (ACAS_DOC_TYPE.equals(d.getValue().getTypeOfDocument())) {
-                    acasBinaryUrl = createDocLinkBinary(d);
-                }
-            }
-        }
-
-        caseDetails.getCaseData().setEt1VettingBeforeYouStart(
-                String.format(BEFORE_LINK_LABEL, et1BinaryUrl, acasBinaryUrl));
+    public void initialiseEt1Vetting(CaseDetails caseDetails) {
+        caseDetails.getCaseData().setEt1VettingBeforeYouStart(initialBeforeYouStart(caseDetails));
     }
 
-    private String createDocLinkDefault(String caseId) {
-        return String.format(DOC_LINK_DEFAULT, caseId);
+    /**
+     * Prepare wordings to be displayed in et1VettingBeforeYouStart.
+     * Check uploaded document in documentCollection
+     *  For ET1 form
+     *  - get and display ET1 form
+     *  For Acas cert
+     *  - get and count number of Acas cert
+     *  - if 0 Acas cert, hide the Acas link
+     *  - if 1-5 Acas cert(s), display one or multi Acas link(s)
+     *  - if 6 or more Acas certs, display a link to case doc tab
+     * @param caseDetails Get caseId and documentCollection
+     * @return et1VettingBeforeYouStart
+     */
+    private String initialBeforeYouStart(CaseDetails caseDetails) {
+
+        String et1Display = "";
+        String acasDisplay = "";
+        IntWrapper acasCount = new IntWrapper(0);
+
+        List<DocumentTypeItem> documentCollection = caseDetails.getCaseData().getDocumentCollection();
+        if (documentCollection != null) {
+            et1Display = documentCollection
+                    .stream()
+                    .filter(d -> d.getValue().getTypeOfDocument().equals(ET1_DOC_TYPE))
+                    .map(d -> String.format(BEFORE_LABEL_ET1, createDocLinkBinary(d)))
+                    .collect(Collectors.joining());
+            acasDisplay = documentCollection
+                    .stream()
+                    .filter(d -> d.getValue().getTypeOfDocument().equals(ACAS_DOC_TYPE))
+                    .map(d -> String.format(
+                            BEFORE_LABEL_ACAS, createDocLinkBinary(d), acasCount.incrementAndReturnValue()))
+                    .collect(Collectors.joining());
+        }
+
+        if (acasCount.getValue() > 5) {
+            acasDisplay = String.format(BEFORE_LABEL_ACAS_OPEN_TAB, caseDetails.getCaseId());
+        }
+
+        return String.format(BEFORE_LABEL_TEMPLATE, et1Display, acasDisplay);
     }
 
     private String createDocLinkBinary(DocumentTypeItem documentTypeItem) {
-        var documentBinaryUrl = documentTypeItem.getValue().getUploadedDocument().getDocumentBinaryUrl();
+        String documentBinaryUrl = documentTypeItem.getValue().getUploadedDocument().getDocumentBinaryUrl();
         return documentBinaryUrl.substring(documentBinaryUrl.indexOf("/documents/"));
     }
 

f4422361f4c8d62fb06273a8c3739bca70edf63b
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2022-06-28 15:30:23 +0100

    Add missing code

--- src/test/end-to-end/paths/verifyRejectCase.js
+++ src/test/end-to-end/paths/verifyRejectCase.js
@@ -1,14 +1,18 @@
 const testConfig = require('./../../config');
 const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
 const {eventNames} = require('../pages/common/constants.js');
-const {rejectCaseEvent} = require("../helpers/caseHelper");
 let caseNumber;
 
 Feature('Create a Leeds Singles Case and move to Rejected state');
 
 Scenario('Verify Reject Case', async ({I}) => {
     caseNumber = await createCaseInCcd('src/test/end-to-end/data/ccd-case-basic-data.json');
-    await rejectCaseEvent(I, caseNumber, eventNames.REJECT_CASE);
+    await I.wait(5);
+    await I.authenticateWithIdam();
+    await I.wait(5);
+    await I.amOnPage('/case-details/' + caseNumber);
+    await I.chooseNextStep(eventNames.REJECT_CASE, 3);
+    await I.rejectTheCase();
 
 }).tag('@e2e')
     .tag('@nightly')

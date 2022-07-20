86de70a077d7e5a565535d4cc7beeb2b3b2ffbf1
btensay <tensay.bulcha@gmail.com>
2022-07-20 17:54:25 +0100

    Delete Judge event JudgeService extension
    JudgeService extension to implement the Delete Judge event.

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/admin/staff/JudgeService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/admin/staff/JudgeService.java
@@ -23,7 +23,8 @@ public class JudgeService {
             "A judge with the same Code (%s) and Tribunal Office (%s) already exists.";
     public static final String ADD_JUDGE_NAME_AND_OFFICE_CONFLICT_ERROR =
             "A judge with the same Name (%s) and Tribunal Office (%s) already exists.";
-    public static final String NO_FOUND_ERROR_MESSAGE = "No judge found in the %s office";
+    public static final String NO_JUDGE_FOUND_ERROR_MESSAGE = "No judge found in the %s office";
+    public static final String NO_JUDGE_FOUND_WITH_NAME_SPECIFIED_ERROR_MESSAGE = "No judge found with name %s";
     public static final String SAVE_ERROR_MESSAGE = "Update failed";
 
     public JudgeService(JudgeRepository judgeRepository) {
@@ -60,11 +61,10 @@ public class JudgeService {
     public List<String> updateJudgeMidEventSelectOffice(AdminData adminData) {
         List<String> errors = new ArrayList<>();
         var tribunalOffice = adminData.getTribunalOffice();
+        List<Judge> judgeList = getJudgesListByOffice(tribunalOffice);
 
-        List<Judge> judgeList =
-                judgeRepository.findByTribunalOfficeOrderById(TribunalOffice.valueOfOfficeName(tribunalOffice));
         if (judgeList.isEmpty()) {
-            errors.add(String.format(NO_FOUND_ERROR_MESSAGE, tribunalOffice));
+            errors.add(String.format(NO_JUDGE_FOUND_ERROR_MESSAGE, tribunalOffice));
             return errors;
         }
 
@@ -83,17 +83,12 @@ public class JudgeService {
     public List<String> updateJudgeMidEventSelectJudge(AdminData adminData) {
         List<String> errors = new ArrayList<>();
         var selectedId = Integer.parseInt(adminData.getJudgeSelectList().getSelectedCode());
-
         var findJudge = judgeRepository.findById(selectedId);
-        if (!findJudge.isEmpty()) {
-            var selectedJudge = findJudge.get(0);
-            adminData.setJudgeCode(selectedJudge.getCode());
-            adminData.setJudgeName(selectedJudge.getName());
-            adminData.setEmploymentStatus(selectedJudge.getEmploymentStatus().toString());
-        } else {
+        if (findJudge.isEmpty()) {
             errors.add(SAVE_ERROR_MESSAGE);
+            return errors;
         }
-
+        setSelectedJudgeDetails(findJudge.get(0), adminData);
         return errors;
     }
 
@@ -118,5 +113,61 @@ public class JudgeService {
         }
 
         return errors;
+    }
+
+    public List<String> deleteJudge(AdminData adminData) {
+        List<String> errors = new ArrayList<>();
+        var selectedId = Integer.parseInt(adminData.getJudgeSelectList().getSelectedCode());
+        var matchingJudgesList = judgeRepository.findById(selectedId);
+        if (matchingJudgesList.isEmpty()) {
+            errors.add(String.format(NO_JUDGE_FOUND_WITH_NAME_SPECIFIED_ERROR_MESSAGE, adminData.getJudgeName()));
+            return errors;
+        }
+        judgeRepository.deleteAll(matchingJudgesList);
+        judgeRepository.flush();
+        return errors;
+    }
+
+    public List<String> deleteJudgeMidEventSelectOffice(AdminData adminData) {
+        List<String> errors = new ArrayList<>();
+        var tribunalOffice = adminData.getTribunalOffice();
+        List<Judge> judgeList = getJudgesListByOffice(adminData.getTribunalOffice());
+        if (judgeList.isEmpty()) {
+            errors.add(String.format(NO_JUDGE_FOUND_ERROR_MESSAGE, tribunalOffice));
+            return errors;
+        }
+        adminData.setJudgeSelectList(getJudgesDynamicList(judgeList));
+        return errors;
+    }
+
+    public List<String> deleteJudgeMidEventSelectJudge(AdminData adminData) {
+        var errors = new ArrayList<String>();
+        var selectedId = Integer.parseInt(adminData.getJudgeSelectList().getSelectedCode());
+        var findJudge = judgeRepository.findById(selectedId);
+        if (findJudge.isEmpty()) {
+            errors.add(String.format(NO_JUDGE_FOUND_WITH_NAME_SPECIFIED_ERROR_MESSAGE, adminData.getJudgeName()));
+            return errors;
+        }
+        setSelectedJudgeDetails(findJudge.get(0), adminData);
+        return errors;
+    }
+
+    private List<Judge> getJudgesListByOffice(String tribunalOffice) {
+        return judgeRepository.findByTribunalOfficeOrderById(TribunalOffice.valueOfOfficeName(tribunalOffice));
+    }
+
+    private DynamicFixedListType getJudgesDynamicList(List<Judge> judgeList) {
+        List<DynamicValueType> dynamicJudge = new ArrayList<>();
+        judgeList.forEach(judge -> dynamicJudge.add(DynamicValueType.create(judge.getId().toString(),
+            judge.getName())));
+        var judgeDynamicList = new DynamicFixedListType();
+        judgeDynamicList.setListItems(dynamicJudge);
+        return judgeDynamicList;
+    }
+
+    private void setSelectedJudgeDetails(Judge selectedJudge, AdminData adminData) {
+        adminData.setJudgeCode(selectedJudge.getCode());
+        adminData.setJudgeName(selectedJudge.getName());
+        adminData.setEmploymentStatus(selectedJudge.getEmploymentStatus().toString());
     }
 }

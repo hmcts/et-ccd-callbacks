787638486bdd73e96ed74cf308095b00179653a7
hsjhita <harpreet.jhita@justice.gov.uk>
2022-08-16 15:28:42 +0100

    Update Et3VettingServiceTest.java

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/Et3VettingServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/Et3VettingServiceTest.java
@@ -2,8 +2,9 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.webjars.NotFoundException;
 import uk.gov.hmcts.et.common.model.bulk.types.DynamicFixedListType;
 import uk.gov.hmcts.et.common.model.bulk.types.DynamicValueType;
@@ -11,20 +12,31 @@ import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.DocumentInfo;
 import uk.gov.hmcts.et.common.model.ccd.types.RespondentSumType;
 import uk.gov.hmcts.ethos.replacement.docmosis.utils.CaseDataBuilder;
+import uk.gov.hmcts.ethos.replacement.docmosis.utils.InternalException;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_CASE_TYPE_ID;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.YES;
+import static uk.gov.hmcts.ethos.replacement.docmosis.utils.InternalException.ERROR_MESSAGE;
 
+@ExtendWith(SpringExtension.class)
 class Et3VettingServiceTest {
     private Et3VettingService et3VettingService;
-    @Autowired
+    @MockBean
     private DocumentManagementService documentManagementService;
-    @Autowired
+    @MockBean
     private TornadoService tornadoService;
     private CaseData caseData;
     private DocumentInfo documentInfo;
@@ -51,6 +63,7 @@ class Et3VettingServiceTest {
                 .description("test-description")
                 .url("https://test.com/documents/random-uuid")
                 .build();
+        doCallRealMethod().when(documentManagementService).addDocumentToDocumentField(documentInfo);
     }
 
     @Test
@@ -90,5 +103,32 @@ class Et3VettingServiceTest {
         et3VettingService.restoreEt3VettingFromRespondentOntoCaseData(caseData);
         assertThat(caseData.getEt3NoEt3Response(),
             equalTo("Test data"));
+    }
+
+    @Test
+    void generateEt3ProcessingDocument() throws IOException {
+        when(tornadoService.generateEventDocument(any(CaseData.class), anyString(),
+                anyString(), anyString())).thenReturn(documentInfo);
+        DocumentInfo documentInfo1 = et3VettingService.generateEt3ProcessingDocument(new CaseData(), "userToken",
+                ENGLANDWALES_CASE_TYPE_ID);
+        assertThat(documentInfo1, is(documentInfo));
+    }
+
+    @Test
+    void generateEt3ProcessingDocumentExceptions() throws IOException {
+        when(tornadoService.generateEventDocument(any(CaseData.class), anyString(),
+                anyString(), anyString())).thenThrow(new InternalException(ERROR_MESSAGE));
+        assertThrows(Exception.class, () -> et3VettingService.generateEt3ProcessingDocument(new CaseData(), "userToken",
+                ENGLANDWALES_CASE_TYPE_ID));
+    }
+
+    @Test
+    void assertThatEt3DocumentIsSaved() {
+        et3VettingService.saveEt3VettingToRespondent(caseData, documentInfo);
+        RespondentSumType respondentSumType = caseData.getRespondentCollection().get(0).getValue();
+
+        assertNotNull(respondentSumType.getEt3Vetting().getEt3VettingDocument());
+        assertThat(respondentSumType.getEt3Vetting().getEt3VettingDocument().getDocumentFilename(),
+                is(documentInfo.getDescription()));
     }
 }

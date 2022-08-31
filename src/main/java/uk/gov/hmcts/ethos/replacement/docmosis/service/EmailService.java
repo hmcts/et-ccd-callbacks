109f2d7bfa11566b829562c9819d8cdb157a1799
Jack Reeve <jack@reevies.tech>
2022-08-31 17:28:52 +0100

    added email client and service

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/EmailService.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.ethos.replacement.docmosis.config.EmailClient;
+import uk.gov.service.notify.NotificationClientException;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class EmailService {
+
+    private final EmailClient emailClient;
+
+    @Autowired
+    public EmailService(EmailClient emailClient) {
+        this.emailClient = emailClient;
+    }
+
+    public void sendEmail(String templateId, String emailAddress, Map<String, String> personalisation) {
+
+        var referenceId = UUID.randomUUID().toString();
+
+        try {
+            emailClient.sendEmail(
+                templateId,
+                emailAddress,
+                personalisation,
+                referenceId
+            );
+            log.info("Sending email success. Reference ID: {}", referenceId);
+
+        } catch (NotificationClientException e) {
+            log.warn("Failed to send email. Reference ID: {}. Reason:", referenceId, e);
+        }
+    }
+} 

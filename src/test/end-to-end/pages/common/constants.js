0d7345b57223f31b016da9e486f1eec5bdf77261
Pettedson John <pettedson.john@hmcts.net>
2022-09-06 10:42:06 +0100

    Ammending the WIP Commit...

--- src/test/end-to-end/pages/common/constants.js
+++ src/test/end-to-end/pages/common/constants.js
@@ -43,8 +43,10 @@ const states = {
     TRANSFERRED: 'Transferred'
 };
 
+const timings = {defaultPageClickWaitTime : 5};
+
 module.exports = {
-    defaultPageClickWaitTime : 5,
+    timings,
     ccdUserType,
     eventNames,
     states

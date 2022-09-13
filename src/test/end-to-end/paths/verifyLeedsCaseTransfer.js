3dbc6ba32f8f34dc36eaf902ffab9949a4453a62
Pettedson John <pettedson.john@hmcts.net>
2022-09-13 12:58:52 +0100

    Added all the relevant code for the username password refactoring...

--- src/test/end-to-end/paths/verifyLeedsCaseTransfer.js
+++ src/test/end-to-end/paths/verifyLeedsCaseTransfer.js
@@ -2,13 +2,14 @@ const testConfig = require('./../../config');
 const {createCaseInCcd} = require("../helpers/ccdDataStoreApi");
 const {eventNames} = require('../pages/common/constants.js');
 const {caseTransfer} = require("../helpers/caseHelper");
+const testConfig = require('../../config');
 let caseNumber;
 
 Feature('Create a Leeds Single Case & Execute Case Transfer');
 
 Scenario('Verify Leeds Case Transfer', async ({I}) => {
     caseNumber = await createCaseInCcd('src/test/end-to-end/data/ccd-case-basic-data.json');
-    await I.authenticateWithIdam();
+    await I.authenticateWithIdam(testConfig.TestEnvCWUser, testConfig.TestEnvCWPassword);
     await I.wait(5);
     await I.amOnPage('/case-details/' + caseNumber);
     await caseTransfer(I, eventNames.CASE_TRANSFER);

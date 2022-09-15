07e802e769f9b5f296ba2a7767a689fab447c757
Sunday Ayeni <sunnyayeni18@gmail.com>
2022-09-15 11:04:30 +0100

    fix fortify failures

--- src/test/config.js
+++ src/test/config.js
@@ -16,6 +16,7 @@ module.exports = {
     TestEnvEtJudgeUser: process.env.ET_JUDGE_EMAIL || '',
     TestEnvEtJudgePwd: process.env.ET_JUDGE_PASSWORD || '',
     TestForXUI: process.env.TESTS_FOR_XUI_SERVICE === 'true',
+    TestAutoDelayEnabled: process.env.E2E_AUTO_DELAY_ENABLED === 'true',
     TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY === 'true',
     TestForCrossBrowser: process.env.TESTS_FOR_CROSS_BROWSER === 'true',
     TestIdamClientSecret: process.env.IDAM_CLIENT_SECRET || '',

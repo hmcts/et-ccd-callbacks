8a891e84dc604a5770420fbb662284516b6094a1
Pettedson John <pettedson.john@hmcts.net>
2022-09-16 11:52:40 +0100

    WIP

--- src/test/end-to-end/pages/amendRespondentRepresentative/amendRespondentRepresentative.js
+++ src/test/end-to-end/pages/amendRespondentRepresentative/amendRespondentRepresentative.js
@@ -1,17 +1,16 @@
 'use strict';
 const commonConfig = require('../../data/commonConfig.json');
 const testConfig = require("../../../config");
+const amendRespondentRepresentative = require("./amendResponsdentRepresentative.json");
 
 module.exports = async function () {
 
     const I = this;
     await I.selectOption('#repCollection_0_dynamic_resp_rep_name', commonConfig.respondentName);
     await I.fillField('#repCollection_0_name_of_representative', commonConfig.respondentRepresentativeName);
-    await I.fillField('#repCollection_0_representative_address_representative_address_postcodeInput', commonConfig.respondentRepPostCode);
-    await I.click(commonConfig.findAddressButton);
-    await I.wait(2);
-    I.waitForText(commonConfig.respondentRepAddress, testConfig.TestTimeToWaitForText);
-    await I.selectOption('#repCollection_0_representative_address_representative_address_addressList', commonConfig.respondentRepAddress);
+    await I.checkOption(amendRespondentRepresentative.doesTheRepresentativeHaveAnAccount);
+    I.wait(1);
+
     await I.navByClick(commonConfig.continue);
     await I.click(commonConfig.submit);
 };

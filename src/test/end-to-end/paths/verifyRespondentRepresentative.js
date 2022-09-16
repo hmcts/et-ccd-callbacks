62f703b68d28b0ea02810bd6dbf00dc0711a74c9
Pettedson John <pettedson.john@hmcts.net>
2022-09-16 14:27:35 +0100

    Adding the Config for the pipeline execution of the tests...

--- src/test/end-to-end/paths/verifyRespondentRepresentative.js
+++ src/test/end-to-end/paths/verifyRespondentRepresentative.js
@@ -14,5 +14,5 @@ Scenario('Verify Respondent Representative', async ({I}) => {
 
 }).tag('@e2e')
     .tag('@nightly')
-    .tag('@wip').tag('pats');
+    .tag('@wip').tag('RET-BAT');
     //.retry(testConfig.TestRetryScenarios);

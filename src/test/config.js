d6d3aaf847712d049a998968611e69cb1ce0f97f
Pettedson John <pettedson.john@hmcts.net>
2022-09-21 14:10:59 +0100

    Further Fixes to cover the merge fixes....

--- src/test/config.js
+++ src/test/config.js
@@ -19,6 +19,7 @@ module.exports = {
     TestAutoDelayEnabled: process.env.E2E_AUTO_DELAY_ENABLED === 'true',
     TestForAccessibility: process.env.TESTS_FOR_ACCESSIBILITY === 'true',
     TestForCrossBrowser: process.env.TESTS_FOR_CROSS_BROWSER === 'true',
+    TestIdamClientSecret: process.env.IDAM_CLIENT_SECRET || '',
     TestCcdGwSecret: process.env.MICROSERVICE_CCD_GW || '',
     oneTimePassword: process.env.ONE_TIME_PASSWORD || '',
 };

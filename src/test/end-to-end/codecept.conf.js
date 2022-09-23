985790c73127106fa833963ace4b2e12ac05670c
Pettedson John <pettedson.john@hmcts.net>
2022-09-23 13:46:45 +0100

    Adding some more to the wait timeout config

--- src/test/end-to-end/codecept.conf.js
+++ src/test/end-to-end/codecept.conf.js
@@ -8,7 +8,7 @@ exports.config = {
             url: config.TestUrl,
             waitForTimeout: 40000,
             getPageTimeout: 40000,
-            waitForAction: 500,
+            waitForAction: 750,
             show: config.TestShowBrowserWindow,
             waitForNavigation: ['domcontentloaded'],
             restart: true,

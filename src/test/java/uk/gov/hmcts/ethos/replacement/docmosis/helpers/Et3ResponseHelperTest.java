61c19204477b431af338d88c88062f7f1aa1d417
JackReevies <jack@reevies.tech>
2022-10-17 18:00:39 +0100

    RET-0000: Using UTF-8 and enforcing Unix line endings to fix tests on windows

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/Et3ResponseHelperTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/Et3ResponseHelperTest.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -146,8 +147,9 @@ class Et3ResponseHelperTest {
         caseData.setEt3ResponseContactPreference("Post");
         caseData.setEt3ResponsePayFrequency("Weekly");
 
+        // UTF-8 is required here for special characters to resolve on Windows correctly
         String expected = new String(Files.readAllBytes(Paths.get(Objects.requireNonNull(getClass().getClassLoader()
-            .getResource("et3ResponseDocument.json")).toURI())));
+            .getResource("et3ResponseDocument.json")).toURI())), UTF_8);
 
         String actual = Et3ResponseHelper.getDocumentRequest(caseData, "any");
         assertThat(actual).isEqualTo(expected);

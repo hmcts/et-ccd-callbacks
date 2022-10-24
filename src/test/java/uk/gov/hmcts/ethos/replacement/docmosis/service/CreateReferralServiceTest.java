8dcff19ba296b728d6f06bc41c2057b1995c3dd0
Daryl Boam <daryl.boam@version1.com>
2022-10-24 16:15:44 +0100

    2639: insert CreateReferralServiceTest

---
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CreateReferralServiceTest.java
@@ -0,0 +1,59 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.service;
+
+import java.io.IOException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.webjars.NotFoundException;
+import uk.gov.hmcts.ecm.common.exceptions.DocumentManagementException;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.DocumentInfo;
+
+@ExtendWith(SpringExtension.class)
+public class CreateReferralServiceTest {
+    private CreateReferralService createReferralService;
+
+    @MockBean
+    private TornadoService tornadoService;
+
+    private DocumentInfo documentInfo;
+
+    @BeforeEach
+    void setUp() {
+        createReferralService = new CreateReferralService(tornadoService);
+
+        documentInfo = DocumentInfo.builder()
+            .description("Referral Summary.pdf")
+            .url("https://test.com/documents/random-uuid")
+            .build();
+    }
+
+    @Test
+    void whenCreatingReferral_ReturnDocument() throws IOException {
+        when(tornadoService.generateEventDocument(any(CaseData.class), anyString(),
+            anyString(), anyString())).thenReturn(documentInfo);
+
+        DocumentInfo responseDoc = createReferralService.generateCRDocument(new CaseData(), "", "");
+        assertThat(responseDoc, is(documentInfo));
+    }
+
+    @Test
+    void whenCreatingReferralFails_ThrowException() throws IOException {
+        Throwable ioException = new IOException("test");
+
+        when(tornadoService.generateEventDocument(any(CaseData.class), anyString(),
+            anyString(), anyString())).thenThrow(ioException);
+        assertThrows(DocumentManagementException.class,
+        () -> createReferralService.generateCRDocument(new CaseData(), "",
+            ""));
+    }
+}

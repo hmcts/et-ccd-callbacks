d655d65416cfc8589f7045d2a0c70706d374f503
Daryl Boam <daryl.boam@version1.com>
2022-10-25 14:09:44 +0100

    2639: undo email removal

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CreateReferralController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CreateReferralController.java
@@ -159,7 +159,18 @@ public class CreateReferralController {
         CaseData caseData = ccdRequest.getCaseDetails().getCaseData();
         UserDetails userDetails = userService.getUserDetails(userToken);
 
-        caseData.setReferredBy(userDetails.getName());
+        emailService.sendEmail(
+            referralTemplateId,
+            caseData.getReferentEmail(),
+            ReferralHelper.buildPersonalisation(
+                ccdRequest.getCaseDetails(),
+                String.valueOf(ReferralHelper.getNextReferralNumber(caseData)),
+                true,
+                userDetails.getName()
+            )
+        );
+
+        caseData.setReferredBy(String.format("%s %s", userDetails.getFirstName(), userDetails.getLastName()));
 
         DocumentInfo documentInfo = createReferralService.generateCRDocument(caseData,
             userToken, caseData.getEcmCaseType());

0a7978116e498d0f404beb0c6c21ce0dc6149883
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2022-12-01 18:03:50 +0000

    Merge RespondentTellSomethingElseController

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/RespondentTellSomethingElseController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/RespondentTellSomethingElseController.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -17,6 +18,7 @@ import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.RespondentTellSomethingElseHelper;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.EmailService;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.RespondentTellSomethingElseService;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.UserService;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.VerifyTokenService;
 
@@ -31,6 +33,7 @@ public class RespondentTellSomethingElseController {
 
     private final VerifyTokenService verifyTokenService;
     private final EmailService emailService;
+    private final RespondentTellSomethingElseService respondentTellSomethingElseService;
     private final UserService userService;
     private String emailTemplateId;
     private static final String NO = "I do not want to copy";
@@ -49,12 +52,51 @@ public class RespondentTellSomethingElseController {
 
     public RespondentTellSomethingElseController(
         @Value("${respondent.tse.template.id}") String emailTemplateId,
-        VerifyTokenService verifyTokenService, EmailService emailService,
+        VerifyTokenService verifyTokenService,
+        RespondentTellSomethingElseService respondentTellSomethingElseService,
+        EmailService emailService,
         UserService userService) {
         this.emailTemplateId = emailTemplateId;
         this.verifyTokenService = verifyTokenService;
+        this.respondentTellSomethingElseService = respondentTellSomethingElseService;
         this.emailService = emailService;
         this.userService = userService;
+    }
+
+    /**
+     * This service Gets userToken as a parameter for security validation
+     * and ccdRequest data which has caseData as an object.
+     * @param ccdRequest        CaseData which is a generic data type for most of the
+     *                          methods which holds ET1 case data
+     * @param  userToken        Used for authorisation
+     * @return ResponseEntity   It is an HTTPEntity response which has CCDCallbackResponse that
+     *                          includes caseData which contains the upload document names of
+     *                          type "Another type of document" in a html string format.
+     */
+    @PostMapping(value = "/midVariableContent", consumes = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Respondent Tell Something Else About To Start Event")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully",
+            content = {
+                @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CCDCallbackResponse.class))
+            }),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<CCDCallbackResponse> midVariableContent(
+            @RequestBody CCDRequest ccdRequest,
+            @RequestHeader(value = "Authorization") String userToken) {
+
+        if (!verifyTokenService.verifyTokenSignature(userToken)) {
+            log.error(INVALID_TOKEN, userToken);
+            return ResponseEntity.status(FORBIDDEN.value()).build();
+        }
+
+        CaseData caseData = ccdRequest.getCaseDetails().getCaseData();
+        caseData.setResTseVariableContent(respondentTellSomethingElseService.resTseSetVariableContent(caseData));
+
+        return getCallbackRespEntityNoErrors(caseData);
     }
 
     @PostMapping(value = "/aboutToSubmit", consumes = APPLICATION_JSON_VALUE)

46ee96b415ced8b87a93ae32a94340d147bcaf3f
KirsanovI <ivan.kirsanov@hmcts.net>
2022-12-01 10:25:12 +0100

    RET-2840: Add functional tests changes

--- src/test/end-to-end/pages/IDAM/signIn.js
+++ src/test/end-to-end/pages/IDAM/signIn.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const testConfig = require('../../../config');
-
 module.exports = async function (userName, password) {
 
     const I = this;
     I.amOnPage('/', 10);
     I.waitForText('Sign in');
     I.fillField('username', userName);
+    I.fillField('password', password);
     I.click('[name="save"]');
     I.waitForText('Case list', 30);
     I.waitForClickable('.hmcts-button--secondary');

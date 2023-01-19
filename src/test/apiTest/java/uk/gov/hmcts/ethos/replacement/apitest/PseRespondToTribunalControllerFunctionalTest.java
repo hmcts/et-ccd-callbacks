645b6bc5c9ba07300c0b0a1b5cc5c25e642c4e10
fergus.oshea <fergus.oshea@version1.com>
2023-01-19 12:14:17 +0000

    RET-2879: Unit Tests for existing functionality

---
+++ src/test/apiTest/java/uk/gov/hmcts/ethos/replacement/apitest/PseRespondToTribunalControllerFunctionalTest.java
@@ -0,0 +1,77 @@
+package uk.gov.hmcts.ethos.replacement.apitest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.ethos.replacement.apitest.utils.CCDRequestBuilder;
+
+@Slf4j
+class PseRespondToTribunalControllerFunctionalTest extends BaseFunctionalTest {
+    private static final String AUTHORIZATION = "Authorization";
+
+    private static final String ABOUT_TO_START_URL = "/pseRespondToTribunal/aboutToStart";
+    private static final String MID_TABLE_DETAILS = "/pseRespondToTribunal/midDetailsTable";
+    private static final String MID_VALIDATE_INPUT = "/pseRespondToTribunal/midValidateInput";
+
+    private CCDRequest ccdRequest;
+
+    @BeforeAll
+    public void setUpCaseData() {
+        CaseData caseData = new CaseData();
+        caseData.setEthosCaseReference("testCaseReference");
+        caseData.setResTseSelectApplication("Amend response");
+
+        ccdRequest = CCDRequestBuilder.builder()
+            .withCaseData(caseData)
+            .withCaseId("123")
+            .build();
+    }
+
+    @Test
+    void shouldReceiveSuccessResponseWhenAboutToStartInvoked() {
+        RestAssured.given()
+            .spec(spec)
+            .contentType(ContentType.JSON)
+            .header(new Header(AUTHORIZATION, userToken))
+            .body(ccdRequest)
+            .post(ABOUT_TO_START_URL)
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .log()
+            .all(true);
+    }
+
+    @Test
+    void shouldReceiveSuccessResponseWhenMidDetailsTableInvoked() {
+        RestAssured.given()
+            .spec(spec)
+            .contentType(ContentType.JSON)
+            .header(new Header(AUTHORIZATION, userToken))
+            .body(ccdRequest)
+            .post(MID_TABLE_DETAILS)
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .log()
+            .all(true);
+    }
+
+    @Test
+    void shouldReceiveSuccessResponseWhenMidValidateInputInvoked() {
+        RestAssured.given()
+            .spec(spec)
+            .contentType(ContentType.JSON)
+            .header(new Header(AUTHORIZATION, userToken))
+            .body(ccdRequest)
+            .post(MID_VALIDATE_INPUT)
+            .then()
+            .statusCode(HttpStatus.SC_OK)
+            .log()
+            .all(true);
+    }
+}

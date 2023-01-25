b7f3c4e71064233710076aaa63eaf5668d6e7eb9
SharifChoudhury1 <114483455+SharifChoudhury1@users.noreply.github.com>
2023-01-25 15:02:56 +0000

    verifyLetters
    verified letters in correspondence

--- src/test/end-to-end/pages/generateCorrespondence/generateCorrespondence.js
+++ src/test/end-to-end/pages/generateCorrespondence/generateCorrespondence.js
@@ -6,9 +6,9 @@ module.exports = async function () {
 
     const I = this;
     I.waitForText(commonConfig.lettersCorrespondence, testConfig.TestTimeToWaitForText);
-    await I.selectOption('#correspondenceType_topLevel_Documents', commonConfig.lettersCorrespondence);
+    I.selectOption('#correspondenceType_topLevel_Documents', commonConfig.lettersCorrespondence);
     I.waitForText(commonConfig.lettersCorrespondence1, testConfig.TestTimeToWaitForText);
-    await I.selectOption('#correspondenceType_part_2_Documents', commonConfig.lettersCorrespondence1);
-    await I.navByClick(commonConfig.continue);
-    await I.click(commonConfig.submit)
+    I.selectOption('#correspondenceType_part_2_Documents', commonConfig.lettersCorrespondence1);
+    I.navByClick(commonConfig.continue);
+    I.click(commonConfig.submit)
 };

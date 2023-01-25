20a565db58747652ad358bc80ed4d5747c198039
Pettedson John <pettedson.john@hmcts.net>
2023-01-25 14:11:49 +0000

    Retagging the validateHearingListedErrorMsgInWeekend to nightly as it is working now...

--- src/test/end-to-end/paths/validateHearingListedErrorMsgInWeekend.js
+++ src/test/end-to-end/paths/validateHearingListedErrorMsgInWeekend.js
@@ -11,4 +11,4 @@ Scenario('Validate hearing error message if user schedules the hearing date duri
     console.log("... case id =>" +caseId);
     await scheduleHearingDuringTheWeekend(I, eventNames.LIST_HEARING, 'Leeds');
 
-}).tag('@RET-BAT').tag('@nightly').retry(testConfig.TestRetryScenarios);
+}).tag('@nightly').retry(testConfig.TestRetryScenarios);

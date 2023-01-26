5a893edb23a53856120d62be361592e2c2ffdd00
SharifChoudhury1 <114483455+SharifChoudhury1@users.noreply.github.com>
2023-01-26 11:01:17 +0000

    Updated text for automation security
    Text was automated for clarity

--- src/test/end-to-end/pages/generateCorrespondence/generateCorrespondence.js
+++ src/test/end-to-end/pages/generateCorrespondence/generateCorrespondence.js
@@ -5,10 +5,13 @@ const testConfig = require("../../../config");
 module.exports = async function () {
 
     const I = this;
+    I.see('List of correspondence items');
+    I.see('Top Level');
     I.waitForText(commonConfig.lettersCorrespondence, testConfig.TestTimeToWaitForText);
     I.selectOption('#correspondenceType_topLevel_Documents', commonConfig.lettersCorrespondence);
     I.waitForText(commonConfig.lettersCorrespondence1, testConfig.TestTimeToWaitForText);
     I.selectOption('#correspondenceType_part_2_Documents', commonConfig.lettersCorrespondence1);
+    I.see('Letters');
     I.click(commonConfig.continue);
     I.click(commonConfig.submit)
 };

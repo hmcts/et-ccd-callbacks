474fdecbc39e768d4ccfc23e13a7a0a2310f66b9
Pettedson John <pettedson.john@hmcts.net>
2023-01-31 11:59:23 +0000

    Working Respondent Representative Changes....

--- src/test/end-to-end/pages/amendRespondentRepresentative/amendRespondentRepresentative.js
+++ src/test/end-to-end/pages/amendRespondentRepresentative/amendRespondentRepresentative.js
@@ -3,17 +3,126 @@ const commonConfig = require('../../data/commonConfig.json');
 const testConfig = require("../../../config");
 const amendRespondentRepresentative = require("./amendResponsdentRepresentative.json");
 
-module.exports = async function () {
+module.exports = async function (myHMMCTSOrganisation = false) {
 
     const I = this;
-    await I.selectOption('#repCollection_0_dynamic_resp_rep_name', commonConfig.respondentName);
-    await I.fillField('#repCollection_0_name_of_representative', commonConfig.respondentRepresentativeName);
-    await I.checkOption(amendRespondentRepresentative.does_the_representative_have_an_account_no);
-    I.wait(1);
-    I.fillField(amendRespondentRepresentative.representative_work_address_enter_a_postcode,'YO18 7LT');
-    I.click(amendRespondentRepresentative.find_address_button);
-    I.wait(2);
-    I.selectOption(amendRespondentRepresentative.representative_work_address_select_an_address,'1: Object');
-    await I.click(commonConfig.continue);
-    await I.click(commonConfig.submit);
+    I.see('Respondent Representative');
+    I.see('Case Number:');
+    I.see('Respondent Representative(s)');
+    I.see('Respondent who is being represented');
+    I.see('Name of representative');
+    I.see('Phone number (Optional)');
+    I.see('Alternative number (Optional)');
+    I.see('Email address (Optional)');
+    I.see('Contact preference (Optional)');
+    I.see('Does the representative have a MyHMCTS account? (Optional)');
+
+    I.selectOption('#repCollection_0_dynamic_resp_rep_name', commonConfig.respondentName);
+    I.fillField('#repCollection_0_name_of_representative', commonConfig.respondentRepresentativeName);
+    I.fillField('#repCollection_0_representative_phone_number', '01234567890');
+    I.fillField('#repCollection_0_representative_mobile_number', '01234657890');
+    I.fillField('#repCollection_0_representative_email_address', 'test.xxx@hmcts.net');
+    I.selectOption('#repCollection_0_representative_preference', '1: Email');
+    if (!myHMMCTSOrganisation) {
+        I.checkOption('#repCollection_0_myHmctsYesNo_No');
+        I.see('Name of Organisation (Optional)');
+        I.see('Reference (Optional)');
+        I.see('Occupation (Optional)');
+        I.see('Address')
+        I.see('Enter a UK postcode');
+        I.fillField('#repCollection_0_name_of_organisation', 'Tester Organisation');
+        I.fillField('#repCollection_0_representative_reference', 'Tester Reference');
+        I.selectOption('ccd-field-write:nth-of-type(11) .form-control', 'Solicitor');
+
+        I.fillField('#repCollection_0_representative_address_representative_address_postcodeInput', 'BR1 4NN');
+        I.click('Find address');
+        I.wait(15);
+        //I.waitForEnabled('[name='address']', 60);
+        //I.selectOption('[name=\'address\']', '55: Object');
+        I.selectOption('[name=\'address\']', '15 Arcus Road, Bromley');
+        I.waitForText('Postcode', testConfig.TestTimeToWaitForText);
+        I.see('Building and Street');
+        I.see('Address Line 2 (Optional)');
+        I.see('Address Line 3 (Optional)');
+        I.see('Town or City (Optional)');
+        I.see('County (Optional)');
+        I.see('Country (Optional)');
+        I.seeInField('#repCollection_0_representative_address__detailAddressLine1', '15 Arcus Road');
+        I.seeInField('#repCollection_0_representative_address__detailPostTown', 'Bromley');
+        I.seeInField('#repCollection_0_representative_address__detailCountry', 'United Kingdom');
+        I.seeInField('#repCollection_0_representative_address__detailPostCode', 'BR1 4NN');
+        I.click(commonConfig.continue);
+
+        I.see('Respondent Representative');
+        I.see('Case Number:');
+        I.click(commonConfig.submit);
+
+        I.waitForNavigation();
+        I.see('has been updated with event: Respondent Representative');
+    }
+
+    //Click of the Respondent Representative is not working....So Further verification Code is not working...
+    /*pause();
+    I.clickLink('//div[contains(text(),\'Respondent Representative\')]');
+    I.waitForText('Respondent Representative(s)', testConfig.TestTimeToWaitForText);
+    I.see('Respondent Representative(s) 1');
+    I.see('Respondent who is being represented');
+    I.see('Mrs Test Auto');
+    I.see('Name of Representative');
+    I.see('ECMGita');
+    I.see('Does the representative have a MyHMCTS account?');
+    I.see('No');
+    I.see('Address');
+    I.see('Building and Street');
+    I.see('15 Arcus Road');
+    I.see('Town or City');
+    I.see('Postcode/Zipcode');
+    I.see('BR1 4NN');
+    I.see('Country');
+    I.see('United Kingdom');
+    I.see('Phone number');
+    I.see('01234567890');
+    I.see('Alternative number');
+    I.see('01234657890');
+    I.see('test.xxx@hmcts.net');
+    I.see('Contact preference');
+    I.see('Email');
+*/
+
+    /* Code that is build but has to be tested for the Yes Option....
+    I.checkOption('#repCollection_0_myHmctsYesNo_Yes');
+    I.waitForText('Search for an organisation',testConfig.TestTimeToWaitForText);
+    I.fillField('#search-org-text', 'ET Organisation 1');
+    I.waitForElement('//a[contains(text(),\'Select\')]');
+    I.click('//a[contains(text(),\'Select\')]');
+    I.click('#content-why-can-not-find-organisation');
+    I.see('If you know that the solicitor is already registered with MyHMCTS, check that you have entered their details correctly. Remember that organisations can only register one office address. This means that the details could be slightly different from what you\'re expecting. Contact the solicitor directly if you have any concerns.');
+    I.click(commonConfig.continue);
+
+    I.see('Respondent Representative');
+    I.see('Case Number:');
+    I.waitForElement(commonConfig.submit);
+    I.click(commonConfig.submit);
+
+    I.waitForNavigation();
+    I.click('[tabindex=\'0\'] > .mat-tab-label-content');
+    I.see('Respondent Representative(s)');
+    I.see('Respondent Representative(s) 1');
+    I.see('Respondent who is being represented');
+    I.see('Mrs Test Auto');
+    I.see('Name of Representative');
+    I.see('ECMGita');
+    I.see('Does the representative have a MyHMCTS account?');
+    I.see('Yes');
+    I.see('MyHMCTS Organisation');
+    I.see('Name:');
+    I.see('ET Organisation 1');
+    I.see('Address:');
+    I.see('15 Arcus Road');
+    I.see('Bromley');
+    I.see('BR1 4NN');
+    I.see('Email address');
+    I.see('test.xxx@hmcts.net');
+    I.see('Contact preference');
+    I.see('Email');*/
 };

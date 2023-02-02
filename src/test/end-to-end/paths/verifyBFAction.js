ce39eaf3381b4d7c76078bd8d96b2037e44a8ea9
SharifChoudhury1 <114483455+SharifChoudhury1@users.noreply.github.com>
2023-02-02 09:43:44 +0000

    test commit

--- src/test/end-to-end/paths/verifyBFAction.js
+++ src/test/end-to-end/paths/verifyBFAction.js
@@ -12,9 +12,9 @@ Scenario('Verify B/F Action', async ({I}) => {
     console.log("... case id =>" +caseId);
     //await I.authenticateWithIdam(testConfig.TestEnvCWUser, testConfig.TestEnvCWPassword);
     //I.refreshPage();
-    //I.wait(commonConfig.time_interval_30_second);
+    //I.wait(commonConfig.time_interval_5_second);
 
     await bfAction(I, eventNames.BF_ACTION);
 
 }).tag('RET-BAT').tag('@nightly').retry(testConfig.TestRetryScenarios);
-//.tag('@sc1')
+

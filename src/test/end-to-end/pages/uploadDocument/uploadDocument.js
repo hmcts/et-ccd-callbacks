8242a08a1caf852c228f517cf0644e9ab5853ff2
SharifChoudhury1 <114483455+SharifChoudhury1@users.noreply.github.com>
2023-02-03 19:01:22 +0000

    Automated Upload Documents with Refactoring
    Automated Upload Documents with Refactoring

--- src/test/end-to-end/pages/uploadDocument/uploadDocument.js
+++ src/test/end-to-end/pages/uploadDocument/uploadDocument.js
@@ -4,14 +4,29 @@ const commonConfig = require('../../data/commonConfig.json');
 module.exports = async function () {
 
     const I = this;
-    await I.see('Upload Document');
-    await I.click(commonConfig.addNewButton);
-    await I.attachFile('#documentCollection_0_uploadedDocument', 'data/fileUpload.txt');
-    await I.wait(5);
-    await I.fillField('#documentCollection_0_shortDescription', commonConfig.shortDescription);
-    await I.click(commonConfig.continue);
-    await I.click(commonConfig.submit);
-    await I.wait(5);
-    await I.click("//div[text()='Documents']");
-    await I.see('fileUpload.txt');
+    I.see('Upload Document');
+    I.see('Case Number:');
+    I.see('Case documentation (Optional)');
+    I.see('Add new');
+    I.see('Upload documentation for the case');
+
+    I.click("//button[@class='button write-collection-add-item__top']");
+    I.see('Case documentation');
+    I.see('Type of Document (Optional)'); 
+    I.attachFile('#documentCollection_0_uploadedDocument', 'data/fileUpload.txt');
+    I.wait(5);
+    I.see('Short Description (Optional)');
+    I.fillField('#documentCollection_0_shortDescription', commonConfig.shortDescription);
+
+    I.click(commonConfig.continue);
+    I.click("//button[@class='button']");
+    I.seeElement("//div[@class='alert-message']");
+    I.wait(5);
+    
+    I.click("//div[text()='Documents']");
+    I.see('Case documentation 1');
+    I.see('Document');
+    I.see('Short Description');
+    I.see('fileUpload.txt');
+    I.see('Upload Document Check');
 };

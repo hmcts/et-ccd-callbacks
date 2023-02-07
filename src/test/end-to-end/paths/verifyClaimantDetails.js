a19ba903d5ed0a32a7fd3544789ee4d8873a9c40
Pettedson John <pettedson.john@hmcts.net>
2023-02-07 19:04:13 +0000

    Removed the unwanted pats tag for the Claimant Details...

--- src/test/end-to-end/paths/verifyClaimantDetails.js
+++ src/test/end-to-end/paths/verifyClaimantDetails.js
@@ -9,8 +9,6 @@ Scenario('Verify Claimant Details', async ({I}) => {
 
     let caseNumber = await processCaseToAcceptedState();
     console.log("... case id =>" + caseNumber);
-
     await claimantDetails(I, eventNames.CLAIMANT_DETAILS);
 
-}).tag('@nightly').tag('@RET-BAT').tag('@pats');
-    //.retry(testConfig.TestRetryScenarios);
+}).tag('@nightly').tag('@RET-BAT').retry(testConfig.TestRetryScenarios);

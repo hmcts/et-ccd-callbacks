d1b7ca7730904e714a74630296f49bcc449c15ab
Brendan McHugh <brendan.mchugh@hmcts.net>
2023-02-15 12:04:31 +0000

    fix checkstyle

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/TseViewApplicationsController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/TseViewApplicationsController.java
@@ -43,8 +43,9 @@ public class TseViewApplicationsController {
 
 
     /**
-     *  Populates the dynamic list of the applications open or closed on a case.
+     * Populates the dynamic list of the applications open or closed on a case.
      * Called after 'view an application' is clicked and open or closed has been selected.
+     *
      * @param ccdRequest holds the request and case data
      * @param userToken  used for authorization
      * @return Callback response entity with case data attached.
@@ -57,19 +58,19 @@ public class TseViewApplicationsController {
             content = {
                 @Content(mediaType = "application/json",
                     schema = @Schema(implementation = CCDCallbackResponse.class))
-            }),
+                }),
         @ApiResponse(responseCode = "400", description = "Bad Request"),
         @ApiResponse(responseCode = "500", description = "Internal Server Error")
     })
     public ResponseEntity<CCDCallbackResponse> populateChooseApplication(
 
-        @RequestBody CCDRequest ccdRequest,
-        @RequestHeader(value = "Authorization") String userToken) {
+            @RequestBody CCDRequest ccdRequest,
+            @RequestHeader(value = "Authorization") String userToken) {
 
         if (!verifyTokenService.verifyTokenSignature(userToken)) {
-             log.error(INVALID_TOKEN, userToken);
-             return ResponseEntity.status(FORBIDDEN.value()).build();
-         }
+            log.error(INVALID_TOKEN, userToken);
+            return ResponseEntity.status(FORBIDDEN.value()).build();
+        }
 
         CaseData caseData = ccdRequest.getCaseDetails().getCaseData();
         caseData.setTseViewApplicationSelect(TseHelper.populateOpenOrClosedApplications(caseData));

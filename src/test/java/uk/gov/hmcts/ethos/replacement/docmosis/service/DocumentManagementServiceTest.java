34fb3e1ca5a5f788be13bd6a4cc3f0b20ef007f5
btensay <tensay.bulcha@gmail.com>
2023-02-16 13:45:19 +0000

    ET CDAM file download case removed

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DocumentManagementServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DocumentManagementServiceTest.java
@@ -146,8 +146,8 @@ public class DocumentManagementServiceTest {
     @Test
     public void downloadFileSecureDocStoreTrue() {
         ReflectionTestUtils.setField(documentManagementService, "secureDocStoreEnabled", true);
-        when(caseDocumentClient.getDocumentBinary(anyString(), anyString(), anyString()))
-                .thenReturn(responseEntity);
+        when(documentDownloadClientApi.downloadBinary(anyString(), anyString(), anyString(), anyString(), anyString()))
+            .thenReturn(responseEntity);
         UploadedDocument uploadedDocument = documentManagementService.downloadFile("authString",
                 "http://dm-store:8080/documents/85d97996-22a5-40d7-882e-3a382c8ae1b4/binary");
         assertEquals("fileName", uploadedDocument.getName());

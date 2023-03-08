facfe6b36c4de66524eca10f79834ebbd4171260
fergus.oshea <fergus.oshea@version1.com>
2023-03-08 14:56:38 +0000

    RET-3314: Add FT test and CheckStyle issues

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/TseAdminController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/TseAdminController.java
@@ -247,13 +247,13 @@ public class TseAdminController {
     @PostMapping(value = "/submittedCloseApplication", consumes = APPLICATION_JSON_VALUE)
     @Operation(summary = "Populates data for the submitted page")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Accessed successfully",
-                    content = {
-                            @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = CCDCallbackResponse.class))
-                    }),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+        @ApiResponse(responseCode = "200", description = "Accessed successfully",
+            content = {
+                @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CCDCallbackResponse.class))
+            }),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
     })
     public ResponseEntity<CCDCallbackResponse> submittedCloseApplication(
             @RequestBody CCDRequest ccdRequest,

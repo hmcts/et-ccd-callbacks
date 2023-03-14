f3cfc0ff10eaee92b2f7d6255b1b65030f158b92
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2023-03-14 16:14:32 +0000

    Update sendTribunalEmail

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/PseRespondToTribunalService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/PseRespondToTribunalService.java
@@ -28,6 +28,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.BOTH_PARTIES;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.NO;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.RESPONDENT_ONLY;
@@ -235,8 +236,12 @@ public class PseRespondToTribunalService {
      * @param caseDetails in which the case details are extracted from
      */
     public void sendTribunalEmail(CaseDetails caseDetails) {
+        String adminEmail = getAdminEmail(caseDetails);
+        if (isNullOrEmpty(adminEmail)) {
+            return;
+        }
         emailService.sendEmail(notificationToAdminTemplateId,
-            getAdminEmail(caseDetails),
+            adminEmail,
             buildPersonalisationAdmin(caseDetails));
     }
 

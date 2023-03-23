8f8476d3614314cf43d4b5b22ea4f7bb5e3e32e4
fergus.oshea <fergus.oshea@version1.com>
2023-03-23 14:03:17 +0000

    RET-3120: Add new common class.

---
+++ src/test/utils/java/uk/gov/hmcts/ethos/utils/CCDRequestBuilder.java
@@ -0,0 +1,48 @@
+package uk.gov.hmcts.ethos.utils;
+
+import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
+
+public class CCDRequestBuilder {
+
+    private CaseData caseData = new CaseData();
+    private String state;
+    private String caseTypeId;
+    private String caseId;
+
+    public static CCDRequestBuilder builder() {
+        return new CCDRequestBuilder();
+    }
+
+    public CCDRequestBuilder withCaseData(CaseData caseData) {
+        this.caseData = caseData;
+        return this;
+    }
+
+    public CCDRequestBuilder withState(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public CCDRequestBuilder withCaseId(String id) {
+        this.caseId = id;
+        return this;
+    }
+
+    public CCDRequestBuilder withCaseTypeId(String caseTypeId) {
+        this.caseTypeId = caseTypeId;
+        return this;
+    }
+
+    public CCDRequest build() {
+        CaseDetails caseDetails = new CaseDetails();
+        caseDetails.setState(state);
+        caseDetails.setCaseTypeId(caseTypeId);
+        caseDetails.setCaseData(caseData);
+        caseDetails.setCaseId(caseId);
+        CCDRequest ccdRequest = new CCDRequest();
+        ccdRequest.setCaseDetails(caseDetails);
+        return ccdRequest;
+    }
+}

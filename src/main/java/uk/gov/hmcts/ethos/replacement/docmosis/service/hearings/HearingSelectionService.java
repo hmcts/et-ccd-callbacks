14300c2a0936c12b66d88b8d0a68ad538866754a
gauravchawla <gaurav-chawla@hotmail.com>
2023-03-23 18:17:41 +0000

    revert allocate hearing changes

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/hearings/HearingSelectionService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/hearings/HearingSelectionService.java
@@ -2,11 +2,13 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service.hearings;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.ecm.common.helpers.UtilHelper;
 import uk.gov.hmcts.et.common.model.bulk.types.DynamicFixedListType;
 import uk.gov.hmcts.et.common.model.bulk.types.DynamicValueType;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.items.DateListedTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.HearingTypeItem;
+import uk.gov.hmcts.et.common.model.ccd.types.DateListedType;
 import uk.gov.hmcts.et.common.model.ccd.types.HearingType;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +26,36 @@ public class HearingSelectionService {
             }
         }
         return values;
+    }
+
+    public List<DynamicValueType> getHearingSelectionAllocateHearing(CaseData caseData) {
+        List<DynamicValueType> values = new ArrayList<>();
+
+        if (CollectionUtils.isNotEmpty(caseData.getHearingCollection())) {
+            for (HearingTypeItem hearing : caseData.getHearingCollection()) {
+                for (DateListedTypeItem listing : hearing.getValue().getHearingDateCollection()) {
+                    String code = listing.getId();
+                    String date = UtilHelper.formatLocalDateTime(listing.getValue().getListedDate());
+                    String label = String.format("Hearing %s, %s", hearing.getValue().getHearingNumber(), date);
+                    values.add(DynamicValueType.create(code, label));
+                }
+            }
+        }
+        return values;
+    }
+
+    public HearingType getSelectedHearingAllocateHearing(CaseData caseData,
+                                                         DynamicFixedListType dynamicFixedListType) {
+        String id = dynamicFixedListType.getValue().getCode();
+        for (HearingTypeItem hearing : caseData.getHearingCollection()) {
+            for (DateListedTypeItem listing : hearing.getValue().getHearingDateCollection()) {
+                if (listing.getId().equals(id)) {
+                    return hearing.getValue();
+                }
+            }
+        }
+        throw new IllegalStateException(String.format("Selected hearing %s not found in case %s",
+                dynamicFixedListType.getValue().getLabel(), caseData.getEthosCaseReference()));
     }
 
     public HearingType getSelectedHearing(CaseData caseData, DynamicFixedListType dynamicFixedListType) {
@@ -45,6 +77,19 @@ public class HearingSelectionService {
             }
         }
         throw new IllegalStateException(String.format("Listings %s not found for selected hearing in case %s",
+                dynamicFixedListType.getValue().getLabel(), caseData.getEthosCaseReference()));
+    }
+
+    public DateListedType getSelectedListing(CaseData caseData, DynamicFixedListType dynamicFixedListType) {
+        String id = dynamicFixedListType.getValue().getCode();
+        for (HearingTypeItem hearing : caseData.getHearingCollection()) {
+            for (DateListedTypeItem listing : hearing.getValue().getHearingDateCollection()) {
+                if (listing.getId().equals(id)) {
+                    return listing.getValue();
+                }
+            }
+        }
+        throw new IllegalStateException(String.format("Selected listing %s not found in case %s",
                 dynamicFixedListType.getValue().getLabel(), caseData.getEthosCaseReference()));
     }
 }

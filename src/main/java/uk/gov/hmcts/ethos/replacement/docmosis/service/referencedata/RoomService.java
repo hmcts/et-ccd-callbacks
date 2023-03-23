1861d6a027ad74f94ed6c8c4d1ececb9085319f1
gauravchawla <gaurav-chawla@hotmail.com>
2023-03-23 17:56:29 +0000

    revert allocate hearing changes

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/referencedata/RoomService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/referencedata/RoomService.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.service.referencedata;
 
 import uk.gov.hmcts.et.common.model.bulk.types.DynamicValueType;
+
 import java.util.List;
 
 public interface RoomService {

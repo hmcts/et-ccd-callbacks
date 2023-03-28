4e6c8e700617be9e07da7a7bb30477a9ed852ce8
Pettedson John <pettedson.john@hmcts.net>
2023-03-28 10:06:31 +0100

    Enabling the Jurisdiction Tests...

--- src/test/end-to-end/paths/verifyJurisdiction.js
+++ src/test/end-to-end/paths/verifyJurisdiction.js
@@ -12,5 +12,4 @@ Scenario('Verify Jurisdiction', async ({I}) => {
     await jurisdiction(I, eventNames.JURISDICTION, "Not allocated");
     await jurisdiction(I, eventNames.JURISDICTION, "Withdrawn or private settlement");
 
-}).tag('@pats');
-    //.retry(testConfig.TestRetryScenarios);
+}).tag('@nightly').tag('@RET-BAT').retry(testConfig.TestRetryScenarios);

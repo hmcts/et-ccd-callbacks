5f10427705c2aa7a25850bc26df3ca11461a84ed
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-05-15 16:20:23 +0100

    PMD

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/Et3ResponseHelper.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/Et3ResponseHelper.java
@@ -40,7 +40,7 @@ import static uk.gov.hmcts.ecm.common.model.helper.Constants.YES;
 @Slf4j
 @SuppressWarnings({"PMD.ClassWithOnlyPrivateConstructorsShouldBeFinal", "PMD.LinguisticNaming",
     "PMD.ExcessiveMethodLength", "PMD.ClassNamingConventions", "PMD.PrematureDeclaration", "PMD.GodClass",
-    "PMD.CyclomaticComplexity", "PMD.TooManyMethods", "PMD.ExcessiveImports"})
+    "PMD.CyclomaticComplexity", "PMD.TooManyMethods", "PMD.ExcessiveImports", "PMD.SwitchStmtsShouldHaveDefault"})
 public class Et3ResponseHelper {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();

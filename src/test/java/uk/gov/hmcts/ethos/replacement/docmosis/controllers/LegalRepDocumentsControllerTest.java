2fdbef17f410e3b7c1e0a5f2ed8b362aec52692b
Itay-Kirsh <Itayk747@gmail.com>
2023-05-15 14:33:10 +0100

    Fix compilation error

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/LegalRepDocumentsControllerTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/LegalRepDocumentsControllerTest.java
@@ -12,9 +12,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
 import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.VerifyTokenService;
-import uk.gov.hmcts.ethos.replacement.docmosis.utils.CCDRequestBuilder;
-import uk.gov.hmcts.ethos.replacement.docmosis.utils.CaseDataBuilder;
 import uk.gov.hmcts.ethos.replacement.docmosis.utils.JsonMapper;
+import uk.gov.hmcts.ethos.utils.CCDRequestBuilder;
+import uk.gov.hmcts.ethos.utils.CaseDataBuilder;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;

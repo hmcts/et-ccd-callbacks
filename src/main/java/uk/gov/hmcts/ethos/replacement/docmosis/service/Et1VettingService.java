1a48e7861da6c1e7d3ed8918bf77bfde18d44e8f
GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
2023-05-17 10:46:36 +0100

    Ret 3501 (#928)

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/Et1VettingService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/Et1VettingService.java
@@ -17,6 +17,7 @@ import uk.gov.hmcts.et.common.model.ccd.items.DocumentTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.JurCodesTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.RespondentSumTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.VettingJurCodesTypeItem;
+import uk.gov.hmcts.et.common.model.ccd.types.ClaimantRequestType;
 import uk.gov.hmcts.et.common.model.ccd.types.JurCodesType;
 import uk.gov.hmcts.et.common.model.ccd.types.RespondentSumType;
 import uk.gov.hmcts.et.common.model.ccd.types.VettingJurisdictionCodesType;
@@ -50,12 +51,13 @@ public class Et1VettingService {
 
     private static final String ET1_DOC_TYPE = "ET1";
     private static final String ACAS_DOC_TYPE = "ACAS Certificate";
-    private static final String BEFORE_LABEL_TEMPLATE = "Open these documents to help you complete this form: %s%s"
-            + "<br>Check the Documents tab for additional ET1 documents the claimant may have uploaded.";
+    private static final String BEFORE_LABEL_TEMPLATE = "Open these documents to help you complete this form: %s%s%s";
     private static final String BEFORE_LABEL_ET1 =
             "<br><a target=\"_blank\" href=\"%s\">ET1 form (opens in new tab)</a>";
     private static final String BEFORE_LABEL_ACAS =
             "<br><a target=\"_blank\" href=\"%s\">Acas certificate %s (opens in new tab)</a>";
+    private static final String BEFORE_LABEL_ET1_ATTACHMENT =
+            "<br><a target=\"_blank\" href=\"%s\">%s (opens in new tab)</a>";
     private static final String BEFORE_LABEL_ACAS_OPEN_TAB =
             "<br><a target=\"_blank\" href=\"/cases/case-details/%s#Documents\">"
                     + "Open the Documents tab to view/open Acas certificates (opens in new tab)</a>";
@@ -170,9 +172,11 @@ public class Et1VettingService {
 
         String et1Display = "";
         String acasDisplay = "";
+        String et1Attachment = "";
         IntWrapper acasCount = new IntWrapper(0);
 
         List<DocumentTypeItem> documentCollection = caseDetails.getCaseData().getDocumentCollection();
+        ClaimantRequestType claimantRequestType = caseDetails.getCaseData().getClaimantRequests();
         if (documentCollection != null) {
             et1Display = documentCollection
                     .stream()
@@ -186,12 +190,17 @@ public class Et1VettingService {
                             BEFORE_LABEL_ACAS, createDocLinkBinary(d), acasCount.incrementAndReturnValue()))
                     .collect(Collectors.joining());
         }
+        if (claimantRequestType != null && claimantRequestType.getClaimDescriptionDocument() != null) {
+            et1Attachment = String.format(BEFORE_LABEL_ET1_ATTACHMENT,
+                    claimantRequestType.getClaimDescriptionDocument().getDocumentBinaryUrl(),
+                    claimantRequestType.getClaimDescriptionDocument().getDocumentFilename());
+        }
 
         if (acasCount.getValue() > FIVE_ACAS_DOC_TYPE_ITEMS_COUNT) {
             acasDisplay = String.format(BEFORE_LABEL_ACAS_OPEN_TAB, caseDetails.getCaseId());
         }
 
-        return String.format(BEFORE_LABEL_TEMPLATE, et1Display, acasDisplay);
+        return String.format(BEFORE_LABEL_TEMPLATE, et1Display, acasDisplay, et1Attachment);
     }
 
     private String createDocLinkBinary(DocumentTypeItem documentTypeItem) {

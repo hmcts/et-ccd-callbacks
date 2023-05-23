d528d34ed7c05c04caaea999eaa5e106b10195c8
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-05-23 12:04:29 +0100

    Functional test

--- src/test/end-to-end/paths/verifyCreateReport.js
+++ src/test/end-to-end/paths/verifyCreateReport.js
@@ -2,15 +2,15 @@ const testConfig = require('./../../config');
 const {generateReport} = require("../helpers/caseHelper");
 const commonConfig = require('../data/commonConfig.json');
 const {eventNames} = require('../pages/common/constants.js');
+const caseListLink = '[href="/cases"]';
 
 Feature('Create Report... ');
 
 Scenario('Generate a Report', async ({I}) => {
     await I.authenticateWithIdam(testConfig.TestEnvCWUser, testConfig.TestEnvCWPassword);
     I.wait(commonConfig.time_interval_1_second);
-    caseListLink: '[href="/cases"]',
-    I.see(this.caseListText);
-    I.waitForElement(this.caseListText, 30);
+    I.waitForElement(this.caseListLink, 30);
+    I.click(this.caseListLink);
     await generateReport(I, commonConfig.jurisdictionType, commonConfig.caseType, eventNames.CREATE_REPORT);
     
 }).tag('@RET-BAT').tag('@nightly').retry(testConfig.TestRetryScenarios);

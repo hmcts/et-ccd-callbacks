568ef97deca2285aa06afde999a88a009c9b6577
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-06-01 15:20:02 +0100

    Merge conflicts

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/TornadoService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/TornadoService.java
@@ -294,21 +294,24 @@ public class TornadoService {
             throw new NullPointerException("Document name cannot be null or empty");
         }
         switch (documentName) {
-            case "ET1 Vetting.pdf":
+            case "ET1 Vetting.pdf" -> {
                 return Et1VettingHelper.getDocumentRequest(caseData, tornadoConnection.getAccessKey());
-            case "ET3 Processing.pdf":
+            }
+            case "ET3 Processing.pdf" -> {
                 return Et3VettingHelper.getDocumentRequest(caseData, tornadoConnection.getAccessKey());
-            case "ET3 Response.pdf":
-                dmStoreDocumentName = String.format("%s - ET3 Response.pdf",
-                    caseData.getSubmitEt3Respondent().getSelectedLabel());
+            }
+            case ET3_RESPONSE_PDF -> {
+                caseData.getSubmitEt3Respondent().getSelectedLabel();
                 return Et3ResponseHelper.getDocumentRequest(caseData, tornadoConnection.getAccessKey());
-            case "Initial Consideration.pdf":
+            }
+            case "Initial Consideration.pdf" -> {
                 return InitialConsiderationHelper.getDocumentRequest(
                         caseData, tornadoConnection.getAccessKey(), caseTypeId);
-            case "Referral Summary.pdf":
+            }
+            case "Referral Summary.pdf" -> {
                 return ReferralHelper.getDocumentRequest(caseData, tornadoConnection.getAccessKey());
-            default:
-                throw new IllegalArgumentException("Unexpected document name " + documentName);
+            }
+            default -> throw new IllegalArgumentException("Unexpected document name " + documentName);
         }
     }
 }

352bdb8cd7e5cd4f10b36f80108245d5baff97a6
Sunday Ayeni <sunnyayeni18@gmail.com>
2023-06-26 20:15:42 +0100

    revert previous changes

--- src/test/end-to-end/saucelabs.conf.js
+++ src/test/end-to-end/saucelabs.conf.js
@@ -1,58 +1,116 @@
-const testConfig = require('./config.js');
-exports.config = {
-  tests: './paths/*.js',
-  output: `${process.cwd()}/${testConfig.TestOutputDir}`,
-  helpers: {
-    WebDriverIO: {
-      host: 'ondemand.saucelabs.com',
-      port: 443,
-      protocol: 'https',
-      url: testConfig.TestUrl,
-      show: false,
-      user: process.env.SAUCE_USERNAME || testConfig.saucelabs.username,
-      key: process.env.SAUCE_ACCESS_KEY || testConfig.saucelabs.key,
-      tunnelIdentifier: process.env.TUNNEL_IDENTIFIER || testConfig.saucelabs.tunnelId,
-      acceptSslCerts: true,
-      tags: ['ET'],
-      desiredCapabilities: {
-        // Common capabilities for all browsers
-        build: process.env.SAUCE_BUILD_NAME || 'ET-CCD-X-Browser', // Set a build name in the environment variable
-        platform: 'any',
-        screenResolution: '1280x1024',
-        name: 'ET ccd-callbacks Crossbrowser Tests',
-      },
-    },
-  },
-  plugins: {
-    wdio: {
-      enabled: true,
-      services: ['sauce'],
-    },
-  },
-  multiple: {
-    parallel: {
-      chunks: 2, // Number of browsers to run in parallel
-    },
-    // Define configurations for different browsers
-    browsers: {
-      chrome: {
-        desiredCapabilities: {
-          browserName: 'chrome',
-          browserVersion: 'latest',
-        },
-      },
-      firefox: {
-        desiredCapabilities: {
-          browserName: 'firefox',
-          browserVersion: 'latest',
-        },
-      },
-      safari: {
-        desiredCapabilities: {
-          browserName: 'safari',
-          browserVersion: 'latest',
-        },
-      },
-    },
-  },
+const config = require('../config.js');
+const supportedBrowsers = require('../crossbrowser/supportedBrowsers');
+const testUrl = process.env.TEST_URL || config.TestUrl;
+
+const waitForTimeout = parseInt(process.env.WAIT_FOR_TIMEOUT) || 45000;
+const smartWait = parseInt(process.env.SMART_WAIT) || 30000;
+const browser = process.env.BROWSER_GROUP || 'chrome';
+
+const defaultSauceOptions = {
+    username: process.env.SAUCE_USERNAME || 'username',
+    accessKey: process.env.SAUCE_ACCESS_KEY || 'privatekey',
+    tunnelIdentifier: process.env.TUNNEL_IDENTIFIER || 'reformtunnel',
+    acceptSslCerts: true,
+    windowSize: '1600x900',
+    tags: ['et-e2e'],
+    extendedDebugging: true,
+    capturePerformance: true
 };
+
+function merge(intoObject, fromObject) {
+    return Object.assign({}, intoObject, fromObject);
+}
+
+function getBrowserConfig(browserGroup) {
+    const browserConfig = [];
+    for (const candidateBrowser in supportedBrowsers[browserGroup]) {
+        if (candidateBrowser) {
+            const candidateCapabilities = supportedBrowsers[browserGroup][candidateBrowser];
+            candidateCapabilities['sauce:options'] = merge(
+                defaultSauceOptions, candidateCapabilities['sauce:options']
+            );
+            browserConfig.push({
+                browser: candidateCapabilities.browserName,
+                capabilities: candidateCapabilities
+            });
+        } else {
+            console.error('ERROR: supportedBrowsers.js is empty or incorrectly defined');
+        }
+    }
+    return browserConfig;
+}
+
+const setupConfig = {
+    tests: config.TestPathToRun,
+    output: `${process.cwd()}/${config.TestOutputDir}`,
+    helpers: {
+        WebDriver: {
+            url: testUrl,
+            browser,
+            smartWait,
+            waitForTimeout,
+            cssSelectorsEnabled: 'true',
+            host: 'ondemand.eu-central-1.saucelabs.com',
+            port: 80,
+            region: 'eu',
+            capabilities: {}
+        },
+        MyHelper: {
+            require: './helpers/saucelabsHelper.js',
+            url: testUrl,
+        },
+        Mochawesome: {
+            uniqueScreenshotNames: 'true'
+        }
+    },
+    plugins: {
+        retryFailedStep: {
+            enabled: true,
+            retries: 2
+        },
+        autoDelay: {
+            enabled: config.TestAutoDelayEnabled,
+            delayAfter: 2000
+        }
+    },
+    include: {I: './pages/steps.js'},
+    mocha: {
+        reporterOptions: {
+            'codeceptjs-cli-reporter': {
+                stdout: '-',
+                options: {
+                    steps: true
+                }
+            },
+            'mocha-junit-reporter': {
+                stdout: '-',
+                options: {
+                    mochaFile: `${config.TestOutputDir}/result.xml`
+                }
+            },
+            'mochawesome': {
+                stdout: config.TestOutputDir + '/console.log',
+                options: {
+                    reportDir: config.TestOutputDir,
+                    reportName: 'index',
+                    reportTitle: 'Crossbrowser results for: ' + browser.toUpperCase(),
+                    inlineAssets: true
+                }
+            }
+        }
+    },
+    multiple: {
+        // microsoft: {
+        //     browsers: getBrowserConfig('microsoft')
+        // },
+        chrome: {
+            browsers: getBrowserConfig('chrome')
+        },
+        firefox: {
+            browsers: getBrowserConfig('firefox')
+        }
+    },
+    name: 'ET Cross Browser Tests'
+};
+
+exports.config = setupConfig;

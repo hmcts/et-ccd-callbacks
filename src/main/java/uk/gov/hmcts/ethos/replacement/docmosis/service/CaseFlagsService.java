e43b577e97e2842528d20da9b2d8d55bc80fc926
Jack Reeve <jack@reevies.tech>
2023-06-29 14:05:02 +0100

    tmp

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseFlagsService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseFlagsService.java
@@ -2,13 +2,26 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.webjars.NotFoundException;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.items.FlagDetailType;
+import uk.gov.hmcts.et.common.model.ccd.items.GenericTypeItem;
+import uk.gov.hmcts.et.common.model.ccd.items.ListTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.types.CaseFlagsType;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Objects;
+
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.ACTIVE;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.DISRUPTIVE_CUSTOMER;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.LANGUAGE_INTERPRETER;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.SIGN_LANGUAGE_INTERPRETER;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.VEXATIOUS_LITIGANT;
 
 @Slf4j
 @Service
 public class CaseFlagsService {
-
     /**
      * Setup case flags for Claimant, Respondent and Case level.
      * @param caseData Data about the current case
@@ -27,5 +40,54 @@ public class CaseFlagsService {
                 .roleOnCase("respondent")
                 .build()
         );
+    }
+
+    /**
+     * Set default flag values for a newly created case.
+     * @param caseData Data about the current case
+     */
+    public void setDefaultFlags(CaseData caseData) {
+        caseData.setCaseRestrictedFlag(false);
+        caseData.setAutoListFlag(true);
+        caseData.setCaseInterpreterRequiredFlag(false);
+        caseData.setCaseAdditionalSecurityFlag(false);
+    }
+
+    /**
+     * Sets additional flags on CaseData dependent on CaseFlags raised.
+     * @param caseData Data about the current case.
+     */
+    public void processNewlySetCaseFlags(CaseData caseData) {
+        ListTypeItem<FlagDetailType> partyLevel = getPartyCaseFlags(caseData);
+        caseData.setCaseInterpreterRequiredFlag(
+                areAnyFlagsActive(partyLevel, SIGN_LANGUAGE_INTERPRETER, LANGUAGE_INTERPRETER)
+        );
+
+        caseData.setCaseAdditionalSecurityFlag(
+                areAnyFlagsActive(partyLevel, VEXATIOUS_LITIGANT, DISRUPTIVE_CUSTOMER)
+        );
+    }
+
+    private ListTypeItem<FlagDetailType> getPartyCaseFlags(CaseData caseData) {
+        return ListTypeItem.concat(
+                caseData.getClaimantFlags().getDetails(),
+                caseData.getRespondentFlags().getDetails()
+        );
+    }
+
+    @Nullable
+    private FlagDetailType findFlagByName(ListTypeItem<FlagDetailType> flags, String name) {
+        return flags.stream()
+                .map(GenericTypeItem::getValue)
+                .filter(o -> name.equals(o.getName()))
+                .findFirst()
+                .orElse(null);
+    }
+
+    private boolean areAnyFlagsActive(ListTypeItem<FlagDetailType> flags, String...names) {
+        return Arrays.stream(names)
+                .map(o -> findFlagByName(flags, o))
+                .filter(Objects::nonNull)
+                .anyMatch(o -> ACTIVE.equals(o.getStatus()));
     }
 }
--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseFlagsServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseFlagsServiceTest.java
@@ -5,12 +5,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.items.FlagDetailType;
+import uk.gov.hmcts.et.common.model.ccd.items.GenericTypeItem;
+import uk.gov.hmcts.et.common.model.ccd.items.ListTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.types.CaseFlagsType;
 import uk.gov.hmcts.ethos.utils.CaseDataBuilder;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.ACTIVE;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.DISRUPTIVE_CUSTOMER;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.INACTIVE;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.LANGUAGE_INTERPRETER;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.SIGN_LANGUAGE_INTERPRETER;
+import static uk.gov.hmcts.ecm.common.model.helper.CaseFlagConstants.VEXATIOUS_LITIGANT;
 
 @ExtendWith(SpringExtension.class)
 class CaseFlagsServiceTest {
@@ -42,5 +51,163 @@ class CaseFlagsServiceTest {
         CaseFlagsType respondentFlags = caseData.getRespondentFlags();
         assertThat(respondentFlags.getPartyName(), is(RESPONDENT_NAME));
         assertThat(respondentFlags.getRoleOnCase(), is("respondent"));
+    }
+
+    @Test
+    void setDefaultFlags_setsDefaultFlags() {
+        caseFlagsService.setDefaultFlags(caseData);
+
+        assertThat(caseData.getCaseRestrictedFlag(), is(false));
+        assertThat(caseData.getAutoListFlag(), is(true));
+        assertThat(caseData.getCaseInterpreterRequiredFlag(), is(false));
+        assertThat(caseData.getCaseAdditionalSecurityFlag(), is(false));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_claimantNeedsLanguageInterpreter_setsInterpreterRequiredFlagTrue() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getClaimantFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(LANGUAGE_INTERPRETER).status(ACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseInterpreterRequiredFlag(), is(true));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_respondentNeedsLanguageInterpreter_setsInterpreterRequiredFlagTrue() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getRespondentFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(LANGUAGE_INTERPRETER).status(ACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseInterpreterRequiredFlag(), is(true));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_claimantNeedsSignLanguageInterpreter_setsInterpreterRequiredFlagTrue() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getClaimantFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(SIGN_LANGUAGE_INTERPRETER).status(ACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseInterpreterRequiredFlag(), is(true));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_respondentNeedsSignLanguageInterpreter_setsInterpreterRequiredFlagTrue() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getRespondentFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(SIGN_LANGUAGE_INTERPRETER).status(ACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseInterpreterRequiredFlag(), is(true));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_noInterpretersNeeded_setsInterpreterRequiredFlagFalse() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getClaimantFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(LANGUAGE_INTERPRETER).status(INACTIVE).build())
+        );
+
+        caseData.getRespondentFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(SIGN_LANGUAGE_INTERPRETER).status(INACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseInterpreterRequiredFlag(), is(false));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_claimantIsVexatious_setsAdditionalSecurityFlagTrue() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getClaimantFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(VEXATIOUS_LITIGANT).status(ACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseAdditionalSecurityFlag(), is(true));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_respondentIsVexatious_setsAdditionalSecurityFlagTrue() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getRespondentFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(VEXATIOUS_LITIGANT).status(ACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseAdditionalSecurityFlag(), is(true));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_claimantIsDisruptive_setsAdditionalSecurityFlagTrue() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getClaimantFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(DISRUPTIVE_CUSTOMER).status(ACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseAdditionalSecurityFlag(), is(true));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_respondentIsDisruptive_setsAdditionalSecurityFlagTrue() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getRespondentFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(DISRUPTIVE_CUSTOMER).status(ACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseAdditionalSecurityFlag(), is(true));
+    }
+
+    @Test
+    void processNewlySetCaseFlags_noPartyIsVexatiousOrDisruptive_setsAdditionalSecurityFlagFalse() {
+        caseData.setClaimantFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+        caseData.setRespondentFlags(CaseFlagsType.builder().details(ListTypeItem.from()).build());
+
+        caseData.getClaimantFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(VEXATIOUS_LITIGANT).status(INACTIVE).build())
+        );
+
+        caseData.getRespondentFlags().getDetails().add(
+                GenericTypeItem.from(FlagDetailType.builder().name(DISRUPTIVE_CUSTOMER).status(INACTIVE).build())
+        );
+
+        caseFlagsService.processNewlySetCaseFlags(caseData);
+
+        assertThat(caseData.getCaseInterpreterRequiredFlag(), is(false));
     }
 }

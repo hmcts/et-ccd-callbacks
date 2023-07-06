8c05dfad5ce34a17418842232740e200a9719101
Sunday Ayeni <sunnyayeni18@gmail.com>
2023-07-06 16:48:06 +0100

    more refactoring due to EXUI change part 5

--- src/test/end-to-end/paths/verifyValidationForDisposalDateJurisdiction.js
+++ src/test/end-to-end/paths/verifyValidationForDisposalDateJurisdiction.js
@@ -46,5 +46,5 @@ Scenario('User enters a disposal date - check error scenarios and completes juri
     await enterDisposalDateJurisdiction(I, 'Date in the future');
     await enterDisposalDateJurisdiction(I,'Date contained in hearing collection');
 
-}).tag('@test')
+}).tag('@nightly')
     .retry(testConfig.TestRetryScenarios);

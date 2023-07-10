e9039e324049afabefeb56f5208f081337799088
Daniel Furnivall <danielfurnivall@gmail.com>
2023-07-10 16:20:34 +0100

    for harpreet

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/config/CcdClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/config/CcdClientConfiguration.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.ethos.replacement.docmosis.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;

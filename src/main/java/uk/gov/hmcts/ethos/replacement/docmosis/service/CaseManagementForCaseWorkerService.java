159d7613ae6dc0c6c105405dbc31ab713dd1ab10
gauravchawla <gaurav-chawla@hotmail.com>
2023-07-17 19:24:50 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseManagementForCaseWorkerService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseManagementForCaseWorkerService.java
@@ -211,7 +211,7 @@ public class CaseManagementForCaseWorkerService {
                     nextListedDate = date;
                 }
             }
-            if(Strings.isNullOrEmpty(caseData.getNextListedDate())) {
+            if (Strings.isNullOrEmpty(caseData.getNextListedDate())) {
                 caseData.setNextListedDate(nextListedDate.split("T")[0]);
             }
         }

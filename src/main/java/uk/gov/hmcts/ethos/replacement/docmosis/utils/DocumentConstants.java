e153e59b6c0205b5e638354886931bc779d22a72
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-07-19 15:13:55 +0100

    RET-3898

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/utils/DocumentConstants.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/utils/DocumentConstants.java
@@ -1,7 +1,10 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.utils;
 
-
 public class DocumentConstants {
+
+    private DocumentConstants() {
+        // Private constructor
+    }
 
     public static final String STARTING_A_CLAIM = "Starting a Claim";
     public static final String RESPONSE_TO_A_CLAIM = "Response to a Claim";
@@ -35,8 +38,10 @@ public class DocumentConstants {
     public static final String APP_TO_VARY_AN_ORDER_R = "App to vary an order – R";
     public static final String APP_TO_REVOKE_AN_ORDER_C = "App to revoke an order- C";
     public static final String APP_TO_REVOKE_AN_ORDER_R = "App to revoke an order – R";
-    public static final String APP_TO_EXTEND_TIME_TO_COMPLY_TO_AN_ORDER_DIRECTIONS_C = "App to extend time to comply to an order/directions – C";
-    public static final String APP_TO_EXTEND_TIME_TO_COMPLY_TO_AN_ORDER_DIRECTIONS_R = "App to extend time to comply to an order/directions – R";
+    public static final String APP_TO_EXTEND_TIME_TO_COMPLY_TO_AN_ORDER_DIRECTIONS_C = "App to extend time to comply "
+            + "to an order/directions – C";
+    public static final String APP_TO_EXTEND_TIME_TO_COMPLY_TO_AN_ORDER_DIRECTIONS_R = "App to extend time to comply "
+            + "to an order/directions – R";
     public static final String APP_TO_ORDER_THE_R_TO_DO_SOMETHING = "App to Order the R to do something";
     public static final String APP_TO_ORDER_THE_C_TO_DO_SOMETHING = "App to Order the C to do something";
     public static final String APP_TO_AMEND_CLAIM = "App to amend claim";
@@ -45,8 +50,10 @@ public class DocumentConstants {
     public static final String DISABILITY_IMPACT_STATEMENT = "Disability Impact statement";
     public static final String R_HAS_NOT_COMPLIED_WITH_AN_ORDER__C = "R has not complied with an order - C";
     public static final String C_HAS_NOT_COMPLIED_WITH_AN_ORDER__R = "C has not complied with an order - R";
-    public static final String APP_TO_STRIKE_OUT_ALL_OR_PART_OF_THE_CLAIM = "App to Strike out all or part of the claim";
-    public static final String APP_TO_STRIKE_OUT_ALL_OR_PART_OF_THE_RESPONSE = "App to Strike out all or part of the response";
+    public static final String APP_TO_STRIKE_OUT_ALL_OR_PART_OF_THE_CLAIM = "App to Strike out all or part of the "
+            + "claim";
+    public static final String APP_TO_STRIKE_OUT_ALL_OR_PART_OF_THE_RESPONSE = "App to Strike out all or part of the "
+            + "response";
     public static final String REFERRAL_JUDICIAL_DIRECTION = "Referral/Judicial Direction";
     public static final String CHANGE_OF_PARTYS_DETAILS = "Change of party’s details";
     public static final String WITHDRAWAL_OF_ENTIRE_CLAIM = "Withdrawal of entire claim";
@@ -65,8 +72,10 @@ public class DocumentConstants {
     public static final String JUDGMENT_WITH_REASONS = "Judgment with Reasons";
     public static final String REASONS = "Reasons";
     public static final String EXTRACT_OF_JUDGMENT = "Extract of Judgment";
-    public static final String APP_TO_HAVE_A_LEGAL_OFFICER_DECISION_CONSIDERED_AFRESH_C = "App to have a Legal Officer decision considered afresh - C";
-    public static final String APP_TO_HAVE_A_LEGAL_OFFICER_DECISION_CONSIDERED_AFRESH_R = "App to have a Legal Officer decision considered afresh - R";
+    public static final String APP_TO_HAVE_A_LEGAL_OFFICER_DECISION_CONSIDERED_AFRESH_C = "App to have a Legal Officer"
+            + " decision considered afresh - C";
+    public static final String APP_TO_HAVE_A_LEGAL_OFFICER_DECISION_CONSIDERED_AFRESH_R = "App to have a Legal Officer "
+            + "decision considered afresh - R";
     public static final String APP_FOR_A_JUDGMENT_TO_BE_RECONSIDERED_C = "App for a judgment to be reconsidered - C";
     public static final String APP_FOR_A_JUDGMENT_TO_BE_RECONSIDERED_R = "App for a judgment to be reconsidered - R";
     public static final String CERTIFICATE_OF_CORRECTION = "Certificate of Correction";

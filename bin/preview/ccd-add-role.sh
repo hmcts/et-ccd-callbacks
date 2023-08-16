626648ed40c1dabd94349b4bf55248ce96f21f74
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-08-16 12:43:22 +0100

    Update

--- bin/preview/ccd-add-role.sh
+++ bin/preview/ccd-add-role.sh
@@ -2,17 +2,17 @@
 
 set -ex
 
-dir=$(dirname ${0})
+dir=$(dirname "${0}")
 
 role=${1}
 
-userToken=$(sh ${dir}/idam-user-token.sh)
-serviceToken=$(sh ${dir}/idam-service-token.sh)
+userToken=$(sh "${dir}"/idam-user-token.sh)
+serviceToken=$(sh "${dir}"/idam-service-token.sh)
 
 echo "Creating CCD role: ${role} using ${CCD_DEFINITION_STORE_API_BASE_URL}"
 
 curl --insecure --fail --show-error --silent -X PUT \
-  ${CCD_DEFINITION_STORE_API_BASE_URL}/api/user-role \
+  "${CCD_DEFINITION_STORE_API_BASE_URL}"/api/user-role \
   -H "Authorization: Bearer ${userToken}" \
   -H "ServiceAuthorization: Bearer ${serviceToken}" \
   -H "Content-Type: application/json" \

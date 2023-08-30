395668f5030955938a49988090b19b925f7ae8bf
marcjudge <98343606+marcjudge@users.noreply.github.com>
2023-08-30 09:35:53 +0100

    RET-3606 - Send mail to legal rep (#1120)
    * Send mail to legal rep
    
    * Suppress batik

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/NotificationHelper.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/NotificationHelper.java
@@ -181,7 +181,10 @@ public final class NotificationHelper {
         return respondent.getRespondentName();
     }
 
-    private static RepresentedTypeR getRespondentRepresentative(CaseData caseData, RespondentSumType respondent) {
+    /**
+     * Gets the representative for the respondent if present.
+     */
+    public static RepresentedTypeR getRespondentRepresentative(CaseData caseData, RespondentSumType respondent) {
         List<RepresentedTypeRItem> repCollection = caseData.getRepCollection();
 
         if (CollectionUtils.isEmpty(repCollection)) {

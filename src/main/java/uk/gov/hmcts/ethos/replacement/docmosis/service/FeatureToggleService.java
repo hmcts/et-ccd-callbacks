90cc6329595ec09072392b1f03095eceda2212b0
dharmendra kumar <dharmendra.kumar@hmcts.net>
2023-09-06 10:07:57 +0100

    Add launch darkly config for global search

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/FeatureToggleService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/FeatureToggleService.java
@@ -2,15 +2,20 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ecm.common.launchdarkly.FeatureToggleApi;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class FeatureToggleService {
 
     private final FeatureToggleApi featureToggleApi;
+
+    @Autowired
+    public FeatureToggleService(FeatureToggleApi featureToggleApi) {
+        this.featureToggleApi = featureToggleApi;
+    }
 
     public boolean isFeatureEnabled(String feature) {
         return this.featureToggleApi.isFeatureEnabled(feature);

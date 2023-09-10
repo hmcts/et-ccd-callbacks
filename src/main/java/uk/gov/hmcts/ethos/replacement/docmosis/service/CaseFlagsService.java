c2710a2397792d01e8250ec1d84cb1691f57e807
Jack Reeve <jack@reevies.tech>
2023-09-10 17:28:24 +0100

    casefalgs v2.1 test

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseFlagsService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/CaseFlagsService.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
 import lombok.extern.slf4j.Slf4j;
+import java.util.UUID;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.types.CaseFlagsType;
@@ -15,16 +16,38 @@ public class CaseFlagsService {
      */
     public void setupCaseFlags(CaseData caseData) {
         caseData.setCaseFlags(CaseFlagsType.builder().build());
+        String claimantGroupId = UUID.randomUUID().toString();
+        String respondentGroupId = UUID.randomUUID().toString();
 
         caseData.setClaimantFlags(CaseFlagsType.builder()
                 .partyName(caseData.getClaimant())
                 .roleOnCase("claimant")
+                .groupId(claimantGroupId)
+                .visibility("Internal")
                 .build()
         );
 
         caseData.setRespondentFlags(CaseFlagsType.builder()
                 .partyName(caseData.getRespondent())
                 .roleOnCase("respondent")
+                .groupId(respondentGroupId)
+                .visibility("Internal")
+                .build()
+        );
+
+        caseData.setExternalClaimantFlags(CaseFlagsType.builder()
+                .partyName(caseData.getClaimant())
+                .roleOnCase("claimant")
+                .groupId(claimantGroupId)
+                .visibility("External")
+                .build()
+        );
+
+        caseData.setExternalRespondentFlags(CaseFlagsType.builder()
+                .partyName(caseData.getRespondent())
+                .roleOnCase("respondent")
+                .groupId(respondentGroupId)
+                .visibility("External")
                 .build()
         );
     }

0310e89c1913cf2f3e447ba60de25bc0266b7211
btensay <tensay.bulcha@gmail.com>
2023-09-14 09:45:29 +0100

    data object annotation aligned to the existing patterns

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/documents/TseAdminReplyData.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/documents/TseAdminReplyData.java
@@ -1,17 +1,23 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.domain.documents;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 /**
  * This object captures information related to the TseAdminReply object during
  * the document creation event for Docmosis.
  */
-@EqualsAndHashCode(callSuper = false)
-@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TseAdminReplyData extends TseReplyData {
     @JsonProperty("responseTitle")
     private String responseTitle;

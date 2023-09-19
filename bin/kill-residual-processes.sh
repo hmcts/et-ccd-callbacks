1c6a840618f033dfd6a15844b49ab2f7a791f013
mehmet.dede <mehmet.dede@justice.gov.uk>
2023-09-19 13:03:00 +0100

    Updates for shell scripts

---
+++ bin/kill-residual-processes.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eu
+
+echo "Checking port 4453"
+processId_4453=`lsof -i -n -P | grep LISTEN | grep :4453 | awk '{print $2}'`
+
+if [ ! -z "$processId_4453" ]
+then
+  echo "killing process with Id $processId_4453"
+  kill -9 "$processId_4453"
+else
+  echo "There is no process running on port 4453"
+fi
+
+echo "Checking port 4455"
+processId_4455=`lsof -i -n -P | grep LISTEN | grep :4455 | awk '{print $2}'`
+
+if [ ! -z "$processId_4455" ]
+then
+  echo "killing process with Id $processId_4455"
+  kill -9 "$processId_4455"
+else
+  echo "There is no process running on port 4455"
+fi

4d05605a081afb7dd178da3446c22dd65fcd2921
jaddison <jephtah.addison@hmcts.net>
2023-11-06 16:34:44 +0000

    RET-3109 correcting review corrections and renaming template id

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/SendNotificationService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/SendNotificationService.java
@@ -66,7 +66,7 @@ public class SendNotificationService {
     private String claimantSendNotificationHearingOtherTemplateId;
     @Value("${template.respondentSendNotificationHearingOther}")
     private String respondentSendNotificationHearingOtherTemplateId;
-    @Value("${template.bundlesClaimantSubmittedRespondentNotificationTemplateId}")
+    @Value("${template.bundles.claimantSubmittedRespondentNotification}")
     private String bundlesClaimantSubmittedRespondentNotificationTemplateId;
 
     private static final String BLANK_DOCUMENT_MARKDOWN = "| Document | | \r\n| Description | |";

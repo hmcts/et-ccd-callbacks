ffbee800af87b2a46b473d0ebe8c7fa338d63cf5
GauravChawlaMOJ <58194626+GauravChawlaMOJ@users.noreply.github.com>
2023-11-13 15:50:28 +0000

    RET-4140 (#1169)
    * ret-3891 implementation added
    
    changes imported to a branch updated from base
    
    * removed illegal char
    
    * checkstyle fix
    
    * hash code and equals annotation added
    
    * annotation default value set
    
    * data object annotation aligned to the existing patterns
    
    * functional test fix
    
    * functional test - list items added
    
    * more functional test params provided
    
    * Revert "more functional test params provided"
    
    This reverts commit 5dde5cc581a328e5cdeefff03f0d54fb6bf1fabb.
    
    * pdf template names updated
    
    * unit tests corrected
    
    * unused import removed
    
    * Rollback for record admin decision
    
    * Rollback for record admin decision
    
    * Rollback for record admin decision
    
    * Rollback for record admin decision
    
    * Rollback for record admin decision
    
    * Update of package commons-compress from 1.23.0 to 1.24.0. because of the vulnerability CVE-2023-42503
    
    * Removed TseAdminDecisionRecordData.java model from docmosis.domain.documents
    
    * Removed TseAdminDecisionRecordData.java model from docmosis.domain.documents
    
    * Updated unit tests for adm reply service.
    
    * pdf attachment to notification email
    
    Functionality added to attach a pdf copy of Tse Admin Reply to Notification email.
    
    * param name correction
    
    * RET-4152 and RET-4166 (#1160)
    
    * RET-4140
    
    * RET-4140
    
    * checkstyle issues
    
    * checkstyle issues
    
    * checkstyle issues
    
    * checkstyle issues
    
    * checkstyle issues
    
    * RET-4140
    
    * RET-4140
    
    * RET-4140
    
    * Shell script updates
    
    * code change to add three new tabs
    
    controllers and tests added
    
    * clean up
    
    unnecessary code removed as a filed is made mandatory in the configs. plus clean up of unused imports.
    
    * controllers removed
    
    when even fields are configured to be optional, then the controllers may be needed for say setting the document upload date
    
    * binary url for document list within pdf document is added
    
    * RET-4309 (#1228)
    
    * RET-4309
    
    * Update build.gradle
    
    * RET-4166
    
    * PMD count
    
    * Sonar
    
    * Shell script updates
    
    * Updates for PreHearingDeposit case creation
    
    * Updates for PreHearingDeposit case creation
    
    * Updates for PreHearingDeposit case creation
    
    * Updates for PreHearingDeposit case creation
    
    * Updates for PreHearingDeposit case creation by excel file
    
    * Updates for PreHearingDeposit case creation manually
    
    * Updates for PreHearingDeposit case creation manually
    
    * Updates for PreHearingDeposit case creation manually
    
    * Updates for PreHearingDeposit case creation manually
    
    * Updates for PreHearingDeposit case creation manually
    
    ---------
    
    Co-authored-by: btensay <tensay.bulcha@gmail.com>
    Co-authored-by: mehmet.dede <mehmet.dede@justice.gov.uk>
    Co-authored-by: Harpreet Jhita <harpreet.jhita@justice.gov.uk>
    Co-authored-by: gauravchawla <gaurav-chawla@hotmail.com>
    Co-authored-by: Mehmet Tahir Dede <105645217+JustMehmet@users.noreply.github.com>

---
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/prehearingdeposit/PreHearingDepositServiceTest.java
@@ -0,0 +1,78 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.service.prehearingdeposit;
+
+import lombok.SneakyThrows;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.ecm.common.client.CcdClient;
+import uk.gov.hmcts.ecm.common.idam.models.UserDetails;
+import uk.gov.hmcts.ecm.common.service.UserService;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.admin.types.Document;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.admin.types.ImportFile;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.excel.ExcelReadingService;
+
+import java.util.List;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class PreHearingDepositServiceTest {
+    private UserDetails userDetails;
+    private PreHearingDepositService preHearingDepositService;
+    private static final String USER_TOKEN = "userToken";
+    private static final String EXCEL_WORKBOOK = "src/test/resources/PreHearingDepositData.xlsx";
+    private static final String EMAIL = "test@test.com";
+    private static final String FIRST_NAME = "Test User First Name";
+    private static final String NAME = "Test User Name";
+    private static final String UID = "Test UUID";
+    private static final String LAST_NAME = "Test Last Name";
+    private static final String ROLES = "Test Role1";
+    private static final String FILE_NAME = "Test File Name";
+    private static final String URL = "testUrl";
+    private static final String BINARY_URL = "Test Binary URL";
+    @Mock
+    UserService userService;
+    @Mock
+    private CcdClient ccdClient;
+    @InjectMocks
+    private ExcelReadingService excelReadingService;
+
+    @BeforeEach
+    void setUp() {
+        ccdClient = mock(CcdClient.class);
+        userService = mock(UserService.class);
+        excelReadingService = mock(ExcelReadingService.class);
+        preHearingDepositService = new PreHearingDepositService(userService, excelReadingService, ccdClient);
+        userDetails = new UserDetails();
+        userDetails.setEmail(EMAIL);
+        userDetails.setFirstName(FIRST_NAME);
+        userDetails.setName(NAME);
+        userDetails.setUid(UID);
+        userDetails.setLastName(LAST_NAME);
+        userDetails.setRoles(List.of(ROLES));
+    }
+
+    @Test
+    @SneakyThrows
+    void importPreHearingDepositData() {
+        when(userService.getUserDetails(USER_TOKEN)).thenReturn(userDetails);
+        when(excelReadingService.readWorkbook(anyString(), anyString())).thenReturn(new XSSFWorkbook(EXCEL_WORKBOOK));
+        Document document = new Document();
+        document.setFilename(FILE_NAME);
+        document.setUrl(URL);
+        document.setBinaryUrl(BINARY_URL);
+        ImportFile importFile = new ImportFile();
+        importFile.setFile(document);
+        preHearingDepositService.importPreHearingDepositData(importFile, USER_TOKEN);
+        verify(ccdClient, times(4)).startGenericTypeCaseCreation(anyString(), any());
+    }
+}

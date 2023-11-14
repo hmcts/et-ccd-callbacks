9d5bdbbab2136ef8bbea8d2cfc6b881c6ca97d9c
JackReevies <43950645+JackReevies@users.noreply.github.com>
2023-11-14 17:14:47 +0000

    Update TseRespondentReplyServiceTest.java

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/TseRespondentReplyServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/TseRespondentReplyServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.verification.VerificationMode;

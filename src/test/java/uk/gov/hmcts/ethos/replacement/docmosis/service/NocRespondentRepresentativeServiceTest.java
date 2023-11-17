9f44a9dbbd6e89f5e9f90337ac5abd358cb52c27
Jack Reeve <jack@reevies.tech>
2023-11-17 17:06:36 +0000

    Only updating claimant rep fields if not already set

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/NocRespondentRepresentativeServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/NocRespondentRepresentativeServiceTest.java
@@ -21,7 +21,6 @@ import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
 import uk.gov.hmcts.et.common.model.ccd.CaseUserAssignment;
 import uk.gov.hmcts.et.common.model.ccd.CaseUserAssignmentData;
-import uk.gov.hmcts.et.common.model.ccd.items.ListTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.items.RepresentedTypeRItem;
 import uk.gov.hmcts.et.common.model.ccd.items.RespondentSumTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.types.ChangeOrganisationRequest;

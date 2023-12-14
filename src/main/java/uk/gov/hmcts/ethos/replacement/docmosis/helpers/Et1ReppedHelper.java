5f8cd841c17776f108ce5992c66202a8780b2c86
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2023-12-14 17:19:34 +0000

    RET-4523 Changes

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/Et1ReppedHelper.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/Et1ReppedHelper.java
@@ -5,6 +5,7 @@ import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import java.util.List;
 
 import static com.google.common.base.Strings.isNullOrEmpty;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.INDIVIDUAL_TYPE_CLAIMANT;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.NO;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.YES;
 
@@ -14,6 +15,12 @@ public class Et1ReppedHelper {
     private static final String COMPLETED = "<strong class=\"govuk-tag govuk-tag--turquoise\">Completed</strong><br>";
     private static final String UNAVAILABLE = "<strong class=\"govuk-tag govuk-tag--grey\">Unavailable</strong><br>";
     private static final String AVAILABLE = "<strong class=\"govuk-tag govuk-tag--blue\">Available</strong><br>";
+    private static final String RESPONDENT_PREAMBLE = """
+            <h2>Respondent added:</h2>
+             %s
+            <hr>
+            <p>You can amend the details of each respondent before you submit the claim.<p>
+            """;
 
     private Et1ReppedHelper() {
         // Access through static methods
@@ -41,28 +48,39 @@ public class Et1ReppedHelper {
         String submitCase = allSectionsCompleted(caseData);
 
         String label = """
-                Complete the ET1 claim form in 3 sections.
-                
-                You can sign out and return to the claim if you've completed at least one section.
-                            
-                You can review and edit any completed section before you submit the claim to the tribunal.
-                            
-                You can complete the sections in any order.
+                       Complete the ET1 claim form in 3 sections.
+                                       
+                       You can sign out and return to the claim if you've completed at least one section.
+                                   
+                       You can review and edit any completed section before you submit the claim to the tribunal.
+                                                        
+                       You can complete the sections in any order.
 
-                Steps to making a claim | Status
-                -|-
-                [ET1 Section 1 - Claimant details](/cases/case-details/${[CASE_REFERENCE]}/trigger/et1SectionOne/et1SectionOne1)                 | %s
-                [ET1 Section 2 - Employment and respondent details](/cases/case-details/${[CASE_REFERENCE]}/trigger/et1SectionTwo/et1SectionTwo1) | %s
-                [ET1 Section 3 - Details of the claim](/cases/case-details/${[CASE_REFERENCE]}/trigger/et1SectionThree/et1SectionThree1)             | %s
-                [Submit claim](/cases/case-details/${[CASE_REFERENCE]}/trigger/submitEt1Draft/submitEt1Draft1)                                     |  %s
-                    """;
+                       Steps to making a claim | Status
+                       -|-
+                       [ET1 Section 1 - Claimant details](/cases/case-details/${[CASE_REFERENCE]}/trigger/et1SectionOne/et1SectionOne1)                 | %s
+                       [ET1 Section 2 - Employment and respondent details](/cases/case-details/${[CASE_REFERENCE]}/trigger/et1SectionTwo/et1SectionTwo1) | %s
+                       [ET1 Section 3 - Details of the claim](/cases/case-details/${[CASE_REFERENCE]}/trigger/et1SectionThree/et1SectionThree1)             | %s
+                       [Submit claim](/cases/case-details/${[CASE_REFERENCE]}/trigger/submitEt1Draft/submitEt1Draft1)                                     |  %s
+                           """;
         caseData.setEt1ClaimStatuses(label.formatted(sectionOne, sectionTwo, sectionThree, submitCase));
     }
 
+    /**
+     * Sets the statuses for the ET1 Repped journey.
+     * @param caseData the case data
+     * @param section the section
+     */
     public static void setEt1SectionStatuses(CaseData caseData, String section) {
         switch (section) {
-            case "et1SectionOne" -> caseData.setEt1ReppedSectionOne(YES);
-            case "et1SectionTwo" -> caseData.setEt1ReppedSectionTwo(YES);
+            case "et1SectionOne" -> {
+                setInitialSectionOneData(caseData);
+                caseData.setEt1ReppedSectionOne(YES);
+            }
+            case "et1SectionTwo" -> {
+                setInitialSectionTwoData(caseData);
+                caseData.setEt1ReppedSectionTwo(YES);
+            }
             case "et1SectionThree" -> caseData.setEt1ReppedSectionThree(YES);
             default -> throw new IllegalStateException("Unexpected value: " + section);
         }
@@ -83,16 +101,53 @@ public class Et1ReppedHelper {
                 ? UNAVAILABLE : AVAILABLE;
     }
 
-    public static void validateClaimantSex(CaseData caseData, List<String> errors) {
-        if (!caseData.getClaimantSex().isEmpty() && caseData.getClaimantSex().size() > 1) {
-            errors.add("Please select only one option");
-        }
-    }
-
+    /**
+     * Validates the options selected for the ET1 Repped journey.
+     * @param options the options
+     * @return a list of error messages
+     */
     public static List<String> validateSingleOption(List<String> options) {
         if (!options.isEmpty() && options.size() > 1) {
             return List.of("Please select only one option");
         }
         return List.of();
+    }
+
+    /**
+     * Generates the preamble for the additional respondent.
+     * @param caseData the case data
+     */
+    public static void generateRespondentPreamble(CaseData caseData) {
+        String respondent = "";
+        if (INDIVIDUAL_TYPE_CLAIMANT.equals(caseData.getRespondentType())) {
+            respondent = caseData.getRespondentFirstName() + " " + caseData.getRespondentLastName();
+        } else if ("Organisation".equals(caseData.getRespondentType())) {
+            respondent = caseData.getRespondentOrganisationName();
+        } else {
+            throw new IllegalStateException("Unexpected value: " + caseData.getRespondentType());
+        }
+        caseData.setAddAdditionalRespondentPreamble(RESPONDENT_PREAMBLE.formatted(respondent));
+
+    }
+
+    /**
+     * Generates the preamble for the claimant work address.
+     * @param caseData the case data
+     */
+    public static void generateWorkAddressLabel(CaseData caseData) {
+        if (caseData.getRespondentAddress() == null) {
+            throw new IllegalStateException("Respondent address is null");
+        }
+        caseData.setDidClaimantWorkAtSameAddressPreamble(caseData.getRespondentAddress().toString());
+    }
+
+    private static void setInitialSectionOneData(CaseData caseData) {
+        caseData.setClaimant(caseData.getClaimantFirstName() + " " + caseData.getClaimantLastName());
+    }
+
+    private static void setInitialSectionTwoData(CaseData caseData) {
+        caseData.setRespondent(INDIVIDUAL_TYPE_CLAIMANT.equals(caseData.getRespondentType())
+                ? caseData.getRespondentFirstName() + " " + caseData.getRespondentLastName()
+                : caseData.getRespondentOrganisationName());
     }
 }

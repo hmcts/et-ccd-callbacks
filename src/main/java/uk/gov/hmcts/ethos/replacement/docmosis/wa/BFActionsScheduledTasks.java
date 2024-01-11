3bb04423fd13974edc3a4fe7e3e4bc76623c3a55
Jack Reeve <jack@reevies.tech>
2024-01-11 09:41:18 +0000

    checkstyle

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/wa/BFActionsScheduledTasks.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/wa/BFActionsScheduledTasks.java
@@ -4,23 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.elasticsearch.index.query.TermQueryBuilder;
-import org.elasticsearch.search.aggregations.support.ValuesSource;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ecm.common.client.CcdClient;
-import uk.gov.hmcts.ecm.common.client.CcdSubmitEventParams;
 import uk.gov.hmcts.ecm.common.helpers.UtilHelper;
-import uk.gov.hmcts.ecm.common.service.UserService;
 import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
-import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
 import uk.gov.hmcts.et.common.model.ccd.SubmitEvent;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.AdminUserService;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
@@ -31,13 +22,11 @@ import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
-import static uk.gov.hmcts.ecm.common.model.helper.Constants.CASE_TRANSFER_EVENT_TRIGGER_ID;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_CASE_TYPE_ID;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.MAX_ES_SIZE;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.NO;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_CASE_TYPE_ID;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.YES;
-import static uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClientApi.SERVICE_AUTHORIZATION;
 
 @Component
 @Slf4j
@@ -68,15 +57,16 @@ public class BFActionsScheduledTasks {
         log.warn("auth: " + adminUserToken);
         log.warn("s2s: " + authToken);
         try {
-            List<SubmitEvent> englandCases = ccdClient.buildAndGetElasticSearchRequest(adminUserToken, ENGLANDWALES_CASE_TYPE_ID, query);
-            List<SubmitEvent> scotlandCases = ccdClient.buildAndGetElasticSearchRequest(adminUserToken, SCOTLAND_CASE_TYPE_ID, query);
+            List<SubmitEvent> englandCases =
+                    ccdClient.buildAndGetElasticSearchRequest(adminUserToken, ENGLANDWALES_CASE_TYPE_ID, query);
+            List<SubmitEvent> scotlandCases =
+                    ccdClient.buildAndGetElasticSearchRequest(adminUserToken, SCOTLAND_CASE_TYPE_ID, query);
 
             englandCases.forEach(o -> triggerTaskEventForCase(adminUserToken, o, ENGLANDWALES_CASE_TYPE_ID));
             scotlandCases.forEach(o -> triggerTaskEventForCase(adminUserToken, o, SCOTLAND_CASE_TYPE_ID));
 
             log.warn("There were " + (englandCases.size() + scotlandCases.size()) + " results returned");
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             log.error(e.getMessage());
         }
     }
@@ -89,7 +79,7 @@ public class BFActionsScheduledTasks {
             CaseData caseData = submitEvent.getCaseData();
             caseData.setWaRule21ReferralSent(YES);
 
-            ccdClient.submitEventForCase(adminUserToken, caseData,caseTypeId,
+            ccdClient.submitEventForCase(adminUserToken, caseData, caseTypeId,
                     "EMPLOYMENT", returnedRequest, String.valueOf(submitEvent.getCaseId()));
             log.error("Called WA_REVIEW_RULE21_REFERRAL for " + submitEvent.getCaseId());
         } catch (IOException e) {

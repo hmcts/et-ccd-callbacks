ae34a09b2480bc27e5aa3e5f613eb99b423b34bb
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-01-23 10:26:49 +0000

    Update Et1VettingService.java

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/Et1VettingService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/Et1VettingService.java
@@ -28,6 +28,7 @@ import uk.gov.hmcts.ethos.replacement.docmosis.utils.IntWrapper;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 

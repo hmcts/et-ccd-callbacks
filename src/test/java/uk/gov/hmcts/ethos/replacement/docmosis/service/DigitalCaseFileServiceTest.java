d93afea241be4191ab606456a70c120b8c9ad662
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2024-02-06 09:38:56 +0000

    Remove PMD exception

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DigitalCaseFileServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DigitalCaseFileServiceTest.java
@@ -68,7 +68,6 @@ class DigitalCaseFileServiceTest {
         assertNotNull(caseData.getCaseBundles());
         assertEquals(1, caseData.getCaseBundles().get(0).value().getDocuments().size());
         assertEquals(YES, caseDetails.getCaseData().getCaseBundles().get(0).value().getEligibleForStitching());
-        System.out.println(caseData);
     }
 
     @Test

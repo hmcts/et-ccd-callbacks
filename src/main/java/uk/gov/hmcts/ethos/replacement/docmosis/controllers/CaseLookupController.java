fb18db6e6eafde3cfffaf944f6abffdc9f1061c8
Jack Reeve <jack@reevies.tech>
2024-02-07 09:39:19 +0000

    removed controller for single case

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseLookupController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/CaseLookupController.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,11 +7,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.compress.archivers.dump.UnrecognizedFormatException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.ecm.common.client.CcdClient;
-import uk.gov.hmcts.et.common.model.ccd.*;
+import uk.gov.hmcts.et.common.model.ccd.CCDCallbackResponse;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.SubmitEvent;
 import uk.gov.hmcts.et.common.model.multiples.MultipleCallbackResponse;
 import uk.gov.hmcts.et.common.model.multiples.MultipleData;
 import uk.gov.hmcts.et.common.model.multiples.MultipleDetails;
@@ -21,70 +24,32 @@ import uk.gov.hmcts.ethos.replacement.docmosis.helpers.ReferralHelper;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.AdminUserService;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.VerifyTokenService;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.CallbackRespHelper.*;
+import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.CallbackRespHelper.getMultipleCallbackRespEntity;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 public class CaseLookupController {
-    private static final String LOG_MESSAGE = "{} received notification request for case reference : {}";
-
     private static final String INVALID_TOKEN = "Invalid Token {}";
     private final VerifyTokenService verifyTokenService;
     private final AdminUserService adminUserService;
     private final CcdClient ccdClient;
 
-    @PostMapping(value = "/anotherCaseLookup", consumes = APPLICATION_JSON_VALUE)
-    @Operation(summary = "Retrieve ACAS Certificate from ACAS")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Accessed successfully",
-                    content = {
-                            @Content(mediaType = "application/json", schema = @Schema(implementation = CCDCallbackResponse.class))
-                    }),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
-    })
-    public ResponseEntity<CCDCallbackResponse> retrieveCertificate(
-            @RequestBody CCDRequest ccdRequest,
-            @RequestHeader("Authorization") String userToken) throws IOException {
-
-        if (!verifyTokenService.verifyTokenSignature(userToken)) {
-            log.error(INVALID_TOKEN, userToken);
-            return ResponseEntity.status(FORBIDDEN.value()).build();
-        }
-
-        CaseDetails caseDetails = ccdRequest.getCaseDetails();
-        CaseData caseData = caseDetails.getCaseData();
-
-        String adminToken = adminUserService.getAdminUserToken();
-
-        SubmitEvent submitEvent = ccdClient.retrieveCase(adminToken, caseDetails.getCaseTypeId(), caseDetails.getJurisdiction(), caseData.getExampleCaseIdToLookup());
-
-        CaseData caseData1 = submitEvent.getCaseData();
-
-        log.error("Got details for " + caseData.getExampleCaseIdToLookup());
-
-        caseData.setExampleCaseDetails(caseData1.getClaimant());
-
-        return getCallbackRespEntityNoErrors(caseData);
-    }
-
     @PostMapping(value = "/leadCaseHearingDetails", consumes = APPLICATION_JSON_VALUE)
     @Operation(summary = "Retrieve ACAS Certificate from ACAS")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Accessed successfully",
-                    content = {
-                            @Content(mediaType = "application/json", schema = @Schema(implementation = CCDCallbackResponse.class))
-                    }),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+        @ApiResponse(responseCode = "200", description = "Accessed successfully",
+            content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = CCDCallbackResponse.class))
+            }),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
     })
     public ResponseEntity<MultipleCallbackResponse> leadCaseHearingDetails(
             @RequestBody MultipleRequest ccdRequest,
@@ -108,7 +73,8 @@ public class CaseLookupController {
         }
 
         String number = matcher.group(1);
-        SubmitEvent submitEvent = ccdClient.retrieveCase(adminToken, caseDetails.getCaseTypeId().replace("_Multiple", ""), caseDetails.getJurisdiction(), number);
+        SubmitEvent submitEvent = ccdClient.retrieveCase(adminToken, caseDetails.getCaseTypeId()
+                .replace("_Multiple", ""), caseDetails.getJurisdiction(), number);
 
         CaseData caseData1 = submitEvent.getCaseData();
 

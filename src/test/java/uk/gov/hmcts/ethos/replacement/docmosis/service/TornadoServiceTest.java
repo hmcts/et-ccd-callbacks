da6a3d59d560d5a921aca2cab653ebac061a4b0d
fergus.oshea <fergus.oshea@version1.com>
2024-02-29 11:19:25 +0000

    RET-4733: Update order of Inputs

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/TornadoServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/TornadoServiceTest.java
@@ -237,7 +237,7 @@ class TornadoServiceTest {
     void generateEt3VettingDocument() throws IOException {
         mockConnectionSuccess();
         CaseData caseData = new CaseData();
-        caseData.setEt3ChooseRespondent(DynamicFixedListType.from("Test Label", "Test Code", true));
+        caseData.setEt3ChooseRespondent(DynamicFixedListType.from("Test Code", "Test Label", true));
         DocumentInfo documentInfo = tornadoService.generateEventDocument(
                 caseData, AUTH_TOKEN, ENGLANDWALES_CASE_TYPE_ID, ET3_PROCESSING_PDF);
         verifyDocumentInfo(documentInfo);

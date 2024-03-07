845122c41cd8a96e6d1bc7b3fdfdc3ed507f026f
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-03-07 15:02:28 +0000

    Updates to have same versions of master branch

--- bin/docker-clean.sh
+++ bin/docker-clean.sh
@@ -3,7 +3,8 @@
 set -eu
 
 docker container stop $(docker container ls -a -q)
-
+docker system prune -a -f --volumes
 docker system prune -af
 docker volume prune -af
 docker builder prune -af
+docker volume prune -a -f
--- bin/pull-cftlib-images.sh
+++ bin/pull-cftlib-images.sh
@@ -12,9 +12,9 @@ docker pull hmctspublic.azurecr.io/hmcts/rse/rse-idam-simulator:latest
 docker pull postgres:12.4
 # Download elasticsearch
 docker pull docker.elastic.co/elasticsearch/elasticsearch:7.11.1
+# Download logstash
+docker pull docker.elastic.co/logstash/logstash:7.16.1
 # Download XUI Manage Cases
 docker pull hmctspublic.azurecr.io/xui/webapp:latest
 # Download XUI Manage Organisations
-docker pull hmctspublic.azurecr.io/xui/mo-webapp:latest
-# Download logstash
-docker pull docker.elastic.co/logstash/logstash:7.16.1
+docker pull hmctspublic.azurecr.io/xui/mo-webapp:l

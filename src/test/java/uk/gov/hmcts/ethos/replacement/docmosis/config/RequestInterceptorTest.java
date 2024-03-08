28b4c44d305019329d22ba2d61361f490edebd3e
Jack Reeve <jack@reevies.tech>
2024-03-08 11:52:23 +0000

    RET-0000: Added request interceptor for token verify service

---
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/config/RequestInterceptorTest.java
@@ -0,0 +1,60 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.config;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import uk.gov.hmcts.ethos.replacement.docmosis.config.interceptors.RequestInterceptor;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.VerifyTokenService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+class RequestInterceptorTest {
+
+    @Mock
+    private VerifyTokenService verifyTokenService;
+
+    private RequestInterceptor requestInterceptor;
+
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        requestInterceptor = new RequestInterceptor(verifyTokenService);
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+    }
+
+    @Test
+    void preHandle_ValidToken_ReturnsTrue() throws Exception {
+        when(verifyTokenService.verifyTokenSignature(anyString())).thenReturn(true);
+
+        request.addHeader(AUTHORIZATION, "");
+        boolean result = requestInterceptor.preHandle(request, response, new Object());
+
+        assertTrue(result);
+    }
+
+    @Test
+    void preHandle_InvalidToken_ReturnsFalseAndSetsErrorResponse() throws Exception {
+        when(verifyTokenService.verifyTokenSignature(anyString())).thenReturn(false);
+
+        boolean result = requestInterceptor.preHandle(request, response, new Object());
+
+        assertFalse(result);
+        assertEquals(HttpServletResponse.SC_FORBIDDEN, response.getStatus());
+        assertEquals("Failed to verify bearer token.", response.getErrorMessage());
+    }
+}

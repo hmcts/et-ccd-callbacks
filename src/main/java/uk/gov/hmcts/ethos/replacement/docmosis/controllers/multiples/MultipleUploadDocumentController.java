bd8ebe03f92800ef1a178ec083691c1052fdb301
JON PROSSER <jonprosser84@gmail.com>
2024-03-13 16:57:49 +0000

    RET-4699 added multiple controller for upload document event

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/multiples/MultipleUploadDocumentController.java
@@ -0,0 +1,104 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.controllers.multiples;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.et.common.model.multiples.MultipleCallbackResponse;
+import uk.gov.hmcts.et.common.model.multiples.MultipleData;
+import uk.gov.hmcts.et.common.model.multiples.MultipleRequest;
+import uk.gov.hmcts.ethos.replacement.docmosis.helpers.UploadDocumentHelper;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.CaseManagementForCaseWorkerService;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.VerifyTokenService;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.CallbackRespHelper.multipleResponse;
+
+/**
+ * REST controller for the Upload Document event page on a multiple.
+ */
+@Slf4j
+@RequestMapping("/multiples/uploadDocument")
+@RestController
+public class MultipleUploadDocumentController {
+
+    private static final String INVALID_TOKEN = "Invalid Token {}";
+    private final VerifyTokenService verifyTokenService;
+    private final CaseManagementForCaseWorkerService caseManagementForCaseWorkerService;
+
+    public MultipleUploadDocumentController(
+                                    VerifyTokenService verifyTokenService,
+                                    CaseManagementForCaseWorkerService caseManagementForCaseWorkerService) {
+
+        this.verifyTokenService = verifyTokenService;
+        this.caseManagementForCaseWorkerService = caseManagementForCaseWorkerService;
+    }
+
+    @PostMapping(value = "/aboutToStart", consumes = APPLICATION_JSON_VALUE)
+    @Operation(summary = "Convert's the legacy style of docs into the new doc naming system")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully",
+            content = {
+                @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = MultipleCallbackResponse.class))
+            }),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<MultipleCallbackResponse> aboutToStart(
+            @RequestBody MultipleRequest ccdRequest,
+            @RequestHeader(value = "Authorization") String userToken) {
+
+        if (!verifyTokenService.verifyTokenSignature(userToken)) {
+            return ResponseEntity.status(FORBIDDEN.value()).build();
+        }
+
+        MultipleData caseData = ccdRequest.getCaseDetails().getCaseData();
+
+        UploadDocumentHelper.convertLegacyDocsToNewDocNaming(caseData);
+
+        return multipleResponse(caseData, null);
+    }
+
+    /**
+     * Called at the end of Upload Document event.
+     * @param ccdRequest holds the request and case data
+     * @param userToken  used for authorization
+     * @return Callback response entity with case data and errors attached.
+     */
+    @PostMapping(value = "/aboutToSubmit", consumes = APPLICATION_JSON_VALUE)
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully",
+             content = {
+                 @Content(mediaType = "application/json",
+                      schema = @Schema(implementation = MultipleCallbackResponse.class))
+             }),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<MultipleCallbackResponse> aboutToSubmitReferralReply(
+            @RequestBody MultipleRequest ccdRequest,
+            @RequestHeader("Authorization") String userToken) {
+
+        if (!verifyTokenService.verifyTokenSignature(userToken)) {
+            log.error(INVALID_TOKEN, userToken);
+            return ResponseEntity.status(FORBIDDEN.value()).build();
+        }
+
+        MultipleData caseData = ccdRequest.getCaseDetails().getCaseData();
+
+        UploadDocumentHelper.setDocumentTypeForDocumentCollection(caseData);
+        caseManagementForCaseWorkerService.addClaimantDocuments(caseData);
+
+        return multipleResponse(caseData, null);
+    }
+}

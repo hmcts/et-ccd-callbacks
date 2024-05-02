93d487a5dd559e76d5d462790359d74051a6194e
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2024-05-02 14:31:58 +0100

    EXUI_CASE_DETAILS_LINK

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/constants/NotificationServiceConstants.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/constants/NotificationServiceConstants.java
@@ -8,6 +8,7 @@ public final class NotificationServiceConstants {
     public static final String CLAIMANT = "claimant";
     public static final String RESPONDENTS = "respondents";
     public static final String RESPONDENT_NAMES = "respondentNames";
+    public static final String EXUI_CASE_DETAILS_LINK = "exuiCaseDetailsLink";
     public static final String EXUI_HEARING_DOCUMENTS_LINK = "exuiHearingDocumentsLink";
     public static final String HEARING_DOCUMENTS_PATH = "#Hearing%20Documents";
     public static final String HEARING_DATE = "hearingDate";

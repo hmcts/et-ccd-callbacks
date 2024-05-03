1cc210ba7c23e5396d4b456ff434caa3080caa54
marcjudge <98343606+marcjudge@users.noreply.github.com>
2024-05-03 11:35:28 +0100

    Ret 4732 - Add notifications extract (#1883)
    * Add notifications extract
    
    * Add controller test
    
    * Add notification schedule service test
    
    * Add tests for multiple notifications helper
    
    * Add NotificationsExcelReportServiceTest
    
    * Add NotificationScheduleCallable test
    
    * Increase code coverage
    
    * Fix test issues
    
    * Bump et-common
    
    * PR comments

---
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/multiples/ExtractNotificationsControllerTest.java
@@ -0,0 +1,106 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.controllers.multiples;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.hmcts.ethos.replacement.docmosis.controllers.BaseControllerTest;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.multiples.NotificationsExcelReportService;
+import uk.gov.hmcts.ethos.replacement.docmosis.utils.JsonMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest({ExtractNotificationsController.class, JsonMapper.class})
+class ExtractNotificationsControllerTest extends BaseControllerTest {
+    private static final String ABOUT_TO_SUBMIT_URL = "/multiples/extractNotifications/aboutToSubmit";
+    private static final String SUBMITTED_URL = "/multiples/extractNotifications/submitted";
+
+    @MockBean
+    private NotificationsExcelReportService notificationsExcelReportService;
+
+    @Autowired
+    private WebApplicationContext applicationContext;
+
+    @Autowired
+    private MockMvc mockMvc;
+    private JsonNode requestContent;
+
+    private void doRequestSetUp() throws IOException, URISyntaxException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        requestContent = objectMapper.readTree(new File(getClass()
+                .getResource("/exampleBulkV1.json").toURI()));
+    }
+
+    @BeforeEach
+    void setUpTests() throws Exception {
+        super.setUp();
+        mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext).build();
+        doRequestSetUp();
+    }
+
+    @Test
+    void aboutToSubmit_ok() throws Exception {
+        when(verifyTokenService.verifyTokenSignature(AUTH_TOKEN)).thenReturn(true);
+        mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
+                        .content(requestContent.toString())
+                        .header("Authorization", AUTH_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(JsonMapper.DATA, notNullValue()))
+                .andExpect(jsonPath(JsonMapper.ERRORS, empty()))
+                .andExpect(jsonPath(JsonMapper.WARNINGS, nullValue()));
+    }
+
+    @Test
+    void aboutToSubmit_badToken() throws Exception {
+        when(verifyTokenService.verifyTokenSignature(AUTH_TOKEN)).thenReturn(false);
+        mockMvc.perform(post(ABOUT_TO_SUBMIT_URL)
+                        .content(requestContent.toString())
+                        .header("Authorization", AUTH_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void submitted_ok() throws Exception {
+        when(verifyTokenService.verifyTokenSignature(AUTH_TOKEN)).thenReturn(true);
+        mockMvc.perform(post(SUBMITTED_URL)
+                        .content(requestContent.toString())
+                        .header("Authorization", AUTH_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(JsonMapper.DATA, nullValue()))
+                .andExpect(jsonPath(JsonMapper.ERRORS, nullValue()))
+                .andExpect(jsonPath(JsonMapper.WARNINGS, nullValue()));
+    }
+
+    @Test
+    void submitted_badToken() throws Exception {
+        when(verifyTokenService.verifyTokenSignature(AUTH_TOKEN)).thenReturn(false);
+        mockMvc.perform(post(SUBMITTED_URL)
+                        .content(requestContent.toString())
+                        .header("Authorization", AUTH_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+}

f7a0bc907476716cc426e44e7fae2202f45cb4de
jonProsser <jonprosser84@gmail.com>
2024-05-10 12:51:36 +0100

    checkStyle fix

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/multiples/MultiplesDocumentAccessServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/multiples/MultiplesDocumentAccessServiceTest.java
@@ -12,7 +12,8 @@ import uk.gov.hmcts.et.common.model.multiples.MultipleData;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
 class MultiplesDocumentAccessServiceTest {

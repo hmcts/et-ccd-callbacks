72faa197fb41e7d7f6c30e38a1d0e4154565f1e9
jonProsser <jonprosser84@gmail.com>
2024-05-14 10:21:00 +0100

    fix lexo error

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/multiples/MultiplesDocumentAccessServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/multiples/MultiplesDocumentAccessServiceTest.java
@@ -12,9 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;

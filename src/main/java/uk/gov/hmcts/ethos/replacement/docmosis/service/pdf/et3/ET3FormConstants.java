58c24af85bcc1bbbd01d3da1b1445785abeeb319
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-06-06 16:14:11 +0100

    Updates for creation of PDF file with PDFBox

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/pdf/et3/ET3FormConstants.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/pdf/et3/ET3FormConstants.java
@@ -40,8 +40,8 @@ public final class ET3FormConstants {
     public static final String TXT_PDF_HEADER_FIELD_CASE_NUMBER = "case number";
     public static final String TXT_PDF_HEADER_FIELD_DATE_RECEIVED = "date_received";
     public static final String TXT_PDF_HEADER_FIELD_RFT = "RTF";
-    public static final String TXT_PDF_HEADER_VALUE_ADDITIONAL_DOCUMENT_EXISTS = "Additional document exists";
-    public static final String TXT_PDF_HEADER_VALUE_ADDITIONAL_DOCUMENT_NOT_EXISTS = "No additional document";
+    public static final String TXT_PDF_HEADER_VALUE_ADDITIONAL_DOCUMENT_EXISTS = "Document exists";
+    public static final String TXT_PDF_HEADER_VALUE_ADDITIONAL_DOCUMENT_NOT_EXISTS = "No document";
 
     // SECTION 1 CLAIMANT CONSTANTS
     public static final String TXT_PDF_CLAIMANT_FIELD_NAME = "1.1 Claimant's name";

9ade0aac3e93fd5fe6e64cd2f334b5224fbaa01f
Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>
2024-06-06 04:44:10 +0100

    implement aboutToStart endpoint

---
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/ClaimantTellSomethingElseControllerTest.java
@@ -0,0 +1,89 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.ethos.replacement.docmosis.helpers.Helper;
+import uk.gov.hmcts.ethos.replacement.docmosis.utils.JsonMapper;
+import uk.gov.hmcts.ethos.utils.CCDRequestBuilder;
+import uk.gov.hmcts.ethos.utils.CaseDataBuilder;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.NO;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest({ClaimantTellSomethingElseController.class, JsonMapper.class})
+class ClaimantTellSomethingElseControllerTest extends BaseControllerTest {
+    private static final String ABOUT_TO_START_URL = "/claimantTSE/aboutToStart";
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private JsonMapper jsonMapper;
+    private CCDRequest ccdRequest;
+    private MockedStatic mockHelper;
+
+    @BeforeEach
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        CaseData caseData = CaseDataBuilder.builder()
+                .withEthosCaseReference("test")
+                .withClaimant("claimant")
+                .build();
+        caseData.setResTseSelectApplication("caseRef");
+        caseData.setResTseCopyToOtherPartyYesOrNo(NO);
+
+        ccdRequest = CCDRequestBuilder.builder()
+                .withCaseData(caseData)
+                .withCaseId("123")
+                .build();
+
+        mockHelper = mockStatic(Helper.class);
+    }
+
+    @AfterEach
+    void afterEach() {
+        mockHelper.close();
+    }
+
+    @Test
+    void aboutToStartClaimantTSE_Success() throws Exception {
+        when(verifyTokenService.verifyTokenSignature(AUTH_TOKEN)).thenReturn(true);
+        mockMvc.perform(post(ABOUT_TO_START_URL)
+                        .contentType(APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, AUTH_TOKEN)
+                        .content(jsonMapper.toJson(ccdRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(JsonMapper.DATA, notNullValue()))
+                .andExpect(jsonPath(JsonMapper.ERRORS, nullValue()))
+                .andExpect(jsonPath(JsonMapper.WARNINGS, nullValue()));
+        mockHelper.verify(() -> Helper.isRespondentNonSystemUser(any()), times(1));
+    }
+
+    @Test
+    void aboutToStartClaimantTSE_badRequest() throws Exception {
+        mockMvc.perform(post(ABOUT_TO_START_URL)
+                        .content("garbage content")
+                        .header("Authorization", AUTH_TOKEN)
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+}

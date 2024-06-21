5d52a05cf07c5e138629088ba3beeeda11ea2c53
fergus.oshea <fergus.oshea@version1.com>
2024-06-21 12:25:51 +0100

    RET-5205: Need returnList to be mutable for MultipleCreationServiceTest

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/MultiplesHelper.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/helpers/MultiplesHelper.java
@@ -25,6 +25,7 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.ET1_ONLINE_CASE_SOURCE;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.MANUALLY_CREATED_POSITION;
@@ -55,7 +56,7 @@ public final class MultiplesHelper {
                     .filter(key -> key.getId() != null && !key.getId().equals("null"))
                     .map(caseId -> caseId.getValue().getEthosCaseReference())
                     .distinct()
-                    .toList();
+                    .collect(Collectors.toList());
 
         } else {
             return new ArrayList<>();

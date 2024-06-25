536d65823f99af481417ad163dbc2576c78dec26
Mehmet Tahir Dede <105645217+JustMehmet@users.noreply.github.com>
2024-06-25 16:22:15 +0100

    RET-5054 (#1918)
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates to create PDF file for ET3-Form by PDFBox framework of apache.
    
    * Updates for creation of PDF file with PDFBox
    
    * Updates for creation of PDF file with PDFBox
    
    * Updates for creation of PDF file with PDFBox
    
    * Updates for creation of PDF file with PDFBox
    
    * Updates for creation of PDF file with PDFBox
    
    * Updates for creation of PDF file with PDFBox
    
    * Updates for creation of PDF file with PDFBox
    
    * Removed jackson-databind suppression and added a resolution to build.gradle to use 2.16.0 version.
    
    * added jackson-databind suppression back and removed resolution in build.gradle to use 2.16.0 version.
    
    * added jackson-databind suppression back and removed resolution in build.gradle to use 2.16.0 version.
    
    * update for having PDF name filled even if the name field is empty.
    
    * update for having PDF name filled even if the name field is empty.
    
    * update for having PDF name filled even if the name field is empty.
    
    * Update to remove javascript validation of currency fields.
    
    * Update to remove javascript validation of currency fields.
    
    * Update to remove javascript validation of currency fields.
    
    * Update to remove javascript validation of currency fields.
    
    * Update to remove old ET3-Form creation scripts.
    
    * Update src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/pdf/et3/ET3FormConstants.java
    
    Co-authored-by: Harpreet Jhita <harpreet.jhita@justice.gov.uk>
    
    * Update for code review spots.
    
    * Update to the text sizes of currency fields.
    
    * Update to the text sizes of currency fields.
    
    * Added ET3 welsh version for later implementation
    
    * Added Time new Romans and Helvetica characters to PDF resources.
    
    * Update to sample pdf byte array size that is created by PdfBoxServiceTest.
    
    * Update to resolve issues mentioned in Jira task RET-5055 (https://tools.hmcts.net/jira/browse/RET-5054)
    
    * Update to resolve issues mentioned in Jira task RET-5055 (https://tools.hmcts.net/jira/browse/RET-5054)
    
    ---------
    
    Co-authored-by: Harpreet Jhita <harpreet.jhita@justice.gov.uk>

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/pdf/util/PdfBoxServiceUtil.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.service.pdf.util;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.pdf.PdfBoxServiceConstants.CHARACTER_DOT;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.pdf.PdfBoxServiceConstants.CURRENCY_DECIMAL_ZERO_WITH_DOT;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.pdf.PdfBoxServiceConstants.ONE;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.pdf.PdfBoxServiceConstants.STRING_ZERO;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.pdf.PdfBoxServiceConstants.TWO;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.pdf.PdfBoxServiceConstants.ZERO;
+import static uk.gov.hmcts.ethos.replacement.docmosis.service.pdf.et3.ET3FormConstants.STRING_EMPTY;
+
+public final class PdfBoxServiceUtil {
+
+    private PdfBoxServiceUtil() {
+        // Utility classes should not have a public or default constructor.
+    }
+
+    /**
+     * Converts given value to currency. Value is coming from frontend with extra 2 ZEROs. That is why
+     * we are adding a dot(.) in between last 2 digits to format it as currency.
+     * Code checks if the entered value has any (.). If it has returns the value and checks if the value's length is
+     * less than 2. If it is less than 2 returns the value adding .00, If value is empty then returns an empty string.
+     * @param value entered by user but added extra 2 zeros.
+     * @return string that has a dot(.) before the ending 2 zeros.
+     */
+    public static String correctCurrency(String value) {
+        if (isBlank(value)) {
+            return STRING_EMPTY;
+        }
+        if (value.indexOf(CHARACTER_DOT) == ZERO) {
+            return STRING_ZERO + value;
+        }
+        if (value.indexOf(CHARACTER_DOT) >= ONE) {
+            return value;
+        }
+        if (value.length() < TWO) {
+            return value + CURRENCY_DECIMAL_ZERO_WITH_DOT;
+        }
+        return value.substring(0, value.length() - 2) + CHARACTER_DOT + value.substring(value.length() - TWO);
+    }
+}

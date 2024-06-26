9e87ec5a6215d4ddddde56ef99750b3753b7bbe1
mehmet.dede <mehmet.dede@justice.gov.uk>
2024-06-26 14:36:55 +0100

    Updates after merge with master branch

--- bin/pull-cftlib-images.sh
+++ bin/pull-cftlib-images.sh
@@ -6,8 +6,7 @@ set -eu
 az acr login --name hmctsprivate --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 
-#CFTLIB IMAGES
-# Download Idam
+# CFTLIB IMAGES
 docker pull hmctspublic.azurecr.io/hmcts/rse/rse-idam-simulator:latest
 # Download Postgres
 docker pull postgres:12.4
@@ -16,9 +15,10 @@ docker pull docker.elastic.co/elasticsearch/elasticsearch:7.11.1
 # Download logstash
 docker pull docker.elastic.co/logstash/logstash:7.16.1
 # Download XUI Manage Cases
-docker pull hmctspublic.azurecr.io/xui/webapp:pr-3507
+docker pull hmctspublic.azurecr.io/xui/webapp:latest
 # Download XUI Manage Organisations
 docker pull hmctspublic.azurecr.io/xui/mo-webapp:latest
+
 # COMPOSE IMAGES
 # Download azurite latest version
 docker pull mcr.microsoft.com/azure-storage/azurite:latest

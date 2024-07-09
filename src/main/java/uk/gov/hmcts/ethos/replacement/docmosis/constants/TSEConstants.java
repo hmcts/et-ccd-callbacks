636cd8db6d10aada24309e716e80960e2c3c561e
Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>
2024-07-09 09:46:29 +0100

    addressed comments

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/constants/TSEConstants.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/constants/TSEConstants.java
@@ -5,7 +5,6 @@ public final class TSEConstants {
     public static final String GIVE_DETAIL_MISSING = "Please upload a document or provide details in the text box.";
     public static final String CLAIMANT_TSE_WITHDRAW_CLAIM = "Withdraw all or part of claim";
     public static final String CLAIMANT_REP_TITLE = "Claimant Representative";
-    public static final String MISSING_DETAILS = "Please upload a document or provide details in the text box.";
     public static final String APPLICATION_COMPLETE_RULE92_ANSWERED_NO = "<hr>"
             + "<h2>What happens next</h2>"
             + "<p>The tribunal will consider all correspondence and let you know what happens next.</p>"

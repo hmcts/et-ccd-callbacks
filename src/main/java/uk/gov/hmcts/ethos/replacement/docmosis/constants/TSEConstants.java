8d7ea9e20081ec8bd7d0ebc717e6736fb8f47895
Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>
2024-07-10 15:17:45 +0100

    added doc markup for every flow

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/constants/TSEConstants.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/constants/TSEConstants.java
@@ -8,7 +8,7 @@ public final class TSEConstants {
     public static final String APPLICATION_COMPLETE_RULE92_ANSWERED_NO = "<hr>"
             + "<h2>What happens next</h2>"
             + "<p>The tribunal will consider all correspondence and let you know what happens next.</p>"
-            + "<hr>";
+            + "<h3>%s</h3>";
     public static final String APPLICATION_COMPLETE_RULE92_ANSWERED_YES_RESP_OFFLINE = "<hr>"
                     + "<h2>Copy this correspondence to the other party</h2>"
                     + "<p>You must submit your application after copying the correspondence to the other party.</p>"
@@ -22,7 +22,7 @@ public final class TSEConstants {
                             + "<p>If they do respond, they are expected to copy their response to you.</p>"
                             + "<p>You may be asked to supply further information. "
                             + "The tribunal will consider all correspondence and let you know what happens next.</p>"
-                            + "<hr>";
+                            + "<h3>%s</h3>";
 
     private TSEConstants() {
         // Access through static methods

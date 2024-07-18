05ac8f4a50ba9422d46e6f05a0f7b71318908fec
marcjudge <98343606+marcjudge@users.noreply.github.com>
2024-07-18 16:33:53 +0100

    RET-5266 - Filter live cases (#2116)
    * Filter live cases
    
    * Add additional tests
    
    * Add java doc

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/MultipleScheduleServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/excel/MultipleScheduleServiceTest.java
@@ -3,15 +3,19 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service.excel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.ecm.common.model.helper.SchedulePayload;
 import uk.gov.hmcts.ecm.common.model.schedule.SchedulePayloadEvent;
 import uk.gov.hmcts.et.common.model.ccd.items.RespondentSumTypeItem;
 import uk.gov.hmcts.et.common.model.ccd.types.RespondentSumType;
 import uk.gov.hmcts.et.common.model.multiples.MultipleDetails;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.MultipleUtil;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.MultiplesScheduleHelper;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.FeatureToggleService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +24,7 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyList;
@@ -27,7 +32,9 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.CLOSED_STATE;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.LIST_CASES_CONFIG;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.YES;
 import static uk.gov.hmcts.ethos.replacement.docmosis.service.excel.MultipleScheduleService.SCHEDULE_LIMIT_CASES;
 
 @ExtendWith(SpringExtension.class)
@@ -39,8 +46,13 @@ class MultipleScheduleServiceTest {
     private SingleCasesReadingService singleCasesReadingService;
     @Mock
     private ExcelDocManagementService excelDocManagementService;
+    @Mock
+    FeatureToggleService featureToggleService;
     @InjectMocks
     private MultipleScheduleService multipleScheduleService;
+
+    @Captor
+    ArgumentCaptor<List<SchedulePayload>> schedulePayloads;
 
     private SortedMap<String, Object> multipleObjectsFlags;
     private SortedMap<String, Object> multipleObjectsSubMultiple;
@@ -74,6 +86,83 @@ class MultipleScheduleServiceTest {
                 multipleDetails.getCaseTypeId(),
                 new ArrayList<>(multipleObjectsFlags.keySet()));
         verifyNoMoreInteractions(singleCasesReadingService);
+    }
+
+    @Test
+    void bulkScheduleLiveCasesFilter() {
+        schedulePayloadEvents.iterator().next().getSchedulePayloadES().setClaimantCompany(null);
+        when(featureToggleService.isMultiplesEnabled()).thenReturn(true);
+        multipleDetails.getCaseData().setLiveCases(YES);
+        when(excelReadingService.readExcel(anyString(), anyString(), anyList(), any(), any()))
+                .thenReturn(multipleObjectsFlags);
+        schedulePayloadEvents.iterator().next().setState(CLOSED_STATE);
+        when(singleCasesReadingService.retrieveScheduleCases(userToken,
+                multipleDetails.getCaseTypeId(),
+                new ArrayList<>(multipleObjectsFlags.keySet())))
+                .thenReturn(schedulePayloadEvents);
+
+        multipleScheduleService.bulkScheduleLogic(userToken,
+                multipleDetails,
+                new ArrayList<>());
+        verify(singleCasesReadingService, times(1)).retrieveScheduleCases(userToken,
+                multipleDetails.getCaseTypeId(),
+                new ArrayList<>(multipleObjectsFlags.keySet()));
+        verifyNoMoreInteractions(singleCasesReadingService);
+        verify(excelDocManagementService, times(1))
+                .writeAndUploadScheduleDocument(any(), any(), any(), schedulePayloads.capture());
+        List<SchedulePayload> schedulePayloadList = schedulePayloads.getValue();
+        assertEquals(1, schedulePayloadList.size());
+        assertNull(multipleDetails.getCaseData().getLiveCases());
+    }
+
+    @Test
+    void bulkScheduleLiveCasesFilterNoClosedCases() {
+        schedulePayloadEvents.iterator().next().getSchedulePayloadES().setClaimantCompany(null);
+        when(featureToggleService.isMultiplesEnabled()).thenReturn(true);
+        when(excelReadingService.readExcel(anyString(), anyString(), anyList(), any(), any()))
+                .thenReturn(multipleObjectsFlags);
+        schedulePayloadEvents.iterator().next().setState(CLOSED_STATE);
+        when(singleCasesReadingService.retrieveScheduleCases(userToken,
+                multipleDetails.getCaseTypeId(),
+                new ArrayList<>(multipleObjectsFlags.keySet())))
+                .thenReturn(schedulePayloadEvents);
+
+        multipleScheduleService.bulkScheduleLogic(userToken,
+                multipleDetails,
+                new ArrayList<>());
+        verify(singleCasesReadingService, times(1)).retrieveScheduleCases(userToken,
+                multipleDetails.getCaseTypeId(),
+                new ArrayList<>(multipleObjectsFlags.keySet()));
+        verifyNoMoreInteractions(singleCasesReadingService);
+        verify(excelDocManagementService, times(1))
+                .writeAndUploadScheduleDocument(any(), any(), any(), schedulePayloads.capture());
+        List<SchedulePayload> schedulePayloadList = schedulePayloads.getValue();
+        assertEquals(2, schedulePayloadList.size());
+    }
+
+    @Test
+    void bulkScheduleLiveCasesFilterFeatureDisabled() {
+        schedulePayloadEvents.iterator().next().getSchedulePayloadES().setClaimantCompany(null);
+        when(featureToggleService.isMultiplesEnabled()).thenReturn(false);
+        when(excelReadingService.readExcel(anyString(), anyString(), anyList(), any(), any()))
+                .thenReturn(multipleObjectsFlags);
+        schedulePayloadEvents.iterator().next().setState(CLOSED_STATE);
+        when(singleCasesReadingService.retrieveScheduleCases(userToken,
+                multipleDetails.getCaseTypeId(),
+                new ArrayList<>(multipleObjectsFlags.keySet())))
+                .thenReturn(schedulePayloadEvents);
+
+        multipleScheduleService.bulkScheduleLogic(userToken,
+                multipleDetails,
+                new ArrayList<>());
+        verify(singleCasesReadingService, times(1)).retrieveScheduleCases(userToken,
+                multipleDetails.getCaseTypeId(),
+                new ArrayList<>(multipleObjectsFlags.keySet()));
+        verifyNoMoreInteractions(singleCasesReadingService);
+        verify(excelDocManagementService, times(1))
+                .writeAndUploadScheduleDocument(any(), any(), any(), schedulePayloads.capture());
+        List<SchedulePayload> schedulePayloadList = schedulePayloads.getValue();
+        assertEquals(2, schedulePayloadList.size());
     }
 
     @Test

21b030956592a039399383aa5c58dd88246b00e0
btensay <tensay.bulcha@gmail.com>
2024-07-25 10:54:35 +0100

    check for system user status added

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/SendNotificationService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/SendNotificationService.java
@@ -48,6 +48,7 @@ import static uk.gov.hmcts.ethos.replacement.docmosis.constants.NotificationServ
 import static uk.gov.hmcts.ethos.replacement.docmosis.constants.NotificationServiceConstants.LINK_TO_EXUI;
 import static uk.gov.hmcts.ethos.replacement.docmosis.constants.NotificationServiceConstants.RESPONDENT_NAMES;
 import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.Helper.createLinkForUploadedDocument;
+import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.Helper.isClaimantNonSystemUser;
 import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.Helper.isRepresentedClaimantWithMyHmctsCase;
 
 @Service("sendNotificationService")
@@ -237,7 +238,8 @@ public class SendNotificationService {
             } else {
                 // If not represented, send notification to the claimant Only
                 String claimantEmailAddress = caseData.getClaimantType().getClaimantEmailAddress();
-                if (!isNullOrEmpty(claimantEmailAddress)) {
+                // Send notification to the claimant only if the claimant is a system user
+                if (!isClaimantNonSystemUser(caseData) && !isNullOrEmpty(claimantEmailAddress)) {
                     emailService.sendEmail(claimantSendNotificationTemplateId, claimantEmailAddress,
                             buildPersonalisation(caseDetails, emailService.getCitizenCaseLink(caseId)));
                 }

aaa0b47701c3e7fc0d27a5156b2df72c8b9cfb87
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-07-29 12:31:48 +0000

    Updating Terraform Formatting

--- infrastructure/database-flexi.tf
+++ infrastructure/database-flexi.tf
@@ -18,7 +18,7 @@ module "postgres" {
   pgsql_version                  = "15"
   admin_user_object_id           = var.jenkins_AAD_objectId
   force_user_permissions_trigger = "1"
-  geo_redundant_backups = true
+  geo_redundant_backups          = true
 }
 
 resource "azurerm_key_vault_secret" "et_cos_postgres_user_v15" {

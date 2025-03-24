c71c3532b95840733c7721062145f1a1f1f3ffb4
justmehmet <mehmet.dede@justice.gov.uk>
2025-03-24 11:29:51 +0000

    Update to download xuiwebapp without work allocation.

--- bin/pull-cftlib-images.sh
+++ bin/pull-cftlib-images.sh
@@ -15,7 +15,11 @@ docker pull docker.elastic.co/elasticsearch/elasticsearch:7.11.1
 # Download logstash
 docker pull docker.elastic.co/logstash/logstash:7.16.1
 # Download XUI Manage Cases
-docker pull hmctspublic.azurecr.io/xui/webapp:latest
+# With work allocation
+#docker pull hmctspublic.azurecr.io/xui/webapp:latest
+# Without work allocation
+docker pull tdmehmet/xui-webapp:withoutWA
+docker tag tdmehmet/xui-webapp:withoutWA hmctspublic.azurecr.io/xui/webapp:latest
 # Download XUI Manage Organisations
 docker pull hmctspublic.azurecr.io/xui/mo-webapp:latest
 

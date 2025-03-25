1b3ef2303b43fd13440e0a3f0e03af970dfac350
justmehmet <mehmet.dede@justice.gov.uk>
2025-03-25 11:28:57 +0000

    Update to download xuiwebapp without work allocation.

--- bin/pull-cftlib-images.sh
+++ bin/pull-cftlib-images.sh
@@ -3,8 +3,7 @@
 set -eu
 
 # Login to azure
-az acr login --name hmctsprivate --subscription 8999dec3-0104-4a27-94ee-6588559729d1
-az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
+az acr login --name hmctspublic && az acr login --name hmctsprivate
 
 # Download Idam
 docker pull hmctspublic.azurecr.io/hmcts/rse/rse-idam-simulator:latest

4beefdf4d161f2e9d824a5c9e7c99594a7f9c557
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-05-20 15:22:26 +0100

    RET-5764 (#2637)
    * Update ACAS Cert Retrieval to get multiple certs in one go rather than multiple calls
    
    * Remove test log line
    
    * Test for Sonar
    
    * Updates to make call multi threaded
    
    * Checkstyle
    
    * PMD
    
    * Changes
    
    * Changes
    
    * Checkstyle
    
    * Change
    
    * Changes to make ACAS cert retrieval a background job
    
    * Add missing property in integration tests
    
    ---------
    
    Co-authored-by: Lahiru Oshara Hinguruduwa <35135059+LahiruOshara@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/tasks/AcasCertificateTask.java
@@ -0,0 +1,112 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.tasks;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.index.query.TermsQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.Scheduled;
+import uk.gov.hmcts.ecm.common.client.CcdClient;
+import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
+import uk.gov.hmcts.et.common.model.ccd.SubmitEvent;
+import uk.gov.hmcts.et.common.model.ccd.items.DocumentTypeItem;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.AdminUserService;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.Et1SubmissionService;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.FeatureToggleService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.EMPLOYMENT;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.NO;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.YES;
+import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.DocumentHelper.setDocumentNumbers;
+
+@ComponentScan({"uk.gov.hmcts.ethos"})
+@RequiredArgsConstructor
+@Slf4j
+public class AcasCertificateTask {
+    private final Et1SubmissionService et1SubmissionService;
+    private final AdminUserService adminUserService;
+    private final CcdClient ccdClient;
+    private final FeatureToggleService featureToggleService;
+
+    @Value("${cron.caseTypeId}")
+    private String caseTypeIdsString;
+
+    @Value("${cron.maxCasesPerSearch}")
+    private int maxCases;
+
+    @Scheduled(cron = "${cron.acasCertSubmission}")
+    public void generateAcasCertificates() {
+        if (!featureToggleService.isAcasCertificatePostSubmissionEnabled()) {
+            return;
+        }
+        String query = buildQuery();
+        String adminUserToken = adminUserService.getAdminUserToken();
+        String[] caseTypeIds = caseTypeIdsString.split(",");
+
+        for (String caseTypeId : caseTypeIds) {
+            try {
+                List<SubmitEvent> cases = ccdClient.buildAndGetElasticSearchRequest(adminUserToken, caseTypeId, query);
+                log.info("{} - Acas certificate task - Retrieved {} cases", caseTypeId, cases.size());
+                while (!cases.isEmpty()) {
+                    cases.forEach(c -> triggerEventForCase(adminUserToken, c, caseTypeId));
+                    cases = ccdClient.buildAndGetElasticSearchRequest(adminUserToken, caseTypeId, query);
+                    log.info("{} - Acas certificate task - Retrieved {} cases", caseTypeId, cases.size());
+                }
+            } catch (IOException e) {
+                log.error("Error retrieving cases for case type ID {}: {}", caseTypeId, e.getMessage(), e);
+            }
+        }
+    }
+
+    private void triggerEventForCase(String adminUserToken, SubmitEvent submitEvent, String caseTypeId) {
+        try {
+            CCDRequest ccdRequest = ccdClient.startEventForCase(adminUserToken, caseTypeId, EMPLOYMENT,
+                    String.valueOf(submitEvent.getCaseId()), "UPDATE_CASE_SUBMITTED");
+            CaseDetails caseDetails = ccdRequest.getCaseDetails();
+            CaseData caseData = caseDetails.getCaseData();
+            if (caseData.getAcasCertificateRequired() == null || !YES.equals(caseData.getAcasCertificateRequired())) {
+                return;
+            }
+            List<DocumentTypeItem> acasCertificates =
+                    et1SubmissionService.retrieveAndAddAcasCertificates(caseData, adminUserToken, caseTypeId);
+            if (isNotEmpty(acasCertificates)) {
+                if (isEmpty(caseData.getDocumentCollection())) {
+                    caseData.setDocumentCollection(new ArrayList<>());
+                }
+                caseData.getDocumentCollection().addAll(acasCertificates);
+                setDocumentNumbers(caseData);
+                if (isEmpty(caseData.getClaimantDocumentCollection())) {
+                    caseData.setClaimantDocumentCollection(new ArrayList<>());
+                }
+                caseData.getClaimantDocumentCollection().addAll(acasCertificates);
+            }
+            caseData.setAcasCertificateRequired(NO);
+            ccdClient.submitEventForCase(adminUserToken, caseData, caseTypeId,
+                    EMPLOYMENT, ccdRequest, String.valueOf(submitEvent.getCaseId()));
+            log.info("Added Acas certificate for case {}", submitEvent.getCaseId());
+        } catch (Exception e) {
+            log.error("Error triggering event for case ID {}: {}", submitEvent.getCaseId(), e.getMessage(), e);
+        }
+    }
+
+    private String buildQuery() {
+        return new SearchSourceBuilder()
+                .size(maxCases)
+                .query(new BoolQueryBuilder()
+                        .must(new TermsQueryBuilder("state.keyword", "Submitted"))
+                        .must(new TermQueryBuilder("data.acasCertificateRequired", YES))
+                ).toString();
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/tasks/AcasCertificateTaskTest.java
@@ -0,0 +1,125 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.tasks;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import uk.gov.hmcts.ecm.common.client.CcdClient;
+import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.SubmitEvent;
+import uk.gov.hmcts.et.common.model.ccd.items.DocumentTypeItem;
+import uk.gov.hmcts.et.common.model.ccd.types.DocumentType;
+import uk.gov.hmcts.et.common.model.ccd.types.UploadedDocumentType;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.AdminUserService;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.Et1SubmissionService;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.FeatureToggleService;
+import uk.gov.hmcts.ethos.replacement.docmosis.utils.ResourceLoader;
+import uk.gov.hmcts.ethos.replacement.docmosis.utils.UploadedDocumentBuilder;
+import uk.gov.hmcts.ethos.utils.CCDRequestBuilder;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static uk.gov.hmcts.ecm.common.constants.PdfMapperConstants.NO;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.EMPLOYMENT;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.DocumentConstants.ACAS_CERTIFICATE;
+import static uk.gov.hmcts.ecm.common.model.helper.DocumentConstants.STARTING_A_CLAIM;
+
+@ExtendWith(SpringExtension.class)
+class AcasCertificateTaskTest {
+    private AcasCertificateTask acasCertificateTask;
+    @MockBean
+    private AdminUserService adminUserService;
+    @MockBean
+    private CcdClient ccdClient;
+    @MockBean
+    private FeatureToggleService featureToggleService;
+    @MockBean
+    private Et1SubmissionService et1SubmissionService;
+    @Captor
+    private ArgumentCaptor<CaseData> caseDataArgumentCaptor;
+
+    @BeforeEach
+    void setUp() {
+        acasCertificateTask = new AcasCertificateTask(
+                et1SubmissionService,
+                adminUserService,
+                ccdClient,
+                featureToggleService
+        );
+        when(adminUserService.getAdminUserToken()).thenReturn("AuthToken");
+        when(featureToggleService.isAcasCertificatePostSubmissionEnabled()).thenReturn(true);
+        ReflectionTestUtils.setField(acasCertificateTask, "caseTypeIdsString", "ET_EnglandWales,ET_Scotland");
+        ReflectionTestUtils.setField(acasCertificateTask, "maxCases", 10);
+    }
+
+    @Test
+    void testEt1DocGen_featureOff() throws Exception {
+        when(featureToggleService.isAcasCertificatePostSubmissionEnabled()).thenReturn(false);
+        acasCertificateTask.generateAcasCertificates();
+        // Verify that the method does not proceed with the task when the feature is off
+        verify(et1SubmissionService, times(0)).retrieveAndAddAcasCertificates(any(), any(), any());
+        verify(ccdClient, times(0)).startEventForCase(any(), any(), any(), any(), any());
+    }
+
+    @Test
+    void testEt1DocGen_featureOn() throws Exception {
+        String resource = ResourceLoader.getResource("et1GenerateDocTask.json");
+        SubmitEvent submitEvent = new ObjectMapper().readValue(resource, SubmitEvent.class);
+        when(ccdClient.buildAndGetElasticSearchRequest(any(), eq(ENGLANDWALES_CASE_TYPE_ID), any()))
+                .thenReturn(List.of(submitEvent)).thenReturn(Collections.emptyList());
+        when(ccdClient.buildAndGetElasticSearchRequest(any(), eq(SCOTLAND_CASE_TYPE_ID), any()))
+                .thenReturn(Collections.emptyList()).thenReturn(Collections.emptyList());
+
+        CaseData caseData = submitEvent.getCaseData();
+        CCDRequest ccdRequest = CCDRequestBuilder.builder()
+                .withCaseData(caseData)
+                .build();
+        ccdRequest.getCaseDetails().setJurisdiction(EMPLOYMENT);
+        when(ccdClient.startEventForCase(any(), any(), any(), any(), any())).thenReturn(ccdRequest);
+        when(et1SubmissionService.retrieveAndAddAcasCertificates(any(), any(), any()))
+                .thenReturn(createAcasDocumentList());
+        acasCertificateTask.generateAcasCertificates();
+        verify(ccdClient, times(1)).startEventForCase(any(), any(), any(), any(), any());
+        verify(ccdClient, times(1)).submitEventForCase(eq("AuthToken"), caseDataArgumentCaptor.capture(),
+                eq(ENGLANDWALES_CASE_TYPE_ID), eq(EMPLOYMENT), any(), eq("1712760487710259"));
+        CaseData caseDataCaptured = caseDataArgumentCaptor.getValue();
+        assertEquals(NO, caseDataCaptured.getAcasCertificateRequired());
+        assertTrue(isNotEmpty(caseDataCaptured.getDocumentCollection()));
+        assertTrue(isNotEmpty(caseDataCaptured.getClaimantDocumentCollection()));
+    }
+
+    private List<DocumentTypeItem> createAcasDocumentList() {
+        UploadedDocumentType uploadedDocumentType = UploadedDocumentBuilder.builder()
+                .withFilename("ACAS_Certificate.pdf")
+                .withUrl("http://example.com/ACAS_Certificate.pdf")
+                .withUuid(UUID.randomUUID().toString())
+                .build();
+
+        DocumentType documentType = new DocumentType();
+        documentType.setTopLevelDocuments(STARTING_A_CLAIM);
+        documentType.setStartingClaimDocuments(ACAS_CERTIFICATE);
+        documentType.setUploadedDocument(uploadedDocumentType);
+        DocumentTypeItem documentTypeItem = new DocumentTypeItem();
+        documentTypeItem.setId(UUID.randomUUID().toString());
+        documentTypeItem.setValue(documentType);
+        return List.of(documentTypeItem);
+    }
+}

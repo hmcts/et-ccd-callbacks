488998ee57d9f23e8c9479c0aab92119bcb27130
lahiru.hinguruduwa <oshara19@gmail.com>
2025-06-11 17:26:45 +0100

    refactor org logic for claimant rep

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/NocClaimantRepresentativeService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/NocClaimantRepresentativeService.java
@@ -6,13 +6,13 @@ import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ecm.common.idam.models.UserDetails;
+import uk.gov.hmcts.et.common.model.ccd.AuditEvent;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
 import uk.gov.hmcts.et.common.model.ccd.items.RepresentedTypeRItem;
-import uk.gov.hmcts.et.common.model.ccd.types.Organisation;
+import uk.gov.hmcts.et.common.model.ccd.types.ChangeOrganisationRequest;
 import uk.gov.hmcts.et.common.model.ccd.types.OrganisationsResponse;
 import uk.gov.hmcts.et.common.model.ccd.types.RepresentedTypeC;
 import uk.gov.hmcts.et.common.model.ccd.types.RepresentedTypeR;
@@ -27,10 +27,13 @@ import static uk.gov.hmcts.ethos.replacement.docmosis.service.Et1ReppedService.g
 @RequiredArgsConstructor
 @Slf4j
 public class NocClaimantRepresentativeService {
+    public static final String NOC_REQUEST = "nocRequest";
+
     private final UserIdamService userIdamService;
     private final AuthTokenGenerator authTokenGenerator;
     private final OrganisationClient organisationClient;
     private final AdminUserService adminUserService;
+    private final NocCcdService nocCcdService;
 
     /**
      * Update claimant representation based on NoC request.
@@ -40,49 +43,58 @@ public class NocClaimantRepresentativeService {
      */
     public CaseData updateClaimantRepresentation(CaseDetails caseDetails, String userToken) throws IOException {
         CaseData caseData = caseDetails.getCaseData();
-        updateClaimantRepMap(caseData, userToken);
+        String caseId = caseDetails.getCaseId();
+        updateClaimantRepMap(caseData, caseId);
+        prepopulateOrgAddress(caseData, userToken);
+
         return caseData;
     }
 
-    private void updateClaimantRepMap(CaseData caseData, String userToken) throws IOException {
-        RepresentedTypeC claimantRep = createRepresentedTypeC(userToken);
+    private void updateClaimantRepMap(CaseData caseData, String caseId) throws IOException {
+        RepresentedTypeC claimantRep = createRepresentedTypeC(caseId, caseData.getChangeOrganisationRequestField());
         caseData.setClaimantRepresentedQuestion(YES);
         caseData.setRepresentativeClaimantType(claimantRep);
     }
 
-    private RepresentedTypeC createRepresentedTypeC(String userToken) throws IOException {
-        UserDetails userDetails = userIdamService.getUserDetails(userToken);
+    private RepresentedTypeC createRepresentedTypeC(String caseId, ChangeOrganisationRequest change) throws IOException {
+        String accessToken = adminUserService.getAdminUserToken();
+
+        Optional<AuditEvent> auditEvent =
+                nocCcdService.getLatestAuditEventByName(accessToken, caseId, NOC_REQUEST);
+        Optional<UserDetails> userDetailsOptional = auditEvent
+                .map(event -> adminUserService.getUserDetails(event.getUserId()));
+
+        UserDetails userDetails = userDetailsOptional.orElseThrow();
+
         RepresentedTypeC claimantRep = new RepresentedTypeC();
         claimantRep.setNameOfRepresentative(userDetails.getFirstName() + " " + userDetails.getLastName());
         claimantRep.setRepresentativeEmailAddress(userDetails.getEmail());
-        OrganisationsResponse organisationDetails = getOrganisationDetailsFromUserId(userDetails.getUid());
-        claimantRep.setMyHmctsOrganisation(Organisation.builder()
-                .organisationID(organisationDetails.getOrganisationIdentifier())
-                .organisationName(organisationDetails.getName())
-                .build());
-        claimantRep.setNameOfOrganisation(organisationDetails.getName());
-        claimantRep.setRepresentativeAddress(getOrganisationAddress(organisationDetails));
+        claimantRep.setMyHmctsOrganisation(change.getOrganisationToAdd());
 
         return claimantRep;
     }
 
-    public OrganisationsResponse getOrganisationDetailsFromUserId(String userId) {
-        try {
-            String userToken = adminUserService.getAdminUserToken();
-            ResponseEntity<OrganisationsResponse> response =
-                    organisationClient.retrieveOrganisationDetailsByUserId(userToken,
-                            authTokenGenerator.generate(),
-                            userId);
-            if (response.getStatusCode().is2xxSuccessful()) {
-                return response.getBody();
+    private void prepopulateOrgAddress(CaseData caseData, String userToken) {
+        List<OrganisationsResponse> organisationList = organisationClient.getOrganisations(
+                userToken, authTokenGenerator.generate());
+
+        RepresentedTypeC claimantRep = caseData.getRepresentativeClaimantType();
+        if (claimantRep != null && claimantRep.getMyHmctsOrganisation() != null) {
+            String orgId = claimantRep.getMyHmctsOrganisation().getOrganisationID();
+            Optional<OrganisationsResponse> organisationResponse = organisationList.stream()
+                    .filter(org -> org.getOrganisationIdentifier().equals(orgId))
+                    .findFirst();
+
+            if (organisationResponse.isPresent()) {
+                OrganisationsResponse organisation = organisationResponse.get();
+                claimantRep.setRepresentativeAddress(getOrganisationAddress(organisation));
+                caseData.setRepresentativeClaimantType(claimantRep);
             }
-        } catch (Exception e) {
-            log.error("Failed to retrieve organisation details", e);
         }
-        return null;
     }
 
-    private List<RepresentedTypeRItem> updateRepCollection(CaseData caseData, RepresentedTypeR claimantRep) {
+
+        private List<RepresentedTypeRItem> updateRepCollection(CaseData caseData, RepresentedTypeR claimantRep) {
         List<RepresentedTypeRItem> repCollection = Optional.ofNullable(caseData.getRepCollection())
                 .orElse(new ArrayList<>());
         RepresentedTypeRItem repItem = new RepresentedTypeRItem();

fba5b4bff6db3afa236457a1fe60100a57a7b113
btensay <tensay.bulcha@gmail.com>
2025-07-21 15:37:45 +0100

    task runner class added

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/DocmosisApplication.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/DocmosisApplication.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.ethos.replacement.docmosis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.ScheduledTaskRunner;
 
 @SpringBootApplication(scanBasePackages = {"uk.gov.hmcts.ethos", "uk.gov.hmcts.ecm.common"})
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.ethos.replacement"})
@@ -11,7 +13,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, this is not a utility class
 public class DocmosisApplication {
 
+    @Autowired
+    ScheduledTaskRunner taskRunner;
+
     public static void main(String[] args) {
+
         SpringApplication.run(DocmosisApplication.class, args);
     }
 

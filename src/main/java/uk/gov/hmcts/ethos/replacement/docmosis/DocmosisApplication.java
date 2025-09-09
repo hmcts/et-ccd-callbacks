c6f1ce48d870808cf292c6d378f667662e43762e
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-09-09 09:10:09 +0100

    Spring Caching (#2820)
    * Implement caching for AdminUserService and update user details retrieval method
    
    * Add caching configuration for AdminUserService in application.yaml
    
    * Add test caching configuration for AdminUserService
    
    ---------
    
    Co-authored-by: Mehmet Tahir Dede <105645217+JustMehmet@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/DocmosisApplication.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/DocmosisApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.ScheduledTaskRunner;
@@ -16,6 +17,7 @@ import java.util.TimeZone;
     "uk.gov.hmcts.reform.document", "uk.gov.hmcts.reform.authorisation", "uk.gov.hmcts.reform.ccd.document"})
 @EnableFeignClients(basePackages = {"uk.gov.hmcts.ethos.replacement"})
 @EnableScheduling
+@EnableCaching
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, this is not a utility class
 @Slf4j
 public class DocmosisApplication implements CommandLineRunner {

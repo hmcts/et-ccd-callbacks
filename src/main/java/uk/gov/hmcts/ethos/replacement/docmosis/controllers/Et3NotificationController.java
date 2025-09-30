1b4aa4dba90fac13d63d0e743b9b478b5c569398
Harpreet Jhita <harpreet.jhita@justice.gov.uk>
2025-09-30 11:04:06 +0100

    Reapply "Merge branch 'master' into ecc-amendments"
    This reverts commit 656fbcee745b683ed0808634892087dfb53eb526.

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/Et3NotificationController.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/Et3NotificationController.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.ethos.replacement.docmosis.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -156,7 +157,7 @@ public class Et3NotificationController {
     })
     public ResponseEntity<CCDCallbackResponse> aboutToSubmit(
             @RequestBody CCDRequest ccdRequest,
-            @RequestHeader("Authorization") String userToken) {
+            @RequestHeader("Authorization") String userToken) throws JsonProcessingException {
         CaseData caseData = ccdRequest.getCaseDetails().getCaseData();
         List<String> errors = new ArrayList<>();
         ET3DocumentHelper.addOrRemoveET3Documents(caseData);

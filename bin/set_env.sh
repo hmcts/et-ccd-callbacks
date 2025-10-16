0a416b8f295e238220b4f03ab9daf731bed738f1
William Craig <mr.craig1991@gmail.com>
2025-10-16 19:15:37 +0100

    -Added setup script to set variables such as key from az keyvault to environment - Updated configuration for CCD paths, to make them non-mandatory

--- bin/set_env.sh
+++ bin/set_env.sh
@@ -22,15 +22,15 @@ export ET_COS_CFTLIB_DB_PASSWORD="postgres"
 export SPRING_PROFILES_ACTIVE="cftlib"
 
 ###! optional - CCD Config paths - update these with your actual local repository paths before use!###
-#export ENGLANDWALES_CCD_CONFIG_PATH="$HOME/Source/et-ccd-definitions-englandwales"
-#export SCOTLAND_CCD_CONFIG_PATH="$HOME/Source/et-ccd-definitions-scotland"
-#export ADMIN_CCD_CONFIG_PATH="$HOME/Source/et-ccd-definitions-admin"
+export ENGLANDWALES_CCD_CONFIG_PATH="$HOME/Source/et-ccd-definitions-englandwales"
+export SCOTLAND_CCD_CONFIG_PATH="$HOME/Source/et-ccd-definitions-scotland"
+export ADMIN_CCD_CONFIG_PATH="$HOME/Source/et-ccd-definitions-admin"
 
 export ET_COS_SYSTEM_USER="admin@hmcts.net"
 export ET_COS_SYSTEM_USER_PASSWORD="Password"
 
 # Optional - set to false to prevent CCD definitions from being imported at startup
-export CFTLIB_IMPORT_CCD_DEFS_ON_BOOT="true"
+#export CFTLIB_IMPORT_CCD_DEFS_ON_BOOT="true"
 
 printf '%s\n' \
   "XUI_LD_ID=${XUI_LD_ID}" \
@@ -40,7 +40,7 @@ printf '%s\n' \
   "SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE}" \
   "CFTLIB_IMPORT_CCD_DEFS_ON_BOOT=${CFTLIB_IMPORT_CCD_DEFS_ON_BOOT}" \
   "ET_COS_SYSTEM_USER=${ET_COS_SYSTEM_USER}" \
-  "ET_COS_SYSTEM_USER_PASSWORD=${ET_COS_SYSTEM_USER_PASSWORD}"
-  #"ENGLANDWALES_CCD_CONFIG_PATH=${ENGLANDWALES_CCD_CONFIG_PATH}" \
-  #"SCOTLAND_CCD_CONFIG_PATH=${SCOTLAND_CCD_CONFIG_PATH}" \
-  #"ADMIN_CCD_CONFIG_PATH=${ADMIN_CCD_CONFIG_PATH}" \
+  "ET_COS_SYSTEM_USER_PASSWORD=${ET_COS_SYSTEM_USER_PASSWORD}" \
+  "ENGLANDWALES_CCD_CONFIG_PATH=${ENGLANDWALES_CCD_CONFIG_PATH}" \
+  "SCOTLAND_CCD_CONFIG_PATH=${SCOTLAND_CCD_CONFIG_PATH}" \
+  "ADMIN_CCD_CONFIG_PATH=${ADMIN_CCD_CONFIG_PATH}" \

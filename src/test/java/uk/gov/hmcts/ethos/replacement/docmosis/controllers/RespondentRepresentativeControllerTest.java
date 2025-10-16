9353b0436291f0c0ed37f6513ad5f05f2b80c6c8
Mehmet Tahir Dede <mehmet.dede@justice.gov.uk>
2025-10-16 14:29:46 +0100

    Updates for removal of respondent representative called directly by et-syr project.

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/RespondentRepresentativeControllerTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/RespondentRepresentativeControllerTest.java
@@ -9,9 +9,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.items.RepresentedTypeRItem;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.VerifyTokenService;
 import uk.gov.hmcts.ethos.replacement.docmosis.utils.JsonMapper;
 import uk.gov.hmcts.ethos.utils.CCDRequestBuilder;
+import uk.gov.hmcts.ethos.utils.CaseDataBuilder;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
@@ -33,7 +38,7 @@ class RespondentRepresentativeControllerTest {
     private JsonMapper jsonMapper;
 
     @Test
-    void testRemoveOwnRepresentative() throws Exception {
+    void testRemoveOwnRepresentative_withoutRepCollection() throws Exception {
         CCDRequest ccdRequest = CCDRequestBuilder.builder().build();
         String token = "some-token";
         when(verifyTokenService.verifyTokenSignature(token)).thenReturn(true);
@@ -42,6 +47,27 @@ class RespondentRepresentativeControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("Authorization", token)
                 .content(jsonMapper.toJson(ccdRequest)))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(JsonMapper.DATA, notNullValue()))
+                .andExpect(jsonPath(JsonMapper.ERRORS, nullValue()))
+                .andExpect(jsonPath(JsonMapper.WARNINGS, nullValue()));
+    }
+
+    @Test
+    void testRemoveOwnRepresentative_withRepCollection() throws Exception {
+        CaseData caseData = CaseDataBuilder.builder().build();
+        RepresentedTypeRItem representedTypeRItem = RepresentedTypeRItem.builder().id("123").build();
+        caseData.setRepCollection(List.of(representedTypeRItem));
+        caseData.setRepCollectionToRemove(List.of(representedTypeRItem));
+        CCDRequest ccdRequest = CCDRequestBuilder.builder().withCaseData(caseData).build();
+        String token = "some-token";
+        when(verifyTokenService.verifyTokenSignature(token)).thenReturn(true);
+
+        mockMvc.perform(post("/respondentRepresentative/removeOwnRepresentative")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", token)
+                        .content(jsonMapper.toJson(ccdRequest)))
 
                 .andExpect(status().isOk())
                 .andExpect(jsonPath(JsonMapper.DATA, notNullValue()))

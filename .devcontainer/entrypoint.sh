438bc2146a60dadd018f88c8def404015bf286dc
Alex Thayne <alex.thayne@justice.gov.uk>
2026-01-26 13:19:23 +0000

    Remove devcontainer and vscode settings from repo, add to gitignore

--- .devcontainer/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Entrypoint script - runs as root at container startup
-# Sets up firewall before user session begins
-
-# Run firewall setup
-/usr/local/bin/setup-firewall.sh
-
-# Execute the main command
-exec "$@"

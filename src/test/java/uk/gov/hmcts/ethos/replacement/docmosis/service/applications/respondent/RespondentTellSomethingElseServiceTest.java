6871b9938527d36c6d0db93c734f15b94cf211e0
btensay <tensay.bulcha@gmail.com>
2026-02-02 13:28:09 +0000

    checklist fix

--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/applications/respondent/RespondentTellSomethingElseServiceTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/service/applications/respondent/RespondentTellSomethingElseServiceTest.java
@@ -67,7 +67,6 @@ import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;

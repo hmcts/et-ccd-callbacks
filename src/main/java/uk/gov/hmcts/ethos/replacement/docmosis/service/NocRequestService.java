e8810de2df0326184ed870b3b2b074e957301c3e
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2026-02-23 18:05:22 +0000

    Add NocRequestController.java

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/NocRequestService.java
@@ -0,0 +1,26 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NocRequestService {
+
+    public void revokeClaimantLegalRep() {
+    }
+
+    public void sendEmailToAdmin() {
+    }
+
+    public void sendEmailToThisLegalRep() {
+    }
+
+    public void sendEmailToRemovedParty() {
+    }
+
+    public void sendEmailToOtherParty() {
+    }
+}

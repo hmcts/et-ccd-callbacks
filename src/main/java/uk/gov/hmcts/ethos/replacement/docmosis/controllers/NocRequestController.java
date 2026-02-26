c8d12fb0fcca4cb891c3bf263a87e7e18931b5dd
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2026-02-26 11:13:31 +0000

    Add NocRequestController.java

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/NocRequestController.java
@@ -0,0 +1,79 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.controllers;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.et.common.model.ccd.CCDCallbackResponse;
+import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
+import uk.gov.hmcts.ethos.replacement.docmosis.service.NocRequestService;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static uk.gov.hmcts.ethos.replacement.docmosis.helpers.CallbackRespHelper.getCallbackRespEntityNoErrors;
+
+@Slf4j
+@RequestMapping("/noCRequest")
+@RestController
+@RequiredArgsConstructor
+public class NocRequestController {
+
+    private static final String green1 = "<h1>Notice of change successful</h1>";
+    private static final String green2 = "Your organisation is no longer representing a client on ";
+    private static final String contentHeader = "<h3>What happens next</h3>";
+    private static final String content1 = "<p>A notification will be sent to all the parties on this case.</p>";
+    private static final String content2 = "<p>This case will no longer appear in your organisation's case list.</p>";
+    private static final String content3 = "<p>This is a new online process - "
+        + "you don't need to file any further documents in relation to this notice of change with the court.</p>";
+
+    private final NocRequestService nocRequestService;
+
+    @PostMapping(value = "/aboutToSubmit", consumes = APPLICATION_JSON_VALUE)
+    @Operation(summary = "nocRequest submitted page")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully",
+            content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = CCDCallbackResponse.class))
+            }),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<CCDCallbackResponse> aboutToSubmit(
+        @RequestBody CCDRequest ccdRequest,
+        @RequestHeader("Authorization") String userToken) {
+
+        nocRequestService.revokeClaimantLegalRep(userToken, ccdRequest.getCaseDetails());
+        return getCallbackRespEntityNoErrors(ccdRequest.getCaseDetails().getCaseData());
+    }
+
+    @PostMapping(value = "/submitted", consumes = APPLICATION_JSON_VALUE)
+    @Operation(summary = "nocRequest submitted page")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Accessed successfully",
+            content = {
+                @Content(mediaType = "application/json", schema = @Schema(implementation = CCDCallbackResponse.class))
+            }),
+        @ApiResponse(responseCode = "400", description = "Bad Request"),
+        @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
+    public ResponseEntity<CCDCallbackResponse> submitted(
+        @RequestBody CCDRequest ccdRequest,
+        @RequestHeader("Authorization") String userToken) {
+
+        nocRequestService.sendEmailNotification(userToken, ccdRequest.getCaseDetails());
+
+        return ResponseEntity.ok(CCDCallbackResponse.builder()
+            .data(ccdRequest.getCaseDetails().getCaseData())
+            .confirmation_header(green1 + "<h5>" + green2 + ccdRequest.getCaseDetails().getCaseId() + "</h5>" + "<br>")
+            .confirmation_body(contentHeader + content1 + content2 + content3)
+            .build());
+    }
+}

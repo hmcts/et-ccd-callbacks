0364d4168745975f7c390c683a2502477e5b9f8f
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2026-03-02 13:12:09 +0000

    Update NocRequestService.java

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/NocRequestService.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/service/NocRequestService.java
@@ -2,43 +2,21 @@ package uk.gov.hmcts.ethos.replacement.docmosis.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.webjars.NotFoundException;
 import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
-import uk.gov.hmcts.et.common.model.ccd.CaseUserAssignment;
-import uk.gov.hmcts.et.common.model.ccd.RetrieveOrgByIdResponse;
 import uk.gov.hmcts.et.common.model.ccd.types.Organisation;
 import uk.gov.hmcts.et.common.model.ccd.types.RepresentedTypeC;
-import uk.gov.hmcts.ethos.replacement.docmosis.helpers.NocNotificationHelper;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.noc.NocCcdService;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.noc.NocNotificationService;
-import uk.gov.hmcts.ethos.replacement.docmosis.utils.ClaimantUtils;
 import uk.gov.hmcts.ethos.replacement.docmosis.utils.noc.ClaimantRepresentativeUtils;
-
-import java.util.List;
-import java.util.Map;
-
-import static uk.gov.hmcts.ecm.common.model.helper.Constants.CLAIMANT_TITLE;
-import static uk.gov.hmcts.ethos.replacement.docmosis.constants.NOCConstants.WARNING_FAILED_TO_SEND_NOC_NOTIFICATION_EMAIL_ORGANISATION;
-import static uk.gov.hmcts.ethos.replacement.docmosis.constants.NOCConstants.WARNING_FAILED_TO_SEND_NOC_NOTIFICATION_TO_REMOVED_REPRESENTATIVE;
-import static uk.gov.hmcts.ethos.replacement.docmosis.constants.NOCConstants.WARNING_FAILED_TO_SEND_NOC_NOTIFICATION_TO_UNREPRESENTED_PARTY;
-import static uk.gov.hmcts.ethos.replacement.docmosis.constants.NOCConstants.WARNING_INVALID_CLAIMANT_EMAIL_CLAIMANT_NOT_NOTIFIED_FOR_REMOVAL_OF_REPRESENTATIVE;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class NocRequestService {
 
-    private final EmailService emailService;
     private final NocCcdService nocCcdService;
     private final NocNotificationService nocNotificationService;
-    private final CaseAccessService caseAccessService;
-    private final EmailNotificationService emailNotificationService;
-
 
     public void revokeClaimantLegalRep(String userToken, CaseDetails caseDetails) {
         // create a copy of existing claimant legal rep details

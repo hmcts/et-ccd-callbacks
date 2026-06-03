8efd01ad12c71a66e9ce56e440908ca0e0cf31a7
E Domenger-Boyce <ellis.domengerboyce@yahoo.co.uk>
2026-06-03 10:50:40 +0100

    Decentralisation (#3090)
    * Update logstash query and add servicebus config
    
    * update request interceptor to verify persistence contract
    
    * Base callback handler and example refactor
    
    * gradle updates
    
    * Add Callback Handlers and corresponding tests
    
    * Cleanup to Handlers
    
    * More cleanup
    
    * More cleanup
    
    * More line cutting
    
    * More line cutting
    
    * fix test failures
    
    * Delete Handlers, add CaseView classes
    
    * checkstyle
    
    * remove case details converter
    
    * fix build issue
    
    * fix build issue 2
    
    * fix build issue 3
    
    * fix pmd
    
    * Unit test coverage
    
    * force proper tomcat version
    
    * Strictly
    
    * more dependency stuff
    
    * more build gradle fixes
    
    * point to correct db logstash
    
    * correct user
    
    * correct logstash pass
    
    * correct logstash again
    
    * correct logstash again
    
    * correct logstash again
    
    * get around PersistenceStrategyResolver bug
    
    * bump lib
    
    * Add states
    
    * Fix pmdMain
    
    * sonar
    
    * defs
    
    * Attempt pk conflict fix
    
    * Update preview db password
    
    * Upgrade tomcat
    
    * checkstyle
    
    * fix pmdTest
    
    * Fix dockerfile
    
    * Update decentralised runtime snapshot tag
    
    * Update decentralised runtime snapshot tag
    
    * generate excel
    
    * generate excel
    
    * fix failed startup
    
    * Update decentralised runtime snapshot fallback tag
    
    * Update decentralised runtime snapshot fallback tag
    
    * update aboutToSubmit with authToken
    
    * bump lib, update noc controller
    
    * ensure base urls are covered
    
    * fix security classification issue
    
    * cve fix
    
    * fix pmd
    
    * fix checkstyle
    
    * fix smoke test
    
    * Point at preview db
    
    * Fix preview ET COS reference data import
    
    * Fix preview ET COS datasource options
    
    * update config to patch sptribs
    
    * Fix Jenkinsfile secret list syntax
    
    * Wait for ET COS reference tables before preview import
    
    * Wait for definition-store schema before preview import
    
    * Allow yellow Elasticsearch health in preview
    
    * Update Tomcat embed to 10.1.55
    
    * Update et-shared Tomcat embed to 10.1.55
    
    * Add cftlib dev-only runtime dependency
    
    * Allow preview gateway CORS origins
    
    * Enable preview case activity API
    
    * Add SearchCriteria field for citizen role in AuthorisationCaseField.json
    
    * Update CRUD permissions for citizen role in AuthorisationCaseField.json
    
    * Add Gson dependency to app
    
    * Align preview WA service bus topic
    
    * Restore preview CCD message publisher for WA
    
    * Use CCD schema for preview message publisher
    
    ---------
    
    Co-authored-by: EllisD-B <ellis.domengerboyce@hmcts.net>
    Co-authored-by: Harpreet Jhita <harpreet.jhita@justice.gov.uk>
    Co-authored-by: Jason Paige <j@jasonpaige.co.uk>
    Co-authored-by: Jason Paige <jasonpaige@users.noreply.github.com>
    Co-authored-by: Mehmet Tahir Dede <105645217+JustMehmet@users.noreply.github.com>

--- et-shared/src/main/java/uk/gov/hmcts/et/common/model/ccd/CCDCallbackResponse.java
+++ et-shared/src/main/java/uk/gov/hmcts/et/common/model/ccd/CCDCallbackResponse.java
@@ -6,7 +6,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import uk.gov.hmcts.ccd.sdk.CallbackResponse;
+import uk.gov.hmcts.ccd.sdk.SubmittedCallbackResponse;
 import uk.gov.hmcts.et.common.model.generic.GenericCallbackResponse;
+
+import java.util.Map;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -14,8 +18,24 @@ import uk.gov.hmcts.et.common.model.generic.GenericCallbackResponse;
 @AllArgsConstructor
 @SuperBuilder
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class CCDCallbackResponse extends GenericCallbackResponse {
+public class CCDCallbackResponse extends GenericCallbackResponse implements CallbackResponse<CaseData>, SubmittedCallbackResponse {
 
     private CaseData data;
-}
+    private Map<String, Object> dataClassification;
+    private String securityClassification;
+    private String errorMessageOverride;
 
+    public CCDCallbackResponse(CaseData data) {
+        this.data = data;
+    }
+
+    @Override
+    public String getConfirmationHeader() {
+        return this.getConfirmation_header();
+    }
+
+    @Override
+    public String getConfirmationBody() {
+        return this.getConfirmation_body();
+    }
+}

b0bb67f976272b09d21281e11e202ba552cd99e5
Alex McAusland <a.m@user.com>
2026-07-14 09:49:03 +0100

    Use class-level access for pre-hearing deposit fields

--- src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/prehearingdeposit/PreHearingDepositData.java
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/prehearingdeposit/PreHearingDepositData.java
@@ -8,122 +8,112 @@ import uk.gov.hmcts.ccd.sdk.type.FieldType;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.admin.types.ImportFile;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@CCD(access = PreHearingDepositAccess.class)
 @Data
 public class PreHearingDepositData {
     @JsonProperty("caseReferenceNumber")
-    @CCD(label = "Case Reference Number", access = PreHearingDepositAccess.class)
+    @CCD(label = "Case Reference Number")
     private String caseReferenceNumber;
     @JsonProperty("preHearingDepositImportFile")
-    @CCD(label = "Pre-Hearing Deposit Import File", access = PreHearingDepositAccess.class)
+    @CCD(label = "Pre-Hearing Deposit Import File")
     private ImportFile preHearingDepositImportFile;
     @JsonProperty("caseNumber")
-    @CCD(label = "Case Number", access = PreHearingDepositAccess.class)
+    @CCD(label = "Case Number")
     private String caseNumber;
     @JsonProperty("payeeName")
-    @CCD(label = "Payee Name (if different from applicant)", access = PreHearingDepositAccess.class)
+    @CCD(label = "Payee Name (if different from applicant)")
     private String payeeName;
     @JsonProperty("regionOffice")
-    @CCD(label = "Region Office", access = PreHearingDepositAccess.class)
+    @CCD(label = "Region Office")
     private String regionOffice;
     @JsonProperty("refundReference")
-    @CCD(label = "Refund Reference", access = PreHearingDepositAccess.class)
+    @CCD(label = "Refund Reference")
     private String refundReference;
     @JsonProperty("dateSentForRefund")
-    @CCD(label = "Date sent for Refund", typeOverride = FieldType.Date,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Date sent for Refund", typeOverride = FieldType.Date)
     private String dateSentForRefund;
     @JsonProperty("status")
-    @CCD(label = "Status", access = PreHearingDepositAccess.class)
+    @CCD(label = "Status")
     private String status;
     @JsonProperty("journalConfirmedPaid")
-    @CCD(label = "Journal Confirmed Paid", typeOverride = FieldType.Date,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Journal Confirmed Paid", typeOverride = FieldType.Date)
     private String journalConfirmedPaid;
     @JsonProperty("comments")
-    @CCD(label = "Other Comments", access = PreHearingDepositAccess.class)
+    @CCD(label = "Other Comments")
     private String comments;
     @JsonProperty("journalConfirmedReceipt")
-    @CCD(label = "Journal Confirmed Receipt", typeOverride = FieldType.Date,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Journal Confirmed Receipt", typeOverride = FieldType.Date)
     private String journalConfirmedReceipt;
     @JsonProperty("bankingDate")
-    @CCD(label = "Banking Date", typeOverride = FieldType.Date,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Banking Date", typeOverride = FieldType.Date)
     private String bankingDate;
     @JsonProperty("phrNumber")
-    @CCD(label = "PHR No.", access = PreHearingDepositAccess.class)
+    @CCD(label = "PHR No.")
     private String phrNumber;
     @JsonProperty("mr1Reference")
-    @CCD(label = "MR1 Reference", access = PreHearingDepositAccess.class)
+    @CCD(label = "MR1 Reference")
     private String mr1Reference;
     @JsonProperty("depositComments")
-    @CCD(label = "Deposit Comments", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit Comments")
     private String depositComments;
     @JsonProperty("depositReceivedFrom")
-    @CCD(label = "Deposit received From", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit received From")
     private String depositReceivedFrom;
     @JsonProperty("documentUpload")
-    @CCD(label = "Document Upload", access = PreHearingDepositAccess.class)
+    @CCD(label = "Document Upload")
     private String documentUpload;
     @JsonProperty("depositRefundedTo")
-    @CCD(label = "Deposit refunded to", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit refunded to")
     private String depositRefundedTo;
     @JsonProperty("notes")
-    @CCD(label = "Notes", access = PreHearingDepositAccess.class)
+    @CCD(label = "Notes")
     private String notes;
     @JsonProperty("claimantOrRespondentName")
-    @CCD(label = "Claimant or Respondent Name", access = PreHearingDepositAccess.class)
+    @CCD(label = "Claimant or Respondent Name")
     private String claimantOrRespondentName;
     @JsonProperty("depositAmount")
-    @CCD(label = "Deposit amount", typeOverride = FieldType.MoneyGBP,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit amount", typeOverride = FieldType.MoneyGBP)
     private String depositAmount;
     @JsonProperty("depositOrderedAgainst")
-    @CCD(label = "Deposit ordered against", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit ordered against")
     private String depositOrderedAgainst;
     @JsonProperty("depositRequestedBy")
-    @CCD(label = "Deposit requested by", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit requested by")
     private String depositRequestedBy;
     @JsonProperty("depositCovers")
-    @CCD(label = "Deposit covers", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit covers")
     private String depositCovers;
     @JsonProperty("depositOrderSent")
-    @CCD(label = "Deposit order sent", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit order sent")
     private String depositOrderSent;
     @JsonProperty("depositDue")
-    @CCD(label = "Deposit due", typeOverride = FieldType.Date,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit due", typeOverride = FieldType.Date)
     private String depositDue;
     @JsonProperty("depositTimeExtension")
-    @CCD(label = "Deposit time extension", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit time extension")
     private String depositTimeExtension;
     @JsonProperty("depositTimeExtensionDue")
-    @CCD(label = "Deposit time extension due", typeOverride = FieldType.Date,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit time extension due", typeOverride = FieldType.Date)
     private String depositTimeExtensionDue;
     @JsonProperty("depositReceived")
-    @CCD(label = "Deposit received", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit received")
     private String depositReceived;
     @JsonProperty("dateDepositReceived")
-    @CCD(label = "Date deposit received", typeOverride = FieldType.Date,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Date deposit received", typeOverride = FieldType.Date)
     private String dateDepositReceived;
     @JsonProperty("depositRefund")
-    @CCD(label = "Deposit refund", typeOverride = FieldType.YesOrNo,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit refund", typeOverride = FieldType.YesOrNo)
     private String depositRefund;
     @JsonProperty("amountRefunded")
-    @CCD(label = "Amount refunded", typeOverride = FieldType.MoneyGBP,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Amount refunded", typeOverride = FieldType.MoneyGBP)
     private String amountRefunded;
     @JsonProperty("depositRefundDate")
-    @CCD(label = "Deposit refund date", typeOverride = FieldType.Date,
-        access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit refund date", typeOverride = FieldType.Date)
     private String depositRefundDate;
     @JsonProperty("chequeOrPONumber")
-    @CCD(label = "Cheque or PO number", access = PreHearingDepositAccess.class)
+    @CCD(label = "Cheque or PO number")
     private String chequeOrPONumber;
     @JsonProperty("receivedBy")
-    @CCD(label = "Deposit received by", access = PreHearingDepositAccess.class)
+    @CCD(label = "Deposit received by")
     private String receivedBy;
 }

3da57aa874916b9c9832a2ab3b8ad8cfd8c07f2d
Jason Paige <j@jasonpaige.co.uk>
2026-07-17 10:10:15 +0100

    Load Legal Ops smoke credentials

--- bin/pull-latest-e2e-tests.sh
+++ bin/pull-latest-e2e-tests.sh
@@ -6,6 +6,3 @@ cd et-xui-e2e-tests
 
 echo "Switch to Master branch on et-xui-e2e-tests repo"
 git checkout master
-
-echo "Increase login redirect wait for preview smoke tests"
-perl -0pi -e 's/waitForURL\(new RegExp\(baseUrl\), \{ timeout: 10000 \}\)/waitForURL(new RegExp(baseUrl), { timeout: 30000 })/' playwrighte2e/pages/loginPage.ts

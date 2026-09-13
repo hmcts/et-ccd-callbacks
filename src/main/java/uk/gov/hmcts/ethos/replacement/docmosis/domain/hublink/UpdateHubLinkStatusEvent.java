0d1116387145256259af9c8bf8a36d0cb9c87de6
Alex McAusland <a.m@user.com>
2026-09-13 21:38:27 +0100

    add hublink status entity and write path

---
+++ src/main/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/hublink/UpdateHubLinkStatusEvent.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.domain.hublink;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
+import uk.gov.hmcts.ccd.sdk.api.callback.AboutToStartOrSubmitResponse;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.ethos.replacement.docmosis.config.EtJsonCcdConfig.PlaceholderRole;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.caseview.state.CaseState;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.ccd.HubLinkStatus;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.ccd.HubLinkStatusRepository;
+
+import java.util.Set;
+
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_CASE_TYPE_ID;
+import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_CASE_TYPE_ID;
+
+@Component
+@RequiredArgsConstructor
+public class UpdateHubLinkStatusEvent implements CCDConfig<CaseData, CaseState, PlaceholderRole> {
+
+    static final String EVENT_ID = "UPDATE_HUBLINK_STATUS";
+
+    private final HubLinkStatusRepository hubLinkStatusRepository;
+
+    @Override
+    public Set<String> caseTypeIds() {
+        return Set.of(ENGLANDWALES_CASE_TYPE_ID, SCOTLAND_CASE_TYPE_ID);
+    }
+
+    @Override
+    public void configure(ConfigBuilder<CaseData, CaseState, PlaceholderRole> builder) {
+        builder.event(EVENT_ID)
+            .forAllStates()
+            .aboutToSubmitCallback(this::submit);
+    }
+
+    private AboutToStartOrSubmitResponse<CaseData, CaseState> submit(
+        CaseDetails<CaseData, CaseState> details,
+        CaseDetails<CaseData, CaseState> detailsBefore
+    ) {
+        CaseData data = details.getData();
+        if (data.getHubLinksStatuses() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Hub-link statuses are required");
+        }
+        hubLinkStatusRepository.saveAndFlush(
+            HubLinkStatus.create(details.getId(), data.getHubLinksStatuses())
+        );
+
+        return AboutToStartOrSubmitResponse.<CaseData, CaseState>builder()
+            .data(data)
+            .build();
+    }
+}
---
+++ src/test/integration/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/hublink/UpdateHubLinkStatusEventTest.java
@@ -0,0 +1,129 @@
+package uk.gov.hmcts.ethos.replacement.docmosis.domain.hublink;
+
+import com.google.common.collect.ImmutableSet;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
+import uk.gov.hmcts.ccd.sdk.ResolvedCCDConfig;
+import uk.gov.hmcts.ccd.sdk.api.CaseDetails;
+import uk.gov.hmcts.ccd.sdk.config.DecentralisedDataConfiguration;
+import uk.gov.hmcts.et.common.model.ccd.CaseData;
+import uk.gov.hmcts.et.common.model.ccd.types.citizenhub.HubLinksStatuses;
+import uk.gov.hmcts.ethos.replacement.docmosis.config.EtJsonCcdConfig.PlaceholderRole;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.caseview.state.CaseState;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.ccd.HubLinkStatus;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.EtCosPostgresqlContainer;
+import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.ccd.HubLinkStatusRepository;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(properties = "core_case_data.api.url=localhost:4452")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@Import(DecentralisedDataConfiguration.class)
+class UpdateHubLinkStatusEventTest {
+
+    private static final long CASE_REFERENCE = 1234567890123456L;
+    private static final PostgreSQLContainer<?> POSTGRES = EtCosPostgresqlContainer.getInstance();
+
+    static {
+        POSTGRES.start();
+    }
+
+    @Autowired
+    private HubLinkStatusRepository repository;
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        jdbc.update("delete from ccd.case_data where reference = ?", CASE_REFERENCE);
+        jdbc.update("""
+            insert into ccd.case_data (
+                id,
+                reference,
+                security_classification,
+                jurisdiction,
+                case_type_id,
+                state,
+                data
+            ) values (?, ?, 'PUBLIC', 'EMPLOYMENT', 'ET_EnglandWales', 'Accepted', '{}'::jsonb)
+            """, CASE_REFERENCE, CASE_REFERENCE);
+    }
+
+    @Test
+    void insertsThenUpdatesStatus() {
+        saveStatus("notStarted");
+        flushAndClear();
+        assertStoredStatus("notStarted");
+
+        saveStatus("completed");
+        flushAndClear();
+        assertStoredStatus("completed");
+    }
+
+    @Test
+    void eventWritesStatus() {
+        UpdateHubLinkStatusEvent eventConfig = new UpdateHubLinkStatusEvent(repository);
+        ResolvedCCDConfig<CaseData, CaseState, PlaceholderRole> resolvedConfig = new ResolvedCCDConfig<>(
+            CaseData.class,
+            CaseState.class,
+            PlaceholderRole.class,
+            Map.of(),
+            ImmutableSet.copyOf(CaseState.values())
+        );
+        ConfigBuilderImpl<CaseData, CaseState, PlaceholderRole> builder = new ConfigBuilderImpl<>(resolvedConfig);
+        eventConfig.configure(builder);
+
+        CaseData caseData = new CaseData();
+        caseData.setHubLinksStatuses(statuses("completed"));
+
+        var response = builder.build()
+            .getEvents()
+            .get(UpdateHubLinkStatusEvent.EVENT_ID)
+            .getAboutToSubmitCallback()
+            .handle(CaseDetails.<CaseData, CaseState>builder()
+                        .id(CASE_REFERENCE)
+                        .data(caseData)
+                        .build(),
+                    null);
+
+        assertThat(eventConfig.caseTypeIds()).containsExactlyInAnyOrder("ET_EnglandWales", "ET_Scotland");
+        assertThat(response.getData()).isSameAs(caseData);
+        assertStoredStatus("completed");
+    }
+
+    private HubLinksStatuses statuses(String personalDetails) {
+        HubLinksStatuses statuses = new HubLinksStatuses();
+        statuses.setPersonalDetails(personalDetails);
+        return statuses;
+    }
+
+    private void saveStatus(String personalDetails) {
+        repository.saveAndFlush(HubLinkStatus.create(CASE_REFERENCE, statuses(personalDetails)));
+    }
+
+    private void assertStoredStatus(String personalDetails) {
+        HubLinkStatus stored = repository.findById(CASE_REFERENCE).orElseThrow();
+        assertThat(stored.getData().getPersonalDetails()).isEqualTo(personalDetails);
+    }
+
+    private void flushAndClear() {
+        entityManager.flush();
+        entityManager.clear();
+    }
+}

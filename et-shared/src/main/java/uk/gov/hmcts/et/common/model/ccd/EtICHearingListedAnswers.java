b35284e370cedf6fa9d32f6a35e2318f72300574
Cindy Chan <94921454+cindychmcts@users.noreply.github.com>
2026-09-15 11:23:18 +0100

    Update InitialConsiderationHelper.java

--- et-shared/src/main/java/uk/gov/hmcts/et/common/model/ccd/EtICHearingListedAnswers.java
+++ et-shared/src/main/java/uk/gov/hmcts/et/common/model/ccd/EtICHearingListedAnswers.java
@@ -2,12 +2,18 @@ package uk.gov.hmcts.et.common.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EtICHearingListedAnswers {
     @JsonProperty("etICHearingListed")
     private List<String> etICHearingListed;

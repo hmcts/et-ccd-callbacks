8a6ea5b0ef4c4816e699f85771c5242343ad33af
Alex McAusland <a.m@user.com>
2026-09-16 08:12:07 +0100

    checkstyle

--- src/test/integration/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/caseview/ETCaseViewTest.java
+++ src/test/integration/java/uk/gov/hmcts/ethos/replacement/docmosis/domain/caseview/ETCaseViewTest.java
@@ -17,8 +17,6 @@ import uk.gov.hmcts.ethos.replacement.docmosis.domain.ccd.HubLinkStatus;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.EtCosPostgresqlContainer;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.ccd.HubLinkStatusRepository;
 
-import java.util.Set;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_CASE_TYPE_ID;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.SCOTLAND_CASE_TYPE_ID;

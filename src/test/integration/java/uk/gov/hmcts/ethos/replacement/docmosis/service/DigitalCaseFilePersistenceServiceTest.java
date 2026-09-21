df349661378e6ff2f34dbe9cdf666fcff8a7e54d
Alex McAusland <a.m@user.com>
2026-09-21 15:38:40 +0100

    simplify tests

--- src/test/integration/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DigitalCaseFilePersistenceServiceTest.java
+++ src/test/integration/java/uk/gov/hmcts/ethos/replacement/docmosis/service/DigitalCaseFilePersistenceServiceTest.java
@@ -9,14 +9,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
 import org.testcontainers.containers.PostgreSQLContainer;
 import uk.gov.hmcts.ccd.sdk.config.DecentralisedDataConfiguration;
 import uk.gov.hmcts.et.common.model.bundle.Bundle;
 import uk.gov.hmcts.et.common.model.bundle.BundleDetails;
+import uk.gov.hmcts.et.common.model.bundle.DocumentLink;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.types.DigitalCaseFileType;
 import uk.gov.hmcts.ethos.replacement.docmosis.domain.repository.EtCosPostgresqlContainer;
@@ -26,7 +23,6 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest(properties = "core_case_data.api.url=localhost:4452")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -49,9 +45,6 @@ class DigitalCaseFilePersistenceServiceTest {
 
     @Autowired
     private JdbcTemplate jdbc;
-
-    @Autowired
-    private PlatformTransactionManager transactionManager;
 
     private DigitalCaseFilePersistenceService service;
 
@@ -93,9 +86,9 @@ class DigitalCaseFilePersistenceServiceTest {
     }
 
     @Test
-    void storesCompletionAndClearsTheActiveBundleId() {
+    void storesSuccessfulCompletionWithItsDocument() {
         Bundle pendingBundle = bundle();
-        Bundle completedBundle = completedBundle(pendingBundle);
+        Bundle completedBundle = successfullyCompletedBundle(pendingBundle);
         start(digitalCaseFile("DCF Updating"), pendingBundle);
         CaseData caseData = new CaseData();
         caseData.setCaseBundles(List.of(completedBundle));
@@ -104,7 +97,14 @@ class DigitalCaseFilePersistenceServiceTest {
         flushAndClear();
 
         var storedDcf = digitalCaseFileRepository.findById(CASE_REFERENCE).orElseThrow();
-        assertThat(storedDcf.getData().getStatus()).startsWith("DCF Failed to generate:");
+        assertThat(storedDcf.getData().getStatus()).startsWith("DCF Generated:");
+        assertThat(storedDcf.getData().getUploadedDocument())
+            .extracting("documentUrl", "documentBinaryUrl", "documentFilename")
+            .containsExactly(
+                "http://documents.example/documents/generated",
+                "http://documents.example/documents/generated/binary",
+                "generated.pdf"
+            );
         assertThat(storedDcf.getActiveBundleId()).isNull();
         assertThat(storedDcf.getCompletedBundleId()).hasToString(pendingBundle.value().getId());
     }
@@ -226,30 +226,6 @@ class DigitalCaseFilePersistenceServiceTest {
         assertThat(caseData.getDigitalCaseFile().getStatus()).startsWith("DCF Failed to generate:");
     }
 
-    @Test
-    @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    void rollsBackTableAndBlobWritesWithTheEnclosingTransaction() {
-        Bundle activeBundle = bundle();
-        TransactionTemplate transaction = new TransactionTemplate(transactionManager);
-
-        assertThatThrownBy(() -> transaction.executeWithoutResult(status -> {
-            jdbc.update(
-                "update ccd.case_data set data = '{\"digitalCaseFile\":{\"status\":\"DCF Updating\"}}'::jsonb "
-                    + "where reference = ?",
-                CASE_REFERENCE
-            );
-            start(digitalCaseFile("DCF Updating"), activeBundle);
-            throw new IllegalStateException("submission failed");
-        })).isInstanceOf(IllegalStateException.class);
-
-        assertThat(digitalCaseFileRepository.findById(CASE_REFERENCE)).isEmpty();
-        assertThat(jdbc.queryForObject(
-            "select data::text from ccd.case_data where reference = ?",
-            String.class,
-            CASE_REFERENCE
-        )).isEqualTo("{}");
-    }
-
     private DigitalCaseFileType digitalCaseFile(String status) {
         DigitalCaseFileType digitalCaseFile = new DigitalCaseFileType();
         digitalCaseFile.setStatus(status);
@@ -275,6 +251,19 @@ class DigitalCaseFilePersistenceServiceTest {
             .value(pendingBundle.value().toBuilder()
                 .stitchStatus("FAILED")
                 .stitchingFailureMessage("Failed to generate")
+                .build())
+            .build();
+    }
+
+    private Bundle successfullyCompletedBundle(Bundle pendingBundle) {
+        return pendingBundle.toBuilder()
+            .value(pendingBundle.value().toBuilder()
+                .stitchStatus("DONE")
+                .stitchedDocument(DocumentLink.builder()
+                    .documentUrl("http://documents.example/documents/generated")
+                    .documentBinaryUrl("http://documents.example/documents/generated/binary")
+                    .documentFilename("generated.pdf")
+                    .build())
                 .build())
             .build();
     }
--- src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/DigitalCaseFileControllerTest.java
+++ src/test/java/uk/gov/hmcts/ethos/replacement/docmosis/controllers/DigitalCaseFileControllerTest.java
@@ -13,21 +13,16 @@ import uk.gov.hmcts.et.common.model.ccd.CCDRequest;
 import uk.gov.hmcts.et.common.model.ccd.CaseData;
 import uk.gov.hmcts.et.common.model.ccd.CaseDetails;
 import uk.gov.hmcts.et.common.model.ccd.items.DocumentTypeItem;
-import uk.gov.hmcts.et.common.model.ccd.types.DigitalCaseFileType;
 import uk.gov.hmcts.et.common.model.ccd.types.DocumentType;
 import uk.gov.hmcts.et.common.model.ccd.types.UploadedDocumentType;
-import uk.gov.hmcts.ethos.replacement.docmosis.client.BundleApiClient;
 import uk.gov.hmcts.ethos.replacement.docmosis.helpers.UploadDocumentHelperTest;
 import uk.gov.hmcts.ethos.replacement.docmosis.service.DigitalCaseFileService;
 import uk.gov.hmcts.ethos.replacement.docmosis.utils.JsonMapper;
-import uk.gov.hmcts.ethos.replacement.docmosis.utils.ResourceLoader;
 import uk.gov.hmcts.ethos.utils.CCDRequestBuilder;
 import uk.gov.hmcts.ethos.utils.CaseDataBuilder;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -35,14 +30,12 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.ENGLANDWALES_CASE_TYPE_ID;
-import static uk.gov.hmcts.ecm.common.model.helper.Constants.NEW_DATE_TIME_PATTERN;
 import static uk.gov.hmcts.ecm.common.model.helper.Constants.REJECTED_STATE;
 import static uk.gov.hmcts.ecm.common.model.helper.DocumentConstants.ET1;
 import static uk.gov.hmcts.ecm.common.model.helper.DocumentConstants.ET1_ATTACHMENT;
@@ -56,8 +49,6 @@ class DigitalCaseFileControllerTest extends BaseControllerTest {
     private static final String ASYNC_ABOUT_TO_SUBMIT_URL = "/dcf/asyncAboutToSubmit";
     private static final String ASYNC_COMPLETE_ABOUT_TO_SUBMIT_URL = "/dcf/asyncCompleteAboutToSubmit";
 
-    @MockitoBean
-    private BundleApiClient bundleApiClient;
     @MockitoBean
     private DigitalCaseFileService digitalCaseFileService;
     @Autowired
@@ -88,59 +79,7 @@ class DigitalCaseFileControllerTest extends BaseControllerTest {
     }
 
     @Test
-    void uploadOrRemoveDcf_Remove() throws Exception {
-        DigitalCaseFileType digitalCaseFileType = new DigitalCaseFileType();
-        digitalCaseFileType.setUploadedDocument(
-                UploadedDocumentType.builder()
-                        .documentBinaryUrl("https://test.com/1234/binary")
-                        .documentFilename("1234.pdf")
-                        .documentUrl("https://test.com/123")
-                .build());
-        ccdRequest.getCaseDetails().getCaseData().setDigitalCaseFile(digitalCaseFileType);
-        ccdRequest.getCaseDetails().getCaseData().setUploadOrRemoveDcf("Remove");
-        doAnswer(invocation -> {
-            invocation.getArgument(1, CaseDetails.class).getCaseData().setDigitalCaseFile(null);
-            return null;
-        }).when(digitalCaseFileService).createUploadRemoveDcf(anyString(), any());
-        mockMvc.perform(post(ASYNC_ABOUT_TO_SUBMIT_URL)
-                        .content(jsonMapper.toJson(ccdRequest))
-                        .header("Authorization", AUTH_TOKEN)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.digitalCaseFile", nullValue()));
-    }
-
-    @Test
-    void uploadOrRemoveDcf_Upload() throws Exception {
-        DigitalCaseFileType digitalCaseFileType = new DigitalCaseFileType();
-        digitalCaseFileType.setUploadedDocument(
-                UploadedDocumentType.builder()
-                        .documentBinaryUrl("https://test.com/1234/binary")
-                        .documentFilename("1234.pdf")
-                        .documentUrl("https://test.com/123")
-                        .build());
-        ccdRequest.getCaseDetails().getCaseData().setDigitalCaseFile(digitalCaseFileType);
-        ccdRequest.getCaseDetails().getCaseData().setUploadOrRemoveDcf("Upload");
-        doAnswer(invocation -> {
-            DigitalCaseFileType dcf = invocation.getArgument(1, CaseDetails.class).getCaseData().getDigitalCaseFile();
-            dcf.setStatus("DCF Uploaded: "
-                + LocalDateTime.now(ZoneId.of("Europe/London")).format(NEW_DATE_TIME_PATTERN));
-            return null;
-        }).when(digitalCaseFileService).createUploadRemoveDcf(anyString(), any());
-        mockMvc.perform(post(ASYNC_ABOUT_TO_SUBMIT_URL)
-                        .content(jsonMapper.toJson(ccdRequest))
-                        .header("Authorization", AUTH_TOKEN)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data.digitalCaseFile.status",
-                        is("DCF Uploaded: " + LocalDateTime.now(ZoneId.of("Europe/London"))
-                                .format(NEW_DATE_TIME_PATTERN))));
-    }
-
-    @Test
     void asyncAboutToSubmit() throws Exception {
-        when(bundleApiClient.asyncStitchBundle(anyString(), anyString(), any()))
-                .thenReturn(ResourceLoader.stitchBundleRequest());
         ccdRequest.getCaseDetails().getCaseData().setUploadOrRemoveDcf("Create");
         mockMvc.perform(post(ASYNC_ABOUT_TO_SUBMIT_URL)
                         .content(jsonMapper.toJson(ccdRequest))
@@ -148,12 +87,11 @@ class DigitalCaseFileControllerTest extends BaseControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath(JsonMapper.DATA, notNullValue()));
+        verify(digitalCaseFileService).createUploadRemoveDcf(anyString(), any(CaseDetails.class));
     }
 
     @Test
     void asyncCompleteAboutToSubmit() throws Exception {
-        when(bundleApiClient.asyncStitchBundle(anyString(), anyString(), any()))
-                .thenReturn(ResourceLoader.stitchBundleRequest());
         mockMvc.perform(post(ASYNC_COMPLETE_ABOUT_TO_SUBMIT_URL)
                         .content(jsonMapper.toJson(ccdRequest))
                         .header("Authorization", AUTH_TOKEN)
